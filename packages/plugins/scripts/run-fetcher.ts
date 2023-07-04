c723950c6f7d3f14153bbc7df628611cc88d833d
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2023-07-04 21:22:23 +0700

    add owner support

--- packages/plugins/scripts/run-fetcher.ts
+++ packages/plugins/scripts/run-fetcher.ts
@@ -12,10 +12,16 @@ if (!fetcherId || fetcherId === '') {
   process.exit(1);
 }
 
+const owner = process.argv.at(3);
+
 async function runFetcher() {
   const fetcher = fetchers.find((f) => f.id === fetcherId);
   if (!fetcher) {
     console.log(`Fetcher cannot be found: ${fetcherId}`);
+    process.exit(1);
+  }
+  if (!owner || owner === '') {
+    console.log('Owner is missing');
     process.exit(1);
   }
 
@@ -25,12 +31,9 @@ async function runFetcher() {
   };
 
   console.log('Fetching...');
-  const res = await fetcher.executor(
-    'tEsT1vjsJeKHw9GH5HpnQszn2LWmjR6q1AVCDCj51nd',
-    context
-  );
+  const elements = await fetcher.executor(owner, context);
   console.log('Portfolio elements:');
-  console.log(util.inspect(res, false, null, true));
+  console.log(util.inspect(elements, false, null, true));
   process.exit(0);
 }
 
