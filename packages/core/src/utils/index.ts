ab6d0b8735c6b10e016bd7bf9b403bdd219ae829
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2023-07-04 19:26:57 +0700

    fix

--- packages/core/src/utils/index.ts
+++ packages/core/src/utils/index.ts
@@ -1,6 +1,7 @@
 export * from './networkIdIncluded';
 export * from './addressValid';
 export * from './formatAddress';
+export * from './formatTokenAddress';
 export * from './getElementLendingValues';
 export * from './compareName';
 export * from './compareUsdValue';
