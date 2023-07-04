6acdd71077c3af238b0f1b861a94f80edd0fccb7
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2023-07-04 21:50:55 +0700

    fix fetcher

--- packages/core/src/Fetcher.ts
+++ packages/core/src/Fetcher.ts
@@ -1,6 +1,7 @@
 import { Context } from './Context';
 import { NetworkIdType } from './Network';
 import { PortfolioElement } from './Portfolio';
+import { formatAddress } from './utils';
 
 export type FetcherExecutor = (
   owner: string,
@@ -11,3 +12,12 @@ export type Fetcher = {
   networkId: NetworkIdType;
   executor: FetcherExecutor;
 };
+
+export function runFetcherExecutor(
+  fetcher: Fetcher,
+  owner: string,
+  context: Context
+) {
+  const fOwner = formatAddress(owner, fetcher.networkId);
+  return fetcher.executor(fOwner, context);
+}
