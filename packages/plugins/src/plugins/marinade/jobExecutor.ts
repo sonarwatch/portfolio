990e76fcc36883a28d86b4d8daf755b6562c16e2
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2023-07-05 08:29:44 +0700

    fix

--- packages/plugins/src/plugins/marinade/jobExecutor.ts
+++ packages/plugins/src/plugins/marinade/jobExecutor.ts
@@ -2,7 +2,7 @@ import { Cache, JobExecutor, NetworkId } from '@sonarwatch/portfolio-core';
 import { cachePrefix } from './constants';
 
 const jobExecutor: JobExecutor = async (cache: Cache) => {
-  await cache.setItem('abc', 'dfg', {
+  await cache.setItem('abc', 'def', {
     prefix: cachePrefix,
     networkId: NetworkId.solana,
   });
