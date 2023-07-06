55718b65448a86cd345dd283023d2bd2c533c9a3
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2023-07-06 11:01:11 +0700

    plugins

---
+++ packages/plugins/src/generators/fetcher/generator.ts
@@ -0,0 +1,43 @@
+import {
+  formatFiles,
+  generateFiles,
+  readProjectConfiguration,
+  Tree,
+} from '@nx/devkit';
+import { existsSync } from 'node:fs';
+import * as path from 'path';
+import { assetNetworkId } from '@sonarwatch/portfolio-core';
+import { FetcherGeneratorSchema } from './schema.d';
+import { toKebabCase } from '../helpers';
+
+export async function fetcherGenerator(
+  tree: Tree,
+  options: FetcherGeneratorSchema
+) {
+  const fetcherName = toKebabCase(options.fetcherName);
+  const pluginId = toKebabCase(options.pluginId);
+  const { networkId } = options;
+  assetNetworkId(networkId);
+
+  const libraryRoot = readProjectConfiguration(tree, 'plugins').root;
+
+  if (!existsSync(`${libraryRoot}/src/plugins/${pluginId}`)) {
+    throw new Error(`The provided plugin does not exist: ${pluginId}`);
+  }
+
+  const substitutions = {
+    pluginId,
+    fetcherName,
+    networkId,
+  };
+
+  generateFiles(
+    tree,
+    path.join(__dirname, 'files'),
+    libraryRoot,
+    substitutions
+  );
+  await formatFiles(tree);
+}
+
+export default fetcherGenerator;
---
+++ packages/plugins/src/generators/fetcher/schema.d.ts
@@ -0,0 +1,5 @@
+export interface FetcherGeneratorSchema {
+  fetcherName: string;
+  pluginId: string;
+  networkId: string;
+}
