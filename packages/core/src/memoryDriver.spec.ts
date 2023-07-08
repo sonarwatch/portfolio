d4ae9f7a496ad842f8c529626022769c097ee274
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2023-07-08 11:00:57 +0700

    add memoryDriver

---
+++ packages/core/src/memoryDriver.spec.ts
@@ -0,0 +1,21 @@
+import { createStorage } from 'unstorage';
+import memoryDriver from './memoryDriver';
+import sleep from './helpers/sleep';
+
+describe('MemoryDriver', () => {
+  it('should works', async () => {
+    const storage = createStorage({
+      driver: memoryDriver({
+        ttl: 500,
+      }),
+    });
+    const key = 'abc';
+    const value = 'def';
+    await storage.setItem(key, value);
+    const item = await storage.getItem(key);
+    expect(item).not.toBeNull();
+    await sleep(600);
+    const item2 = await storage.getItem(key);
+    expect(item2).toBeNull();
+  });
+});
---
+++ packages/core/src/memoryDriver.ts
@@ -0,0 +1,49 @@
+import { defineDriver } from 'unstorage';
+
+const DRIVER_NAME = 'memory';
+
+export interface MemoryStorageOptions {
+  ttl?: number;
+}
+
+export default defineDriver((opts: MemoryStorageOptions = {}) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const data = new Map<string, any>();
+  const { ttl } = opts;
+  return {
+    name: DRIVER_NAME,
+    options: {},
+    hasItem(key) {
+      return data.has(key);
+    },
+    getItem(key) {
+      return data.get(key) || null;
+    },
+    getItemRaw(key) {
+      return data.get(key) || null;
+    },
+    setItem(key, value) {
+      data.set(key, value);
+      if (ttl) {
+        setTimeout(() => {
+          data.delete(key);
+        }, ttl);
+      }
+    },
+    setItemRaw(key, value) {
+      data.set(key, value);
+    },
+    removeItem(key) {
+      data.delete(key);
+    },
+    getKeys() {
+      return Array.from(data.keys());
+    },
+    clear() {
+      data.clear();
+    },
+    dispose() {
+      data.clear();
+    },
+  };
+});
