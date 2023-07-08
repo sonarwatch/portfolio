86eddb7d8880b22b72d74d59abc8c2526b5a4108
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2023-07-08 16:31:06 +0700

    log

--- packages/plugins/src/utils/clients/getClientAptos.ts
+++ packages/plugins/src/utils/clients/getClientAptos.ts
@@ -4,6 +4,7 @@ import { rpcEnpoints } from './constants';
 
 export default function getClientAptos() {
   const rpcEndpoint = rpcEnpoints[NetworkId.aptos];
+  console.log('rpcEndpoint:', rpcEndpoint);
   return new AptosClient(rpcEndpoint.url, {
     USERNAME: rpcEndpoint.basicAuth?.username,
     PASSWORD: rpcEndpoint.basicAuth?.password,
