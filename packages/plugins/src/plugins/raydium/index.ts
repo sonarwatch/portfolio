f463e3b033bf7db5e7a15f79b7253e3e8de5bb2b
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2023-07-16 18:44:10 +0200

    remove raydium

--- packages/plugins/src/plugins/raydium/index.ts
+++ packages/plugins/src/plugins/raydium/index.ts
@@ -1,6 +1,8 @@
 import { Fetcher, Job } from '@sonarwatch/portfolio-core';
 import lpTokensJob from './lpTokensJob';
-import clmmPositionsFetcher from './clmmPositionsFetcher';
+// import clmmPositionsFetcher from './clmmPositionsFetcher';
 
 export const jobs: Job[] = [lpTokensJob];
-export const fetchers: Fetcher[] = [clmmPositionsFetcher];
+export const fetchers: Fetcher[] = [
+  // clmmPositionsFetcher
+];
