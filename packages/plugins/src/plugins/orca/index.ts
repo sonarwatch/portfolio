f4a0676d70f929233c927e6e11dbe8d80cdc63de
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2023-07-16 18:45:40 +0200

    remove orca

--- packages/plugins/src/plugins/orca/index.ts
+++ packages/plugins/src/plugins/orca/index.ts
@@ -1,7 +1,9 @@
 import { Fetcher, Job } from '@sonarwatch/portfolio-core';
 // import lpJob from './lpTokensJob';
 import whirlpoolJob from './whirlpoolsJob';
-import clmmPositionsFetcher from './clmmPositionsFetcher';
+// import clmmPositionsFetcher from './clmmPositionsFetcher';
 
 export const jobs: Job[] = [whirlpoolJob];
-export const fetchers: Fetcher[] = [clmmPositionsFetcher];
+export const fetchers: Fetcher[] = [
+  // clmmPositionsFetcher
+];
