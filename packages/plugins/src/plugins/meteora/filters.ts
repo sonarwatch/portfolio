852a353197552a845731b62235a1319c2c9dcc8f
Courmont Guillaume <courmontguillaume@gmail.com>
2023-07-17 15:49:09 +0200

    rename filter

--- packages/plugins/src/plugins/meteora/filters.ts
+++ packages/plugins/src/plugins/meteora/filters.ts
@@ -1,6 +1,6 @@
 import { GetProgramAccountsFilter } from '@solana/web3.js';
 import { vaultStruct } from './struct';
 
-export const aquafarmFilters: GetProgramAccountsFilter[] = [
+export const vaultsFilters: GetProgramAccountsFilter[] = [
   { dataSize: vaultStruct.byteSize },
 ];
