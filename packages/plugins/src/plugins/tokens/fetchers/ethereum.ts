bd6e5bb565be6968e9d66f72a1bf5873c1aa12a2
Courmont Guillaume <courmontguillaume@gmail.com>
2023-07-25 16:11:20 +0200

    fetch eth tokens based on tokenlist

---
+++ packages/plugins/src/plugins/tokens/fetchers/ethereum.ts
@@ -0,0 +1,98 @@
+import {
+  NetworkId,
+  PortfolioAssetToken,
+  PortfolioElementMultiple,
+  PortfolioElementType,
+  TokenPrice,
+  getUsdValueSum,
+} from '@sonarwatch/portfolio-core';
+import { getAddress } from 'viem';
+import { Cache } from '../../../Cache';
+import { Fetcher, FetcherExecutor } from '../../../Fetcher';
+import { walletTokensPlatform } from '../../../platforms';
+import { getEvmClients } from '../../../utils/clients';
+import runInBatch from '../../../utils/misc/runInBatch';
+import { erc20ABI } from '../../../utils/evm/erc20Abi';
+import tokenPriceToAssetToken from '../../../utils/misc/tokenPriceToAssetToken';
+import { TokenList } from '../types';
+
+const executor: FetcherExecutor = async (owner: string, cache: Cache) => {
+  const client = getEvmClients().ethereum;
+
+  const tokenList = await cache.getItem<TokenList>(NetworkId.ethereum, {
+    prefix: 'tokenList',
+  });
+  if (!tokenList || tokenList.tokens.length === 0) return [];
+
+  const tokensContracts = tokenList.tokens.map((token) => token.address);
+
+  const results = await runInBatch(
+    tokensContracts.map(
+      (contract) => () => cache.getTokenPrice(contract, NetworkId.ethereum)
+    )
+  );
+  const tokenPrices: TokenPrice[] = [];
+  results.forEach((r) => {
+    if (r.status === 'rejected') return;
+    if (!r.value) return;
+    tokenPrices.push(r.value);
+  });
+
+  const ownerEvmAddress = getAddress(owner);
+  const erc20CommonProperties = {
+    abi: erc20ABI,
+    functionName: 'balanceOf',
+    args: [ownerEvmAddress],
+  } as const;
+  const contracts = [];
+  for (const tokenPrice of tokenPrices) {
+    const address = getAddress(tokenPrice.address);
+    const contract = {
+      address,
+      ...erc20CommonProperties,
+    };
+    contracts.push(contract);
+  }
+  const contractsRes = await client.multicall({ contracts });
+
+  const walletTokensAssets: PortfolioAssetToken[] = [];
+  for (let index = 0; index < contractsRes.length; index++) {
+    const contractRes = contractsRes[index];
+    if (contractRes.status === 'failure') continue;
+    if (!contractRes.result) continue;
+
+    const tokenPrice = tokenPrices[index];
+    const { address } = tokenPrice;
+    const amount = Number(contractRes.result) / 10 ** tokenPrice.decimals;
+    if (amount === 0) continue;
+
+    const asset = tokenPriceToAssetToken(
+      address,
+      amount,
+      NetworkId.ethereum,
+      tokenPrice
+    );
+    walletTokensAssets.push(asset);
+  }
+  if (walletTokensAssets.length === 0) return [];
+
+  const element: PortfolioElementMultiple = {
+    type: PortfolioElementType.multiple,
+    networkId: NetworkId.ethereum,
+    platformId: walletTokensPlatform.id,
+    label: 'Wallet',
+    value: getUsdValueSum(walletTokensAssets.map((a) => a.value)),
+    data: {
+      assets: walletTokensAssets,
+    },
+  };
+  return [element];
+};
+
+const fetcher: Fetcher = {
+  id: `${walletTokensPlatform.id}-ethereum`,
+  networkId: NetworkId.ethereum,
+  executor,
+};
+
+export default fetcher;
--- packages/plugins/src/plugins/tokens/types.ts
+++ packages/plugins/src/plugins/tokens/types.ts
@@ -11,3 +11,30 @@ export type CoingeckoSimpleRes = Record<
     usd?: number;
   }
 >;
+
+export type TokenList = {
+  name: string;
+  timestamp: string;
+  version: {
+    major: number;
+    minor: number;
+    patch: number;
+  };
+  tags: object;
+  logoURI: string;
+  keywords: string[];
+  tokens: TokenListItem[];
+};
+
+export type TokenListItem = {
+  chainId: number;
+  address: string;
+  decimals: number;
+  name: string;
+  symbol: string;
+  logoURI: string;
+  tags?: string[];
+  extensions?: {
+    coingeckoId: string;
+  };
+};
