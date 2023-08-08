2422a0069a1c42d133b0ddc5644b9ca69c00f12e
Courmont Guillaume <courmontguillaume@gmail.com>
2023-08-08 10:12:42 +0200

    add solanaNative fectcher

--- packages/plugins/src/plugins/native-stake/index.ts
+++ packages/plugins/src/plugins/native-stake/index.ts
@@ -2,7 +2,8 @@ import { Job } from '../../Job';
 import { Fetcher } from '../../Fetcher';
 import suiFetcher from './suiFetcher';
 import aptosFetcher from './aptosFetcher';
+import solanaFetcher from './solanaFetcher';
 import aptosValidatorsJob from './activeValidatorsJob';
 
 export const jobs: Job[] = [aptosValidatorsJob];
-export const fetchers: Fetcher[] = [suiFetcher, aptosFetcher];
+export const fetchers: Fetcher[] = [suiFetcher, aptosFetcher, solanaFetcher];
