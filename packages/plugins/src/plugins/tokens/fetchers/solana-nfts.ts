22e22f6a84c05662aac2fc349b576ac577ec1910
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2023-08-11 18:10:29 +0200

    add images to nft solana fetcher

--- packages/plugins/src/plugins/tokens/fetchers/solana-nfts.ts
+++ packages/plugins/src/plugins/tokens/fetchers/solana-nfts.ts
@@ -7,6 +7,7 @@ import {
 } from '@sonarwatch/portfolio-core';
 import { PublicKey } from '@solana/web3.js';
 import {
+  Metadata,
   Metaplex,
   Token,
   isNftWithToken,
@@ -16,15 +17,69 @@ import BigNumber from 'bignumber.js';
 import { Fetcher, FetcherExecutor } from '../../../Fetcher';
 import { walletNftsPlatform, walletTokensPlatform } from '../../../platforms';
 import { getClientSolana } from '../../../utils/clients';
+import { Cache } from '../../../Cache';
+import runInBatch from '../../../utils/misc/runInBatch';
 
-const executor: FetcherExecutor = async (owner: string) => {
+const prefix = 'nft-images';
+const noImageValue = 'noimage';
+
+const executor: FetcherExecutor = async (owner: string, cache: Cache) => {
   const client = getClientSolana();
   const ownerPubKey = new PublicKey(owner);
   const metaplex = new Metaplex(client);
-  if (!owner) return [];
+
   const outputs = await metaplex.nfts().findAllByOwner({
     owner: ownerPubKey,
   });
+  const cachedImages = await cache.getItems<string>(
+    outputs.map((o) =>
+      o.model === 'metadata'
+        ? o.mintAddress.toString()
+        : o.mint.address.toString()
+    ),
+    {
+      prefix,
+      networkId: NetworkId.solana,
+    }
+  );
+  const images: Map<string, string> = new Map();
+  const missings: Metadata[] = [];
+  outputs.forEach((o, i) => {
+    const cachedImage = cachedImages[i];
+    if (cachedImage) {
+      const address =
+        o.model === 'metadata'
+          ? o.mintAddress.toString()
+          : o.mint.address.toString();
+      images.set(address, cachedImage);
+    } else if (o.model === 'metadata') {
+      missings.push(o);
+    }
+  });
+
+  const res = await runInBatch(
+    missings.map((o) => () => metaplex.nfts().load({ metadata: o })),
+    5
+  );
+
+  const promises = res.map((r, i) => {
+    const image = r.status !== 'rejected' ? r.value.json?.image : undefined;
+    if (r.status === 'rejected' || !image) {
+      const address = missings[i].mintAddress.toString();
+      return cache.setItem(address, noImageValue, {
+        prefix,
+        networkId: NetworkId.solana,
+      });
+    }
+    const address = r.value.mint.address.toString();
+    images.set(address, image);
+    return cache.setItem(address, image, {
+      prefix,
+      networkId: NetworkId.solana,
+    });
+  });
+  await Promise.allSettled(promises);
+
   const assets: PortfolioAssetCollectible[] = outputs.map((output) => {
     const address =
       output.model === 'metadata'
@@ -40,6 +95,9 @@ const executor: FetcherExecutor = async (owner: string) => {
           .toNumber()
       : 1;
 
+    let image = images.get(address);
+    if (image === noImageValue) image = undefined;
+
     return {
       networkId: NetworkId.solana,
       type: PortfolioAssetType.collectible,
@@ -51,6 +109,7 @@ const executor: FetcherExecutor = async (owner: string) => {
         price: null,
         value: null,
         floorPrice: null,
+        image,
         name: output.name,
         collectionId: output.collection?.address.toString(),
       },
