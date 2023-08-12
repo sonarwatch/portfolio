7af39c19ffefe6fc183bb500a5cf5b33aafff218
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2023-08-12 15:43:20 +0200

    linting

--- packages/plugins/src/plugins/saber/index.ts
+++ packages/plugins/src/plugins/saber/index.ts
@@ -1,5 +1,6 @@
 import { Fetcher } from '../../Fetcher';
 import { Job } from '../../Job';
 import lpTokensJob from './lpTokensJob';
+
 export const jobs: Job[] = [lpTokensJob];
 export const fetchers: Fetcher[] = [];
--- packages/plugins/src/plugins/saber/lpTokensJob.ts
+++ packages/plugins/src/plugins/saber/lpTokensJob.ts
@@ -1,45 +1,46 @@
 import { PublicKey } from "@solana/web3.js";
 import axios from "axios";
+import { NetworkId, TokenPrice, TokenPriceUnderlying } from "@sonarwatch/portfolio-core";
+import { BN } from "bn.js";
+import BigNumber from "bignumber.js";
 import { Cache } from "../../Cache";
 import { Job, JobExecutor } from "../../Job";
 import { getClientSolana } from "../../utils/clients";
 import { platformId, SABER_SWAPS } from "./constants";
 import { getMultipleAccountsInfoSafe } from '../../utils/solana/getMultipleAccountsInfoSafe';
-import { tokenAccountStruct } from "../../utils/solana";
-import { mintAccountStruct } from "../../utils/solana";
+import { tokenAccountStruct , mintAccountStruct } from "../../utils/solana";
+
 import runInBatch from "../../utils/misc/runInBatch";
-import { NetworkId, TokenPrice, TokenPriceUnderlying } from "@sonarwatch/portfolio-core";
-import { BN } from "bn.js";
-import BigNumber from "bignumber.js";
+
 const executor: JobExecutor = async (cache: Cache) => {
     const client = getClientSolana();
-    let swaps = (
-        await axios.get(SABER_SWAPS).catch(() => { return {data: []} })
+    const swaps = (
+        await axios.get(SABER_SWAPS).catch(() => ({data: []}))
       ).data;
-    let accounts = [];
+    const accounts = [];
     for(let i = 0; i < swaps.length; i++) {
-        let reserveA = swaps[i].addresses.reserves[0];
-        let reserveB = swaps[i].addresses.reserves[1];
-        let lpMint = swaps[i].addresses.lpTokenMint;
+        const reserveA = swaps[i].addresses.reserves[0];
+        const reserveB = swaps[i].addresses.reserves[1];
+        const lpMint = swaps[i].addresses.lpTokenMint;
 
         accounts.push(new PublicKey(reserveA));
         accounts.push(new PublicKey(reserveB));
         accounts.push(new PublicKey(lpMint));
     }
 
-    let accountInfos = await getMultipleAccountsInfoSafe(client, accounts);
+    const accountInfos = await getMultipleAccountsInfoSafe(client, accounts);
 
-    let mints: PublicKey[] = [];
+    const mints: PublicKey[] = [];
     
     for(let i=0; i<swaps.length; i++) {
-        let reserveAInfo = accountInfos[i * 3];
-        let reserveBInfo = accountInfos[i * 3 + 1];
-        let lpMintInfo = accountInfos[i * 3 + 2];
+        const reserveAInfo = accountInfos[i * 3];
+        const reserveBInfo = accountInfos[i * 3 + 1];
+        const lpMintInfo = accountInfos[i * 3 + 2];
 
         if(!reserveAInfo || !reserveBInfo || !lpMintInfo) continue;
 
-        let reserveA = tokenAccountStruct.deserialize(reserveAInfo.data)[0];
-        let reserveB = tokenAccountStruct.deserialize(reserveBInfo.data)[0];
+        const reserveA = tokenAccountStruct.deserialize(reserveAInfo.data)[0];
+        const reserveB = tokenAccountStruct.deserialize(reserveBInfo.data)[0];
         mints.push(reserveA.mint);
         mints.push(reserveB.mint);
     }
@@ -54,27 +55,27 @@ const executor: JobExecutor = async (cache: Cache) => {
         if (!r.value) return;
         tokenPrices.set(r.value.address, r.value);
     });
-    let counter = 0;
+    const counter = 0;
     for(let i=0; i<swaps.length; i++) {
-        let reserveAInfo = accountInfos[i * 3];
-        let reserveBInfo = accountInfos[i * 3 + 1];
-        let lpMintInfo = accountInfos[i * 3 + 2];
+        const reserveAInfo = accountInfos[i * 3];
+        const reserveBInfo = accountInfos[i * 3 + 1];
+        const lpMintInfo = accountInfos[i * 3 + 2];
 
         if(!reserveAInfo || !reserveBInfo || !lpMintInfo) continue;
 
-        let reserveA = tokenAccountStruct.deserialize(reserveAInfo.data)[0];
-        let reserveB = tokenAccountStruct.deserialize(reserveBInfo.data)[0];
-        let lpMint = mintAccountStruct.deserialize(lpMintInfo.data)[0];
+        const reserveA = tokenAccountStruct.deserialize(reserveAInfo.data)[0];
+        const reserveB = tokenAccountStruct.deserialize(reserveBInfo.data)[0];
+        const lpMint = mintAccountStruct.deserialize(lpMintInfo.data)[0];
 
-        let priceA = tokenPrices.get(reserveA.mint.toBase58());
-        let priceB = tokenPrices.get(reserveB.mint.toBase58());
+        const priceA = tokenPrices.get(reserveA.mint.toBase58());
+        const priceB = tokenPrices.get(reserveB.mint.toBase58());
 
         if(!priceA || !priceB) continue;
         
-        let coinHoldings = reserveA.amount.multipliedBy(priceA.price).div(10 ** priceA.decimals);
-        let pcHoldings = reserveB.amount.multipliedBy(priceB.price).div(10 ** priceB.decimals);
+        const coinHoldings = reserveA.amount.multipliedBy(priceA.price).div(10 ** priceA.decimals);
+        const pcHoldings = reserveB.amount.multipliedBy(priceB.price).div(10 ** priceB.decimals);
 
-        let lpTokenPrice = coinHoldings.plus(pcHoldings).div(lpMint.supply).div(10 ** lpMint.decimals);
+        const lpTokenPrice = coinHoldings.plus(pcHoldings).div(lpMint.supply).div(10 ** lpMint.decimals);
 
         const underlyings: TokenPriceUnderlying[] = [];
         underlyings.push({
