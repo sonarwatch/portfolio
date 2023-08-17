b444d490614799e184474d50012f7cfe441644f3
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2023-08-17 08:28:39 +0200

    add sdai

---
+++ packages/plugins/src/plugins/maker/constants.ts
@@ -0,0 +1,9 @@
+export const platformId = 'maker';
+
+export const daiAddress = '0x6B175474E89094C44Da98b954EedeAC495271d0F';
+export const daiDecimals = 18;
+export const sDaiAddress = '0x83F20F44975D03b1b09e64809B757c47f942BEeA';
+export const sDaiDecimals = 18;
+export const chaiAddress = '0x06AF07097C9Eeb7fD685c692751D5C66dB49c215';
+export const chaiDecimals = 18;
+export const potAddress = '0x197E90f9FAD81970bA7976f33CbD77088E5D7cf7';
---
+++ packages/plugins/src/plugins/maker/index.ts
@@ -0,0 +1,6 @@
+import { Fetcher } from '../../Fetcher';
+import { Job } from '../../Job';
+import sdaiJob from './sdaiJob';
+
+export const jobs: Job[] = [sdaiJob];
+export const fetchers: Fetcher[] = [];
