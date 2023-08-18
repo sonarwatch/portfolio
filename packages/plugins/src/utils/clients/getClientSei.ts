43ac6b2e4f53a4b058163e05d0fe5c6a1c32c2df
Courmont Guillaume <courmontguillaume@gmail.com>
2023-08-18 20:37:24 +0200

    clean

--- packages/plugins/src/utils/clients/getClientSei.ts
+++ packages/plugins/src/utils/clients/getClientSei.ts
@@ -1,26 +1,11 @@
 import { seiprotocol } from '@sei-js/proto';
 
 import { NetworkId } from '@sonarwatch/portfolio-core';
-import { AptosClient } from 'aptos';
-import { getRpcEndpoint, getUrlEndpoint } from './constants';
-import { getBasicAuthHeaders } from '../misc/getBasicAuthHeaders';
+import { getUrlEndpoint } from './constants';
 
 export default function getClientSei() {
   const urlEndpoint = getUrlEndpoint(NetworkId.sei);
   return seiprotocol.ClientFactory.createLCDClient({
     restEndpoint: urlEndpoint,
-  });
-}
-
-export function getClientAptos() {
-  const rpcEndpoint = getRpcEndpoint(NetworkId.aptos);
-  const headers = rpcEndpoint.basicAuth
-    ? getBasicAuthHeaders(
-        rpcEndpoint.basicAuth?.username,
-        rpcEndpoint.basicAuth?.password
-      )
-    : undefined;
-  return new AptosClient(rpcEndpoint.url, {
-    HEADERS: headers,
   });
 }
