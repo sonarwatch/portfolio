43bc316cde5737f19a8c995813ac849f200ca626
Courmont Guillaume <courmontguillaume@gmail.com>
2023-08-23 19:52:27 +0200

    remove serum/openBook orders

--- packages/plugins/src/plugins/orders/index.ts
+++ packages/plugins/src/plugins/orders/index.ts
@@ -1,48 +1,48 @@
-import { NetworkId } from '@sonarwatch/portfolio-core';
 import { Fetcher } from '../../Fetcher';
 import { Job } from '../../Job';
 import jupiterFetcher from './jupiter/limitFetcher';
-import openbookFetcher from './clobs-solana/openbookFetcher';
-import { clobVersions } from './clobs-solana/constants';
-import getMarketJobExecutor from './clobs-solana/MarketJobExecutorGenerator';
-import { platformId } from './constants';
-import getSerumFetcherExecutor from './clobs-solana/serumFetcherExecutorGenerator';
-import { serumPlatform } from '../../platforms';
+// import { NetworkId } from '@sonarwatch/portfolio-core';
+// import openbookFetcher from './clobs-solana/openbookFetcher';
+// import { clobVersions } from './clobs-solana/constants';
+// import getMarketJobExecutor from './clobs-solana/MarketJobExecutorGenerator';
+// import { platformId } from './constants';
+// import getSerumFetcherExecutor from './clobs-solana/serumFetcherExecutorGenerator';
+// import { serumPlatform } from '../../platforms';
 
 export const jobs: Job[] = [
-  {
-    id: `${platformId}-markets-openbookV1`,
-    executor: getMarketJobExecutor(clobVersions.openbookV1),
-  },
-  {
-    id: `${platformId}-markets-serumV3`,
-    executor: getMarketJobExecutor(clobVersions.serumV3),
-  },
-  {
-    id: `${platformId}-markets-serumV2`,
-    executor: getMarketJobExecutor(clobVersions.serumV2),
-  },
-  {
-    id: `${platformId}-markets-serumV1`,
-    executor: getMarketJobExecutor(clobVersions.serumV1),
-  },
+  // {
+  //   id: `${platformId}-markets-openbookV1`,
+  //   executor: getMarketJobExecutor(clobVersions.openbookV1),
+  // },
+  // {
+  //   id: `${platformId}-markets-serumV3`,
+  //   executor: getMarketJobExecutor(clobVersions.serumV3),
+  // },
+  // {
+  //   id: `${platformId}-markets-serumV2`,
+  //   executor: getMarketJobExecutor(clobVersions.serumV2),
+  // },
+  // {
+  //   id: `${platformId}-markets-serumV1`,
+  //   executor: getMarketJobExecutor(clobVersions.serumV1),
+  // },
 ];
 export const fetchers: Fetcher[] = [
-  openbookFetcher,
   jupiterFetcher,
-  {
-    id: `${platformId}-${serumPlatform.id}V3`,
-    executor: getSerumFetcherExecutor(clobVersions.serumV3),
-    networkId: NetworkId.solana,
-  },
-  {
-    id: `${platformId}-${serumPlatform.id}V2`,
-    executor: getSerumFetcherExecutor(clobVersions.serumV2),
-    networkId: NetworkId.solana,
-  },
-  {
-    id: `${platformId}-${serumPlatform.id}V1`,
-    executor: getSerumFetcherExecutor(clobVersions.serumV1),
-    networkId: NetworkId.solana,
-  },
+  // openbookFetcher,
+  // {
+  //   id: `${platformId}-${serumPlatform.id}V3`,
+  //   executor: getSerumFetcherExecutor(clobVersions.serumV3),
+  //   networkId: NetworkId.solana,
+  // },
+  // {
+  //   id: `${platformId}-${serumPlatform.id}V2`,
+  //   executor: getSerumFetcherExecutor(clobVersions.serumV2),
+  //   networkId: NetworkId.solana,
+  // },
+  // {
+  //   id: `${platformId}-${serumPlatform.id}V1`,
+  //   executor: getSerumFetcherExecutor(clobVersions.serumV1),
+  //   networkId: NetworkId.solana,
+  // },
 ];
