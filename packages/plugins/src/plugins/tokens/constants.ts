156b9a93e9a3e3c472898e131f192fd8705d0452
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2023-08-28 11:57:21 +0200

    add tokenListsDetails

--- packages/plugins/src/plugins/tokens/constants.ts
+++ packages/plugins/src/plugins/tokens/constants.ts
@@ -1,6 +1,7 @@
 import { walletTokensPlatform } from '../../platforms';
 
 export const tokenListsPrefix = 'tokenlists';
+export const tokenListsDetailsPrefix = 'tokenlistsdetails';
 export const nIdsToFetch = 50;
 
 export const platformId = walletTokensPlatform.id;
