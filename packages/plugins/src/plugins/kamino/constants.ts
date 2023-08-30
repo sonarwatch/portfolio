c5217e74d330f9cd46a049d210ec48fce5bf2a28
Courmont Guillaume <courmontguillaume@gmail.com>
2023-08-30 13:33:10 +0200

    add kaminoPlatform info

--- packages/plugins/src/plugins/kamino/constants.ts
+++ packages/plugins/src/plugins/kamino/constants.ts
@@ -1,7 +1,12 @@
 import { PublicKey } from '@solana/web3.js';
+import { Platform } from '@sonarwatch/portfolio-core';
 
 export const platformId = 'kamino';
-
+export const driftPlatform: Platform = {
+  id: platformId,
+  name: 'Kamino',
+  image: `https://alpha.sonar.watch/img/platforms/${platformId}.png`,
+};
 export const programId = new PublicKey(
   '6LtLpnUFNByNXLyCoK9wA2MykKAmQNZKBdY8s47dehDc'
 );
