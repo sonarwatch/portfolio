c580645707465cc2c4afa70fea3f244f6f41e796
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2023-08-31 12:01:48 +0200

    add getEvmEthersClient

--- packages/plugins/src/utils/clients/index.ts
+++ packages/plugins/src/utils/clients/index.ts
@@ -6,5 +6,6 @@ export { default as getClientCosmos } from './getClientCosmos';
 export { default as getClientIbc } from './getClientIbc';
 export { default as getClientCosmWasm } from './getClientCosmWasm';
 export { default as getEvmClient } from './getEvmClient';
+export { default as getEvmEthersClient } from './getEvmEthersClient';
 export { default as getEvmClients } from './getEvmClients';
 export { default as urlToRpcEndpoint } from './urlToRpcEndpoint';
