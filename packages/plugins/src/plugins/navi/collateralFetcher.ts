6c61955e34d7a9afaa511c3c5e181ab64420d4e8
Courmont Guillaume <courmontguillaume@gmail.com>
2023-08-31 14:53:11 +0200

    ahead

--- packages/plugins/src/plugins/navi/collateralFetcher.ts
+++ packages/plugins/src/plugins/navi/collateralFetcher.ts
@@ -2,9 +2,16 @@ import { NetworkId, PortfolioElement } from '@sonarwatch/portfolio-core';
 import { Cache } from '../../Cache';
 import { Fetcher, FetcherExecutor } from '../../Fetcher';
 import { platformId } from './constants';
+import { getClientSui } from '../../utils/clients';
 
 const executor: FetcherExecutor = async (owner: string, cache: Cache) => {
-  // do some stuff
+  const client = getClientSui();
+
+  const infos = client.getDynamicFieldObject({
+    parentId:
+      '0xb8c5eab02a0202f638958cc79a69a2d30055565caad1684b3c8bbca3bddcb322',
+    name: '',
+  });
   const elements: PortfolioElement[] = [];
   return elements;
 };
