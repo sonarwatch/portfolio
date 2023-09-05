eda44c034c1287d8dcc17ae29067b690d6fa6f24
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2023-09-05 09:21:18 +0200

    add getAddressSystemOrFail

--- packages/core/src/utils/validAddress.ts
+++ packages/core/src/utils/validAddress.ts
@@ -67,6 +67,15 @@ export function getAddressSystem(address: string): AddressSystemType | null {
   return null;
 }
 
+export function getAddressSystemOrFail(
+  address: string
+): AddressSystemType | null {
+  for (const [addressSystem, validator] of Object.entries(validators)) {
+    if (validator(address)) return addressSystem as AddressSystemType;
+  }
+  throw new Error(`Address is not valid: ${address}`);
+}
+
 export function isAddress(
   address: string,
   addressSystem: AddressSystemType
