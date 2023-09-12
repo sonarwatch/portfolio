4267f7e64dcba762a8f732d1249964c374d4a3d1
nicetomeetyou1 <han@scallop.io>
2023-09-12 23:33:34 +0700

    feat: add helper

---
+++ packages/plugins/src/plugins/scallop/helpers.ts
@@ -0,0 +1,84 @@
+import { NetworkId } from "@sonarwatch/portfolio-core"
+import { CoinMetadata, PaginatedObjectsResponse, SuiObjectData, SuiObjectResponseQuery, normalizeStructTag } from "@mysten/sui.js";
+import { Cache } from "../../Cache"
+import { addressId, addressPrefix } from "./constants"
+import { AddressInfo, Coin, CoinTypeMetadata } from "./type";
+import { getClientSui } from "../../utils/clients";
+
+const SUI_ID = '0x0000000000000000000000000000000000000000000000000000000000000002';
+const client = getClientSui();
+
+export async function getCoinTypeMetadata(cache: Cache): Promise<{ [T in string]: CoinTypeMetadata }> {
+    const coinTypes: { [T in string]: CoinTypeMetadata } = {};
+    const address = await cache.getItem(addressId, {
+        prefix: addressPrefix,
+        networkId: NetworkId.sui
+    }) as AddressInfo;
+    const coins = new Map<string, Coin>(Object.entries(address.mainnet.core.coins));
+    const coinNames: string[] = [...coins.keys()];
+    for (const coinName of coinNames) {
+        const detail = coins.get(coinName);
+        if (detail?.id === SUI_ID) {
+            const coinType = `${SUI_ID}::sui::SUI`;
+            coinTypes[coinName] = {
+                coinType,
+                metadata: await client.getCoinMetadata({ coinType })
+            }
+        } else {
+            const object = await client.getObject({
+                id: detail!.metaData,
+                options: {
+                    showType: true,
+                    showContent: true,
+                }
+            });
+            if (object.data && object.data.content && 'fields' in object.data.content) {
+                const structTag = object.data.content.type
+                const coinType = getCoinType(structTag)
+                coinTypes[coinName] = {
+                    coinType,
+                    metadata: object.data.content.fields as CoinMetadata
+                };
+            }
+        }
+    }
+    return coinTypes;
+}
+
+export async function getOwnerObject(owner: string, query?: SuiObjectResponseQuery) {
+    const objects: SuiObjectData[] = [];
+    let cursor:
+        | string
+        | {
+            objectId: string;
+            atCheckpoint?: number | undefined;
+        }
+        | null = null;
+
+    do {
+        const { data, nextCursor }: PaginatedObjectsResponse = await client.getOwnedObjects({
+            owner,
+            filter: query?.filter,
+            options: query?.options ?? {
+                showType: true,
+                showContent: true,
+                showDisplay: true,
+            },
+            cursor,
+        });
+
+        if (!data || !data.length) {
+            break;
+        }
+        const objectDatas = data.map((obj) => obj.data) as SuiObjectData[];
+        objects.push(...objectDatas);
+        cursor = nextCursor;
+    } while (cursor);
+
+    return objects;
+}
+
+export function getCoinType(type: string) {
+    const structTag = normalizeStructTag(type);
+    return structTag.substring(structTag.indexOf('<') + 1, structTag.indexOf('>'));
+}
