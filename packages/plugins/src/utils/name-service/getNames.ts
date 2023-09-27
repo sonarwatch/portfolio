09a70cc2cadbf1f559fcb92951ce80c60b58ad63
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2023-09-27 14:45:08 +0200

    fix

--- packages/plugins/src/utils/name-service/getNames.ts
+++ packages/plugins/src/utils/name-service/getNames.ts
@@ -27,7 +27,7 @@ export async function getNames(
         }))
       );
     case AddressSystem.move:
-      return [...(await getNamesAptos(fAddress))];
+      return getNamesAptos(fAddress);
     default:
       return [];
   }
