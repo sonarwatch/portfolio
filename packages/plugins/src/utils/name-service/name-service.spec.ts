115e9bcf74c97df7da17a07d27cde7ac70b52243
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2023-09-27 14:38:02 +0200

    fix

--- packages/plugins/src/utils/name-service/name-service.spec.ts
+++ packages/plugins/src/utils/name-service/name-service.spec.ts
@@ -8,19 +8,19 @@ describe('name-service', () => {
     expect(owner).toBe('0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045');
   });
 
-  it('should works for sui', async () => {
+  it('should works for aptos', async () => {
     const owner = await getOwnerAptos('geninsus.apt');
     expect(owner).toBe(
       '0xaa3fca2b46efb0c9b63e9c92ee31a28b9f22ca52a36967151416706f2ca138c6'
     );
   });
 
-  it('should get owner', async () => {
+  it('should get owner with .eth', async () => {
     const owner = await getOwner('vitalik.eth');
     expect(owner.address).toBe('0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045');
   });
 
-  it('should get owner', async () => {
+  it('should get owner with .apt', async () => {
     const owner = await getOwner('geninsus.apt');
     expect(owner.address).toBe(
       '0xaa3fca2b46efb0c9b63e9c92ee31a28b9f22ca52a36967151416706f2ca138c6'
