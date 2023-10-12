89f38b002508f6e9d91efe256809ec47fc4085e4
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2023-10-12 17:30:37 +0200

    fix foo

--- packages/plugins/src/plugins/foo/constants.ts
+++ packages/plugins/src/plugins/foo/constants.ts
@@ -1,2 +1,8 @@
+import { Platform } from '@sonarwatch/portfolio-core';
+
 export const platformId = 'foo';
+export const fooPlatform: Platform = {
+  id: platformId,
+  name: 'Foo Finanace',
+};
 export const marketsCachePrefix = `${platformId}-markets`;
