affb47516dccb773fa2eddfc33b8b23e25bfed3c
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2023-10-13 19:01:12 +0200

    fix foo

--- packages/plugins/src/plugins/foo/constants.ts
+++ packages/plugins/src/plugins/foo/constants.ts
@@ -4,5 +4,7 @@ export const platformId = 'foo';
 export const fooPlatform: Platform = {
   id: platformId,
   name: 'Foo Finanace',
+  image: 'https://sonar.watch/img/platforms/foo.png',
+  defiLlamaId: 'foo-finance',
 };
 export const marketsCachePrefix = `${platformId}-markets`;
