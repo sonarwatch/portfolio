10a4b3a57a1fe2048a5849e36938922cddc0b519
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2023-10-15 10:37:30 +0200

    fix

--- packages/plugins/src/plugins/curve/marketsJob.ts
+++ packages/plugins/src/plugins/curve/marketsJob.ts
@@ -1,5 +1,4 @@
 import { NetworkId, ethereumNetwork } from '@sonarwatch/portfolio-core';
-import curve from '@curvefi/api';
 import { Cache } from '../../Cache';
 import { Job, JobExecutor } from '../../Job';
 import { marketsCachePrefix, platformId } from './constants';
@@ -8,16 +7,7 @@ import { getRpcEndpoint, getUrlEndpoint } from '../../utils/clients/constants';
 
 const executor: JobExecutor = async (cache: Cache) => {
   const network = ethereumNetwork;
-  const endpoint = getUrlEndpoint(network.id);
-
-  await curve.init('JsonRpc', { url: endpoint }, { chainId: network.chainId });
-  await curve.factory.fetchPools();
-  await curve.crvUSDFactory.fetchPools();
-  await curve.EYWAFactory.fetchPools();
-  await curve.cryptoFactory.fetchPools();
-  await curve.tricryptoFactory.fetchPools();
-  const res = curve.getPoolList();
-  console.log('res:', res);
+  // const endpoint = getUrlEndpoint(network.id);
 
   // await cache.setItem('market_id', 'market_object', {
   //   prefix: marketsCachePrefix,
