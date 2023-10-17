92f6028b77894e6253b2427bc8247ec797ac22b4
Courmont Guillaume <courmontguillaume@gmail.com>
2023-10-17 11:21:39 +0200

    add blRates for NAVI

--- packages/plugins/src/plugins/navi/collateralFetcher.ts
+++ packages/plugins/src/plugins/navi/collateralFetcher.ts
@@ -21,8 +21,8 @@ import { getClientSui } from '../../utils/clients';
 import tokenPriceToAssetToken from '../../utils/misc/tokenPriceToAssetToken';
 import { Balance, ReserveData } from './types';
 
-const indexFactor = 18;
-const rateFactor = 27;
+export const indexFactor = 18;
+export const rateFactor = 27;
 
 const executor: FetcherExecutor = async (owner: string, cache: Cache) => {
   const client = getClientSui();
