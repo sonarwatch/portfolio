b7f1a4cbabfd04a7a31fd56876647aca93e1fcb0
Courmont Guillaume <courmontguillaume@gmail.com>
2023-10-27 13:51:41 +0200

    rename

--- packages/plugins/src/plugins/paraswap/index.ts
+++ packages/plugins/src/plugins/paraswap/index.ts
@@ -2,10 +2,10 @@ import { Platform } from '@sonarwatch/portfolio-core';
 import { Fetcher } from '../../Fetcher';
 import { Job } from '../../Job';
 import stakingFetcher from './stakingFetcher';
-import bptFarmingFetcher from './poolsFetcher';
+import poolsFetcher from './poolsFetcher';
 import bptInfoJob from './bptInfoJob';
 import { paraswapPlatform } from './constants';
 
 export const jobs: Job[] = [bptInfoJob];
-export const fetchers: Fetcher[] = [stakingFetcher, bptFarmingFetcher];
+export const fetchers: Fetcher[] = [stakingFetcher, poolsFetcher];
 export const platforms: Platform[] = [paraswapPlatform];
