bd0f67dc9f96a607938de7da441bcd52fa9213db
Courmont Guillaume <courmontguillaume@gmail.com>
2023-11-07 15:43:01 +0100

    add flexlend

---
+++ packages/plugins/src/plugins/flexlend/constants.ts
@@ -0,0 +1,14 @@
+import { PublicKey } from '@solana/web3.js';
+import { Platform } from '@sonarwatch/portfolio-core';
+
+export const platformId = 'flexlend';
+export const flexlendPlatform: Platform = {
+  id: platformId,
+  name: 'FlexLend',
+  image: 'https://sonar.watch/img/platforms/flexlend.png',
+  defiLlamaId: 'flexlend',
+};
+
+export const flexProgramId = new PublicKey(
+  'FL3X2pRsQ9zHENpZSKDRREtccwJuei8yg9fwDu9UN69Q'
+);
