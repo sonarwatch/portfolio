cb1385cf7d20103553ddbca686c1826322e0f900
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2023-11-11 10:56:41 +0100

    update

--- packages/plugins/src/utils/misc/constants.ts
+++ packages/plugins/src/utils/misc/constants.ts
@@ -1,1 +1,4 @@
+import BigNumber from 'bignumber.js';
+
 export const zeroBigInt = BigInt(0);
+export const zeroBigNumber = BigNumber(0);
--- packages/plugins/src/utils/misc/getSourceWeight.ts
+++ packages/plugins/src/utils/misc/getSourceWeight.ts
@@ -1,6 +1,7 @@
 import BigNumber from 'bignumber.js';
+import { zeroBigNumber } from './constants';
 
-const usdValueForWeightOfOne = new BigNumber(10).pow(9);
+const usdValueForWeightOfOne = new BigNumber(10 ** 9);
 /**
  * Get the weight of a new TokenPriceSource depending on the size of the pool in USD$ value
  *
@@ -9,7 +10,8 @@ const usdValueForWeightOfOne = new BigNumber(10).pow(9);
  * @returns weight between 0 and 1
  */
 export default function getSourceWeight(poolUsdValue: BigNumber): number {
-  if (poolUsdValue.isGreaterThan(usdValueForWeightOfOne)) return 1;
+  if (poolUsdValue.isGreaterThanOrEqualTo(usdValueForWeightOfOne)) return 1;
+  if (poolUsdValue.isLessThanOrEqualTo(zeroBigNumber)) return 0;
   return poolUsdValue
     .dividedBy(usdValueForWeightOfOne)
     .decimalPlaces(3, BigNumber.ROUND_CEIL)
