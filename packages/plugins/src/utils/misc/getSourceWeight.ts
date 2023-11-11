a683b24be5b1f02c667ca1ed80a80513c676fd3d
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2023-11-11 17:06:45 +0100

    fix getSourceWeight

--- packages/plugins/src/utils/misc/getSourceWeight.ts
+++ packages/plugins/src/utils/misc/getSourceWeight.ts
@@ -1,19 +1,23 @@
 import BigNumber from 'bignumber.js';
 import { zeroBigNumber } from './constants';
 
-const usdValueForWeightOfOne = new BigNumber(10 ** 9);
 /**
  * Get the weight of a new TokenPriceSource depending on the size of the pool in USD$ value
  *
- * @param poolUsdValue Total value in USD$ of the pool
+ * @param {BigNumber | number} poolTvl Pool total value locked (in USD$)
+ * @param {number} [trustValue=1000000000] Value (in USD$) from which the weight is 1
  *
  * @returns weight between 0 and 1
  */
-export default function getSourceWeight(poolUsdValue: BigNumber): number {
-  if (poolUsdValue.isGreaterThanOrEqualTo(usdValueForWeightOfOne)) return 1;
-  if (poolUsdValue.isLessThanOrEqualTo(zeroBigNumber)) return 0;
-  return poolUsdValue
-    .dividedBy(usdValueForWeightOfOne)
+export default function getSourceWeight(
+  poolTvl: BigNumber | number,
+  trustValue = 1000000000
+): number {
+  const fPoolTvl = new BigNumber(poolTvl);
+  if (fPoolTvl.isGreaterThanOrEqualTo(trustValue)) return 1;
+  if (fPoolTvl.isLessThanOrEqualTo(zeroBigNumber)) return 0;
+  return fPoolTvl
+    .dividedBy(trustValue)
     .decimalPlaces(3, BigNumber.ROUND_CEIL)
     .toNumber();
 }
