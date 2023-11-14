313c0f6a85060fc3f718468a283775e94e93a009
Courmont Guillaume <courmontguillaume@gmail.com>
2023-11-14 15:25:50 +0100

    remove redeemV3 mango

--- packages/plugins/src/plugins/mango/index.ts
+++ packages/plugins/src/plugins/mango/index.ts
@@ -4,9 +4,8 @@ import { Job } from '../../Job';
 import banksJob from './banksJob';
 import groupsJob from './groupsJob';
 import collateralFetcher from './collateralFetcher';
-import redeemFetcher from './redeemFetcher';
 import { mangoPlatform } from './constants';
 
 export const platforms: Platform[] = [mangoPlatform];
 export const jobs: Job[] = [banksJob, groupsJob];
-export const fetchers: Fetcher[] = [collateralFetcher, redeemFetcher];
+export const fetchers: Fetcher[] = [collateralFetcher];
