83a3ffd57fb60a8597e99af957f86d20f0b88faf
Courmont Guillaume <courmontguillaume@gmail.com>
2023-11-14 15:37:37 +0100

    eslint

--- packages/plugins/src/utils/name-service/services/allDomains.ts
+++ packages/plugins/src/utils/name-service/services/allDomains.ts
@@ -1,12 +1,12 @@
 import { allDomainsNameChecker } from '@sonarwatch/portfolio-core';
+import { TldParser } from '@onsol/tldparser';
 import { getClientSolana } from '../../clients';
 import { NameService } from '../types';
-import { TldParser } from '@onsol/tldparser';
 
 async function getOwner(name: string): Promise<string | null> {
   const client = getClientSolana();
   const parser = new TldParser(client);
-  const owner = await parser.getOwnerFromDomainTld(name)
+  const owner = await parser.getOwnerFromDomainTld(name);
   if (owner) return owner.toString();
   return null;
 }
@@ -15,9 +15,13 @@ async function getNames(address: string): Promise<string[]> {
   const client = getClientSolana();
 
   const parser = new TldParser(client);
-  const allDomainsWithNameAccounts = await parser.getParsedAllUserDomains(address).catch(() => undefined)
+  const allDomainsWithNameAccounts = await parser
+    .getParsedAllUserDomains(address)
+    .catch(() => undefined);
   if (!allDomainsWithNameAccounts) return [];
-  return allDomainsWithNameAccounts.map((domainsWithNameAccounts) => domainsWithNameAccounts.domain);;
+  return allDomainsWithNameAccounts.map(
+    (domainsWithNameAccounts) => domainsWithNameAccounts.domain
+  );
 }
 
 export const nameService: NameService = {
