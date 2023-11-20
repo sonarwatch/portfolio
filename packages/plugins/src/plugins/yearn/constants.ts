d63c9a8b06c0530dbd6d22c527722f3da3e98f0d
Courmont Guillaume <courmontguillaume@gmail.com>
2023-11-20 18:22:56 +0100

    plugin

---
+++ packages/plugins/src/plugins/yearn/constants.ts
@@ -0,0 +1,11 @@
+import { Platform } from '@sonarwatch/portfolio-core';
+
+export const platformId = 'yearn';
+export const platform: Platform = {
+  id: platformId,
+  name: 'Yearn',
+  image: 'https://sonar.watch/img/platforms/yearn.png',
+  // defiLlamaId: 'foo-finance', // from https://defillama.com/docs/api
+  // website: 'https://myplatform.com',
+  // twitter: 'https://twitter.com/myplatform',
+};
