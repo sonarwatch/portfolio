969c130590c2a26da0d2e4d6e99205612df4b966
Courmont Guillaume <courmontguillaume@gmail.com>
2023-11-22 09:39:00 +0100

    plugin

---
+++ packages/plugins/src/plugins/convex/constants.ts
@@ -0,0 +1,11 @@
+import { Platform } from '@sonarwatch/portfolio-core';
+
+export const platformId = 'convex';
+export const platform: Platform = {
+  id: platformId,
+  name: 'Convex',
+  image: 'https://sonar.watch/img/platforms/convex.png',
+  // defiLlamaId: 'foo-finance', // from https://defillama.com/docs/api
+  // website: 'https://myplatform.com',
+  // twitter: 'https://twitter.com/myplatform',
+};
