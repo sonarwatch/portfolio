20aca35bd76a8e4c6e4c377e67de0bedd6e4cd9c
Courmont Guillaume <courmontguillaume@gmail.com>
2023-11-24 17:26:40 +0100

    add staking

--- packages/plugins/src/plugins/goosefx/filters.ts
+++ packages/plugins/src/plugins/goosefx/filters.ts
@@ -1,5 +1,5 @@
 import { GetProgramAccountsFilter } from '@solana/web3.js';
-import { liquidityStruct } from './structs';
+import { liquidityStruct, userMetadataStruct } from './structs';
 
 export const liquidityAccountFilter = (
   owner: string
@@ -11,4 +11,16 @@ export const liquidityAccountFilter = (
     },
   },
   { dataSize: liquidityStruct.byteSize },
+];
+
+export const stakingAccountFilter = (
+  owner: string
+): GetProgramAccountsFilter[] => [
+  {
+    memcmp: {
+      offset: 8,
+      bytes: owner,
+    },
+  },
+  { dataSize: userMetadataStruct.byteSize },
 ];
--- packages/plugins/src/plugins/goosefx/index.ts
+++ packages/plugins/src/plugins/goosefx/index.ts
@@ -3,7 +3,8 @@ import { Fetcher } from '../../Fetcher';
 import { Job } from '../../Job';
 import { platform } from './constants';
 import liquidityFetcher from './poolsFetcher';
+import stakingFetcher from './stakingFetcher';
 
 export const platforms: Platform[] = [platform];
 export const jobs: Job[] = [];
-export const fetchers: Fetcher[] = [liquidityFetcher];
+export const fetchers: Fetcher[] = [liquidityFetcher, stakingFetcher];
