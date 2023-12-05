f99497ef8a7134168640768ded96f22c84af1248
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2023-12-05 16:15:24 +0400

    Add validation functions for address system, network ID, name, and USD value comparison

--- packages/core/src/utils/assertAddressSystem.ts
+++ packages/core/src/utils/assertAddressSystem.ts
@@ -1,3 +1,10 @@
+/**
+ * Asserts that the given address system is valid.
+ *
+ * @param addressSystem - The address system to be validated.
+ * @returns The validated address system.
+ * @throws Error if the address system is not valid.
+ */
 import { AddressSystem, AddressSystemType } from '../Address';
 
 export function assertAddressSystem(addressSystem: string): AddressSystemType {
