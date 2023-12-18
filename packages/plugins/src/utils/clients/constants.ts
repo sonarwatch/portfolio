316684d0d9b2605b85cf449d69139dc13db32cc0
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2023-12-18 11:01:11 +0400

    change default sei rpc

--- packages/plugins/src/utils/clients/constants.ts
+++ packages/plugins/src/utils/clients/constants.ts
@@ -15,7 +15,7 @@ export const viemChainsByNetworkId: Record<EvmNetworkIdType, Chain> = {
 
 export function getUrlEndpoints(): Record<NetworkIdType, string> {
   return {
-    sei: process.env['PORTFOLIO_SEI_RPC'] || 'https://sei-rpc.brocha.in/',
+    sei: process.env['PORTFOLIO_SEI_RPC'] || 'https://sei.public-rpc.com/',
     bitcoin:
       process.env['PORTFOLIO_BITCOIN_RPC'] || 'https://blockstream.info/api/',
     bnb: process.env['PORTFOLIO_BNB_RPC'] || 'https://binance.llamarpc.com',
