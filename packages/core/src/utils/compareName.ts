0cc42f938e169b469ba10fb1bdafbf6396e54553
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2024-01-05 12:04:39 +0100

    fix

--- packages/core/src/utils/compareName.ts
+++ packages/core/src/utils/compareName.ts
@@ -9,8 +9,8 @@ export function compareName(
   b: string | undefined
 ): number {
   if (a === b) return 0;
-  if (a === undefined) return -1;
-  if (b === undefined) return 1;
+  if (a === undefined) return 1;
+  if (b === undefined) return -1;
   if (a < b) return -1;
   if (b > a) return 1;
   return 0;
