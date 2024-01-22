f5178d83c7b9d2f300b59d93ac41b81a4e642736
Courmont Guillaume <courmontguillaume@gmail.com>
2024-01-22 13:50:50 +0100

    add DUAL and MANGO dao

--- packages/plugins/src/plugins/realms/constants.ts
+++ packages/plugins/src/plugins/realms/constants.ts
@@ -19,6 +19,14 @@ export const vsrInfos: VSRInfos[] = [
     programId: new PublicKey('VoteMBhDCqGLRgYpp9o7DGyq81KNmwjXQRAHStjtJsS'),
     mint: 'MNDEFzGvMt87ueuHvVU9VcTqsAP5b3fTGPsHuuPA5ey',
   },
+  {
+    programId: new PublicKey('4Q6WW2ouZ6V3iaNm56MTd5n2tnTm4C5fiH8miFHnAFHo'),
+    mint: 'MangoCzJ36AjZyKwVj3VnYU4GTonjfVEnJmvvWaxLac',
+  },
+  {
+    programId: new PublicKey('vsr2nfGVNHmSY8uxoBGqq8AQbwz3JwaEaHqGbsTPXqQ'),
+    mint: 'DUALa4FC2yREwZ59PHeu1un4wis36vHRv5hWVBmzykCJ',
+  },
 ];
 export const splGovernanceUrl =
   'https://app.realms.today/api/splGovernancePrograms';
