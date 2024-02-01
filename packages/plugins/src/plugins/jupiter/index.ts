ad0723a053fd8c4fb6061b6049645066008b6e5a
Guillaume <courmontguillaume@gmail.com>
2024-02-01 18:26:34 +0100

    add jup airdrop

--- packages/plugins/src/plugins/jupiter/index.ts
+++ packages/plugins/src/plugins/jupiter/index.ts
@@ -3,6 +3,7 @@ import { Job } from '../../Job';
 import poolJob from './poolJob';
 import perpetualFetcher from './perpetualFetcher';
 import custodiesJob from './custodiesJob';
+import airdropFetcher from './airdropFetcher';
 
 export const jobs: Job[] = [poolJob, custodiesJob];
-export const fetchers: Fetcher[] = [perpetualFetcher];
+export const fetchers: Fetcher[] = [perpetualFetcher, airdropFetcher];
