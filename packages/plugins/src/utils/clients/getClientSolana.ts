0b38fc76261a8dab83ea8f91671603514a821bc6
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2024-02-08 15:40:31 +0400

    add RPC logs

--- packages/plugins/src/utils/clients/getClientSolana.ts
+++ packages/plugins/src/utils/clients/getClientSolana.ts
@@ -1,5 +1,5 @@
 import { NetworkId } from '@sonarwatch/portfolio-core';
-import { Connection } from '@solana/web3.js';
+import { Connection, FetchMiddleware } from '@solana/web3.js';
 import { getBasicAuthHeaders } from '../misc/getBasicAuthHeaders';
 import { getRpcEndpoint } from './constants';
 
@@ -11,7 +11,28 @@ export default function getClientSolana() {
         rpcEndpoint.basicAuth.password
       )
     : undefined;
+
+  let fetchMiddleware: FetchMiddleware | undefined;
+  if (process.env['PORTFOLIO_RPC_LOGS'] === 'true') {
+    const reqs: Record<string, number> = {
+      total: 0,
+    };
+    fetchMiddleware = (info, init, fetch) => {
+      const { method } = JSON.parse(init?.body?.toString() || '{}');
+      if (typeof method !== 'string') return;
+      if (!reqs[method]) reqs[method] = 0;
+      reqs[method] += 1;
+      reqs['total'] += 1;
+      if (reqs['total'] % 5 === 1) {
+        // eslint-disable-next-line no-console
+        console.log(`RPC Requests: ${JSON.stringify(reqs, undefined, 2)}`);
+      }
+      fetch(info, init);
+    };
+  }
+
   return new Connection(rpcEndpoint.url, {
     httpHeaders,
+    fetchMiddleware,
   });
 }
