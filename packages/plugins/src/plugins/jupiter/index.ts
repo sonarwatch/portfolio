0fa0b33cc8f818d2c8833f8937eb2cfd69e902b3
Courmont Guillaume <courmontguillaume@gmail.com>
2024-02-20 10:21:31 +0100

    remove jlp pool job

--- packages/plugins/src/plugins/jupiter/index.ts
+++ packages/plugins/src/plugins/jupiter/index.ts
@@ -1,9 +1,8 @@
 import { Fetcher } from '../../Fetcher';
 import { Job } from '../../Job';
-import poolJob from './poolJob';
 import perpetualFetcher from './perpetualFetcher';
 import custodiesJob from './custodiesJob';
 import airdropFetcher from './airdropFetcher';
 
-export const jobs: Job[] = [poolJob, custodiesJob];
+export const jobs: Job[] = [custodiesJob];
 export const fetchers: Fetcher[] = [perpetualFetcher, airdropFetcher];
