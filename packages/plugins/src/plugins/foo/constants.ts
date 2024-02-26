9491692617fa6c972ad861c4e5cb2b2e79bbfff2
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2024-02-26 14:45:00 +0100

    fix

--- packages/plugins/src/plugins/foo/constants.ts
+++ packages/plugins/src/plugins/foo/constants.ts
@@ -3,8 +3,10 @@ import { Platform } from '@sonarwatch/portfolio-core';
 export const platformId = 'foo';
 export const platform: Platform = {
   id: platformId,
-  name: 'Foo Finanace',
+  name: 'Foo Finance',
   image: 'https://sonar.watch/img/platforms/foo.png',
+  website: 'https://foo.com/',
+  twitter: 'https://twitter.com/foo_finance',
   defiLlamaId: 'foo-finance', // from https://defillama.com/docs/api
 };
 export const marketsCachePrefix = `${platformId}-markets`;
