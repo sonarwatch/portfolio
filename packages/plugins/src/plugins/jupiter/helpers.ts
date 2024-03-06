61fca2e4d5b854d775ddb6a68e1be0b5d5d41c0b
Courmont Guillaume <courmontguillaume@gmail.com>
2024-03-06 08:55:11 +0100

    add vote JUP

--- packages/plugins/src/plugins/jupiter/helpers.ts
+++ packages/plugins/src/plugins/jupiter/helpers.ts
@@ -1,4 +1,5 @@
 import { PublicKey } from '@solana/web3.js';
+import { lockerPubkey, voteProgramId } from './constants';
 
 export function deriveClaimStatus(
   claimant: PublicKey,
@@ -9,4 +10,15 @@ export function deriveClaimStatus(
     [Buffer.from('ClaimStatus'), claimant.toBytes(), distributor.toBytes()],
     programId
   );
+}
+
+export function getVotePda(owner: string): PublicKey {
+  return PublicKey.findProgramAddressSync(
+    [
+      Buffer.from('Escrow'),
+      lockerPubkey.toBytes(),
+      new PublicKey(owner).toBytes(),
+    ],
+    voteProgramId
+  )[0];
 }
--- packages/plugins/src/plugins/jupiter/index.ts
+++ packages/plugins/src/plugins/jupiter/index.ts
@@ -3,6 +3,11 @@ import { Job } from '../../Job';
 import perpetualFetcher from './perpetualFetcher';
 import custodiesJob from './custodiesJob';
 import airdropFetcher from './airdropFetcher';
+import voteFetcher from './voteFetcher';
 
 export const jobs: Job[] = [custodiesJob];
-export const fetchers: Fetcher[] = [perpetualFetcher, airdropFetcher];
+export const fetchers: Fetcher[] = [
+  perpetualFetcher,
+  airdropFetcher,
+  voteFetcher,
+];
