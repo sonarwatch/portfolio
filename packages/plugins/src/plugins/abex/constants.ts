cdffd7ff8dc08daf30cf91c6c2263ae40f09c23f
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2024-03-12 18:27:02 +0100

    add abex

---
+++ packages/plugins/src/plugins/abex/constants.ts
@@ -0,0 +1,19 @@
+import { Platform } from '@sonarwatch/portfolio-core';
+
+export const platformId = 'abex';
+export const platform: Platform = {
+  id: platformId,
+  name: 'ABEx Finance',
+  image: 'https://sonar.watch/img/platforms/abex.webp',
+  website: 'https://abex.fi/',
+  twitter: 'https://twitter.com/ABExFinance',
+  defiLlamaId: 'abex-finance', // from https://defillama.com/docs/api
+};
+
+export const alpType =
+  '0xceab84acf6bf70f503c3b0627acaff6b3f84cee0f2d7ed53d00fa6c2a168d14f::alp::ALP';
+export const alpDecimals = 6;
+export const depositVaultRegistry =
+  '0x3c6595e543c4766dd63b5b2fa918516bac2920bc1944da068be031dced46a18d';
+export const alpSupplyObjectId =
+  '0x7705d4670e7ef4623d6392888f73f6773b5f0218b6cb1486a4be238692a58bca';
---
+++ packages/plugins/src/plugins/abex/types.ts
@@ -0,0 +1,61 @@
+export type AlpMarket = {
+  fun_mask: string;
+  id: {
+    id: string;
+  };
+  lp_supply: {
+    type: string;
+    fields: { value: string };
+  };
+};
+
+export type Market = {
+  id: {
+    id: string;
+  };
+  name: {
+    type: string;
+    fields: {
+      dummy_field: boolean;
+    };
+  };
+  value: {
+    type: string;
+    fields: MarketFields;
+  };
+};
+
+export type MarketFields = {
+  acc_reserving_rate: {
+    type: string;
+    fields: {
+      value: string;
+    };
+  };
+  enabled: boolean;
+  last_update: string;
+  liquidity: string;
+  price_config: {
+    type: string;
+    fields: {
+      feeder: string;
+      max_confidence: string;
+      max_interval: string;
+      precision: string;
+    };
+  };
+  reserved_amount: string;
+  reserving_fee_model: string;
+  unrealised_reserving_fee_amount: {
+    type: string;
+    fields: {
+      value: string;
+    };
+  };
+  weight: {
+    type: string;
+    fields: {
+      value: string;
+    };
+  };
+};
