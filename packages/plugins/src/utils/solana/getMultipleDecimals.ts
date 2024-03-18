43f0cb9e938c0b5048f3ec17f9e7002823f7f91f
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2024-03-18 11:48:23 +0100

    fix

---
+++ packages/plugins/src/utils/solana/getMultipleDecimals.ts
@@ -0,0 +1,30 @@
+import { PublicKey } from '@solana/web3.js';
+import {
+  solanaNativeAddress,
+  solanaNativeDecimals,
+  solanaNativeWrappedAddress,
+} from '@sonarwatch/portfolio-core';
+import { SolanaClient } from '../clients/types';
+import { mintAccountStruct } from './structs';
+import { getParsedMultipleAccountsInfo } from './getParsedMultipleAccountsInfo';
+
+const solMints = [solanaNativeAddress, solanaNativeWrappedAddress];
+
+export async function getMultipleDecimals(
+  client: SolanaClient,
+  mints: PublicKey[]
+): Promise<(number | null)[]> {
+  const mintAccounts = await getParsedMultipleAccountsInfo(
+    client,
+    mintAccountStruct,
+    mints.filter((m) => solMints.includes(m.toString()))
+  );
+  const decimalsMap: Map<string, number> = new Map();
+  decimalsMap.set(solanaNativeAddress, solanaNativeDecimals);
+  decimalsMap.set(solanaNativeWrappedAddress, solanaNativeDecimals);
+  mintAccounts.forEach((acc) => {
+    if (!acc) return;
+    decimalsMap.set(acc.pubkey.toString(), acc.decimals);
+  });
+  return mints.map((m) => decimalsMap.get(m.toString()) || null);
+}
