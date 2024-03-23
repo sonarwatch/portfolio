0571431e60d71987f0d5f31f38cb6df6f292c75f
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2024-03-23 12:06:30 +0100

    add jup pricing job

--- packages/plugins/src/plugins/jupiter/index.ts
+++ packages/plugins/src/plugins/jupiter/index.ts
@@ -3,13 +3,14 @@ import { Fetcher } from '../../Fetcher';
 import { Job } from '../../Job';
 import perpetualFetcher from './perpetualFetcher';
 import custodiesJob from './custodiesJob';
+import pricingJob from './pricingJob';
 import airdropFetcher from './airdropFetcher';
 import voteFetcher from './voteFetcher';
 import { jupiterPlatform } from './constants';
 import valueAverageFetcher from './valueAverageFetcher';
 
 export const platforms: Platform[] = [jupiterPlatform];
-export const jobs: Job[] = [custodiesJob];
+export const jobs: Job[] = [custodiesJob, pricingJob];
 export const fetchers: Fetcher[] = [
   perpetualFetcher,
   airdropFetcher,
