a9c47b79e3c65c20ec7900896365dbced1e1dbe2
Courmont Guillaume <courmontguillaume@gmail.com>
2024-04-04 19:56:54 +0200

    fix platforms

--- packages/plugins/src/plugins/jupiter/index.ts
+++ packages/plugins/src/plugins/jupiter/index.ts
@@ -8,13 +8,19 @@ import dcaFetcher from './exchange/dcaFetcher';
 import custodiesJob from './exchange/custodiesJob';
 
 import allocationFetcher from './launchpad/allocationsFetcher';
+import { jupLaunchpadPlatform } from './launchpad/constants';
 
 import voteFetcher from './governance/voteFetcher';
+import { jupGovernancePlatform } from './governance/constants';
 
 import pricingJob from './pricingJob';
 import { jupiterPlatform } from './exchange/constants';
 
-export const platforms: Platform[] = [jupiterPlatform];
+export const platforms: Platform[] = [
+  jupiterPlatform,
+  jupGovernancePlatform,
+  jupLaunchpadPlatform,
+];
 export const jobs: Job[] = [custodiesJob, pricingJob];
 export const fetchers: Fetcher[] = [
   perpetualFetcher,
