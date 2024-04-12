455099ecdea4f5fd1ef68dd262f269caf5e7f5c9
Courmont Guillaume <courmontguillaume@gmail.com>
2024-04-12 10:24:20 +0200

    remove bskt fetcher

--- packages/plugins/src/plugins/bskt/index.ts
+++ packages/plugins/src/plugins/bskt/index.ts
@@ -2,8 +2,7 @@ import { Platform } from '@sonarwatch/portfolio-core';
 import { Fetcher } from '../../Fetcher';
 import { Job } from '../../Job';
 import { platform } from './constants';
-import vestingFetcher from './vestingFetcher';
 
 export const platforms: Platform[] = [platform];
 export const jobs: Job[] = [];
-export const fetchers: Fetcher[] = [vestingFetcher];
+export const fetchers: Fetcher[] = [];
