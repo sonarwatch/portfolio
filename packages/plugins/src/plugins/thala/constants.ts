364a3bc9fc0a6a369121e594f688cb7af685ce68
Guillaume <courmontguillaume@gmail.com>
2024-04-19 15:17:44 +0200

    fix

--- packages/plugins/src/plugins/thala/constants.ts
+++ packages/plugins/src/plugins/thala/constants.ts
@@ -9,21 +9,21 @@ export const platform: Platform = {
   defiLlamaId: 'parent#thala-labs',
   website: 'https://www.thala.fi/',
 };
-export const programAdressThala =
+export const packageId =
   '0x6f986d146e4a90b828d8c12c14b6f4e003fdff11a8eecceceb63744363eaac01';
 
 const stabilityModule = 'stability_pool';
 const vaultModule = 'vault';
 
-const liquidityDeposit = `${programAdressThala}::${stabilityModule}::account_deposit`;
-const liquidityClaimable = `${programAdressThala}::${stabilityModule}::claimable_thl`;
+const liquidityDeposit = `${packageId}::${stabilityModule}::account_deposit`;
+const liquidityClaimable = `${packageId}::${stabilityModule}::claimable_thl`;
 
-const cryptoType = `${programAdressThala}::${stabilityModule}::Crypto`;
-export const vaultFilter = `${programAdressThala}::${vaultModule}::Vault<`;
-export const vaultCollateralParamsFilter = `${programAdressThala}::${vaultModule}::VaultCollateralParams<`;
+const cryptoType = `${packageId}::${stabilityModule}::Crypto`;
+export const vaultFilter = `${packageId}::${vaultModule}::Vault<`;
+export const vaultCollateralParamsFilter = `${packageId}::${vaultModule}::VaultCollateralParams<`;
 
 export const thlCoin = `0x7fd500c11216f0fe3095d0c4b8aa4d64a4e2e04f83758462f2b127255643615::thl_coin::THL`;
-export const modCoin = `${programAdressThala}::mod_coin::MOD`;
+export const modCoin = `${packageId}::mod_coin::MOD`;
 
 export const stabilityEndpoint = 'https://app.thala.fi/api/total-stability';
 
