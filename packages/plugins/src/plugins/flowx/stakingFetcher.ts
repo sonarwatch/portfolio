6949b11d2b354cd88504275e032bec6761a7f68f
Courmont Guillaume <courmontguillaume@gmail.com>
2024-04-25 12:29:01 +0200

    eslint + group assets & add tags

--- packages/plugins/src/plugins/flowx/stakingFetcher.ts
+++ packages/plugins/src/plugins/flowx/stakingFetcher.ts
@@ -1,16 +1,20 @@
 import {
   getUsdValueSum,
-  NetworkId, PortfolioAsset, PortfolioElement,
-  PortfolioElementType
+  NetworkId,
+  PortfolioAsset,
+  PortfolioElement,
+  PortfolioElementType,
 } from '@sonarwatch/portfolio-core';
 import BigNumber from 'bignumber.js';
 import { Cache } from '../../Cache';
 import { Fetcher, FetcherExecutor } from '../../Fetcher';
 import {
   flxMint,
-  platformId, stakingParentObject, suiMint,
+  platformId,
+  stakingParentObject,
+  suiMint,
   unstackStruct,
-  xflxMint
+  xflxMint,
 } from './constants';
 import { getDynamicFieldObject } from '../../utils/sui/getDynamicFieldObject';
 import { getClientSui } from '../../utils/clients';
@@ -22,71 +26,88 @@ const executor: FetcherExecutor = async (owner: string, cache: Cache) => {
   const elements: PortfolioElement[] = [];
   const client = getClientSui();
 
-  const xflxTokenPrice = await cache.getTokenPrice(xflxMint, NetworkId.sui);
-  const flxTokenPrice = await cache.getTokenPrice(flxMint, NetworkId.sui);
-  const suiTokenPrice = await cache.getTokenPrice(suiMint, NetworkId.sui);
+  const tokenPricesById = await cache.getTokenPricesAsMap(
+    [xflxMint, flxMint, suiMint],
+    NetworkId.sui
+  );
+  const xflxTokenPrice = tokenPricesById.get(xflxMint);
+  const flxTokenPrice = tokenPricesById.get(flxMint);
+  const suiTokenPrice = tokenPricesById.get(suiMint);
 
-  const stakingPosition = await getDynamicFieldObject<StakingPosition>(client, {
-    parentId: stakingParentObject,
-    name: { type: 'address', value: owner },
-  });
+  const [stakingPosition, unstakingPositions] = await Promise.all([
+    getDynamicFieldObject<StakingPosition>(client, {
+      parentId: stakingParentObject,
+      name: { type: 'address', value: owner },
+    }),
+    getOwnedObjects<UnstakingPositionObject>(client, owner, {
+      filter: {
+        StructType: unstackStruct,
+      },
+      options: {
+        showContent: true,
+      },
+    }),
+  ]);
 
-  if (!stakingPosition.error && stakingPosition.data?.content && stakingPosition.data.content.fields.value.fields.amount !== "0") {
-    const amount = new BigNumber(stakingPosition.data.content.fields.value.fields.amount).dividedBy(10 ** 8);
+  const assets: PortfolioAsset[] = [];
+  if (
+    !stakingPosition.error &&
+    stakingPosition.data?.content &&
+    stakingPosition.data.content.fields.value.fields.amount !== '0'
+  ) {
+    const amount = new BigNumber(
+      stakingPosition.data.content.fields.value.fields.amount
+    ).dividedBy(10 ** 8);
 
-    const assets: PortfolioAsset[] = [];
+    assets.push(
+      tokenPriceToAssetToken(
+        xflxMint,
+        amount.toNumber(),
+        NetworkId.sui,
+        xflxTokenPrice
+      )
+    );
 
-    assets.push(tokenPriceToAssetToken(
-      xflxMint,
-      amount.toNumber(),
-      NetworkId.sui,
-      xflxTokenPrice
-    ));
-
-    assets.push({
-      ...tokenPriceToAssetToken(
+    assets.push(
+      tokenPriceToAssetToken(
         suiMint,
-        new BigNumber(stakingPosition.data.content.fields.value.fields.sui_pending).dividedBy(10 ** (suiTokenPrice?.decimals ?? 9)).toNumber(),
+        new BigNumber(
+          stakingPosition.data.content.fields.value.fields.sui_pending
+        )
+          .dividedBy(10 ** (suiTokenPrice?.decimals ?? 9))
+          .toNumber(),
         NetworkId.sui,
-        suiTokenPrice
-      ),
-      attributes: { isClaimable: true },
-    });
+        suiTokenPrice,
+        undefined,
+        { isClaimable: true, tags: ['rewards'] }
+      )
+    );
 
-    assets.push({
-      ...tokenPriceToAssetToken(
+    assets.push(
+      tokenPriceToAssetToken(
         flxMint,
-        new BigNumber(stakingPosition.data.content.fields.value.fields.flx_pending).dividedBy(10 ** (flxTokenPrice?.decimals ?? 8)).toNumber(),
+        new BigNumber(
+          stakingPosition.data.content.fields.value.fields.flx_pending
+        )
+          .dividedBy(10 ** (flxTokenPrice?.decimals ?? 8))
+          .toNumber(),
         NetworkId.sui,
-        flxTokenPrice
-      ),
-      attributes: { isClaimable: true },
-    });
+        flxTokenPrice,
+        undefined,
+        { isClaimable: true, tags: ['rewards'] }
+      )
+    );
 
-    elements.push({
-      type: PortfolioElementType.multiple,
-      label: 'Staked',
-      networkId: NetworkId.sui,
-      platformId,
-      data: { assets },
-      value: getUsdValueSum(assets.map((asset) => asset.value)),
-    });
+    elements.push();
   }
-
-  const unstakingPositions = await getOwnedObjects<UnstakingPositionObject>(client, owner, {
-    "filter": {
-      "StructType": unstackStruct
-    },
-    "options": {
-      "showContent": true
-    }
-  });
 
   unstakingPositions.forEach((unstakingPosition) => {
     const fields = unstakingPosition.data?.content?.fields;
 
-    if (fields && fields.balance !== "0") {
+    if (fields && fields.balance !== '0') {
       const amount = new BigNumber(fields.balance).dividedBy(10 ** 8);
+
+      const unlockingAt = Number(fields.unlocked_at_epoch);
 
       const asset: PortfolioAsset = {
         ...tokenPriceToAssetToken(
@@ -96,22 +117,27 @@ const executor: FetcherExecutor = async (owner: string, cache: Cache) => {
           xflxTokenPrice
         ),
         attributes: {
-          // lockedUntil: endDate.getTime(),
+          tags: ['unstaking'],
+          lockedUntil: unlockingAt,
         },
       };
 
-      elements.push({
-        type: PortfolioElementType.multiple,
-        label: 'Staked',
-        networkId: NetworkId.sui,
-        platformId,
-        data: { assets: [asset] },
-        value: asset.value,
-      });
+      assets.push(asset);
     }
   });
 
-  return elements;
+  if (assets.length === 0) return [];
+
+  return [
+    {
+      type: PortfolioElementType.multiple,
+      label: 'Staked',
+      networkId: NetworkId.sui,
+      platformId,
+      data: { assets },
+      value: getUsdValueSum(assets.map((asset) => asset.value)),
+    },
+  ];
 };
 
 const fetcher: Fetcher = {
