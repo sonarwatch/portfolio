fce44ea3b2d8cd5a8ecfa8af4ed47c0e048b5749
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2024-04-25 09:57:29 +0200

    optimize meteora farms

--- packages/plugins/src/plugins/meteora/helpers.ts
+++ packages/plugins/src/plugins/meteora/helpers.ts
@@ -1,5 +1,8 @@
 import { PublicKey } from '@solana/web3.js';
 import { farmProgramId } from './constants';
+import { Farm } from './struct';
+import { FormattedFarm } from './types';
+import { ParsedAccount } from '../../utils/solana';
 
 export function getStakingAccounts(
   owner: string,
@@ -12,4 +15,28 @@ export function getStakingAccounts(
         farmProgramId
       )[0]
   );
+}
+
+export function formatFarm(farm: ParsedAccount<Farm>): FormattedFarm {
+  return {
+    pubkey: farm.pubkey.toString(),
+    authority: farm.authority.toString(),
+    stakingMint: farm.stakingMint.toString(),
+    stakingVault: farm.stakingVault.toString(),
+    rewardAMint: farm.rewardAMint.toString(),
+    rewardAVault: farm.rewardAVault.toString(),
+    rewardBMint: farm.rewardBMint.toString(),
+    rewardBVault: farm.rewardBVault.toString(),
+    rewardDuration: farm.rewardDuration.toString(),
+    rewardDurationEnd: farm.rewardDurationEnd.toString(),
+    lastUpdateTime: farm.lastUpdateTime.toString(),
+    rewardARate: farm.rewardARate.toString(),
+    rewardBRate: farm.rewardBRate.toString(),
+    rewardAPerTokenStored: farm.rewardAPerTokenStored.toString(),
+    rewardBPerTokenStored: farm.rewardBPerTokenStored.toString(),
+    rewardARateU128: farm.rewardARateU128.toString(),
+    rewardBRateU128: farm.rewardBRateU128.toString(),
+    paused: farm.paused,
+    totalStaked: farm.totalStaked.toString(),
+  };
 }
