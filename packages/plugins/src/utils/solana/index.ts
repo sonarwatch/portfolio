62ff96782c547546ca24bebd98fd48a2e7e4f167
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2024-06-10 11:34:42 +0200

    fix

--- packages/plugins/src/utils/solana/index.ts
+++ packages/plugins/src/utils/solana/index.ts
@@ -4,7 +4,6 @@ export * from './constants';
 export * from './getParsedProgramAccounts';
 export * from './getParsedMultipleAccountsInfo';
 export * from './getProgramAccounts';
-export * from './getTokenAccountsByOwner';
 export * from './u8ArrayToString';
 export * from './structs';
 export * from './types';
