9f220adf4f81fdb13dd677e5891a08fa3e6a2f00
Courmont Guillaume <courmontguillaume@gmail.com>
2024-06-18 11:10:43 +0200

    add missing daos

--- packages/plugins/src/plugins/futarchy/constants.ts
+++ packages/plugins/src/plugins/futarchy/constants.ts
@@ -18,4 +18,7 @@ export const daoPid = new PublicKey(
 export const daoNameById: Map<string, string> = new Map([
   ['CNMZgxYsQpygk8CLN9Su1igwXX2kHtcawaNAGuBPv3G9', 'MetaDAO '],
   ['5vVCYQHPd8o3pGejYWzKZtnUSdLjXzDZcjZQxiFumXXx', 'Drift '],
+  ['ofvb3CPvEyRfD5az8PAqW6ATpPqVBeiB5zBnpPR5cgm', 'Future '],
+  ['9TKh2yav4WpSNkFV2cLybrWZETBWZBkQ6WB6qV9Nt9dJ', "Dean's List "],
+  ['EqgPmuKVugfsjjUpxNtqeTkM34oh2H3BVvEfFJTo2Uj5', 'Manifesto '],
 ]);
