48e563fbd63d661b1abdd4738b9672d176fcbc6f
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2024-06-21 09:58:03 +0200

    fix client

--- packages/plugins/src/utils/clients/getClientAptos.ts
+++ packages/plugins/src/utils/clients/getClientAptos.ts
@@ -12,12 +12,16 @@ export default function getClientAptos(): AptosClient {
         rpcEndpoint.basicAuth?.password
       )
     : undefined;
+  let fullnode = rpcEndpoint.url.slice(0, -1);
+  if (rpcEndpoint.url.endsWith('/')) {
+    fullnode = fullnode.slice(0, -1);
+  }
   const config = new AptosConfig({
     network: Network.MAINNET,
     clientConfig: {
       HEADERS: headers,
     },
-    fullnode: rpcEndpoint.url,
+    fullnode,
   });
   return new Aptos(config);
 }
