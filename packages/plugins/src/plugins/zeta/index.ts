bc5f7a233848efad94edb29a0ee90d7aaa4a673e
Courmont Guillaume <courmontguillaume@gmail.com>
2024-06-21 09:32:23 +0200

    add zeta airdrop

--- packages/plugins/src/plugins/zeta/index.ts
+++ packages/plugins/src/plugins/zeta/index.ts
@@ -2,8 +2,9 @@ import { Platform } from '@sonarwatch/portfolio-core';
 import { Fetcher } from '../../Fetcher';
 import { Job } from '../../Job';
 import depositsFetcher from './depositsFetcher';
+import airdropFetcher from './airdropFetcher';
 import { platform } from './constants';
 
 export const jobs: Job[] = [];
-export const fetchers: Fetcher[] = [depositsFetcher];
+export const fetchers: Fetcher[] = [depositsFetcher, airdropFetcher];
 export const platforms: Platform[] = [platform];
