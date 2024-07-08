1d61408646796d23fee372c83a4f4f4843c3bbf8
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2024-07-08 08:52:16 +0200

    replace

--- packages/plugins/src/utils/misc/constants.ts
+++ packages/plugins/src/utils/misc/constants.ts
@@ -4,5 +4,5 @@ export const zeroBigInt = BigInt(0);
 export const zeroBigNumber = BigNumber(0);
 export const lpAddressesCachePrefix = 'lp-addresses';
 export const usdcLogoUri =
-  'https://raw.githubusercontent.com/sonarwatch/token-lists/main/images/common/USDC.png';
+  'https://raw.githubusercontent.com/sonarwatch/token-lists/main/images/common/USDC.webp';
 export const minimumReserveValue = 2500;
