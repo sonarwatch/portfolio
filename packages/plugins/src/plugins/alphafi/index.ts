cbd6b6bda4410eb0a23615747537b0bca751636a
Courmont Guillaume <courmontguillaume@gmail.com>
2024-08-19 16:07:56 +0200

    finalized

--- packages/plugins/src/plugins/alphafi/index.ts
+++ packages/plugins/src/plugins/alphafi/index.ts
@@ -3,7 +3,8 @@ import { Fetcher } from '../../Fetcher';
 import { Job } from '../../Job';
 import { platform } from './constants';
 import positionsFetcher from './positionsFetcher';
+import poolsJob from './poolsJob';
 
 export const platforms: Platform[] = [platform];
-export const jobs: Job[] = [];
+export const jobs: Job[] = [poolsJob];
 export const fetchers: Fetcher[] = [positionsFetcher];
--- packages/plugins/src/plugins/alphafi/positionsFetcher.ts
+++ packages/plugins/src/plugins/alphafi/positionsFetcher.ts
@@ -1,207 +1,216 @@
 import {
-  formatMoveAddress,
   formatMoveTokenAddress,
   getUsdValueSum,
   NetworkId,
   PortfolioAsset,
-  PortfolioAssetAttributes,
   PortfolioElement,
-  PortfolioElementMultiple,
+  PortfolioLiquidity,
 } from '@sonarwatch/portfolio-core';
 import BigNumber from 'bignumber.js';
 import { Cache } from '../../Cache';
 import {
-  alphaPoolReceiptType,
+  alphaPoolsInfoKey,
   alphaToken,
-  cetusPoolMap,
-  distinctReceiptTypes,
   platformId,
+  receiptTypes,
 } from './constants';
 import { Fetcher, FetcherExecutor } from '../../Fetcher';
 import { getOwnedObjects } from '../../utils/sui/getOwnedObjects';
 import { getClientSui } from '../../utils/clients';
-import { Balance, Pool, Receipt } from './types';
-import { pool as cetusPool } from '../cetus/types';
-import { multiGetObjects } from '../../utils/sui/multiGetObjects';
-import { ObjectResponse } from '../../utils/sui/types';
-import { extractCointypesFromPool, getExchangeRate } from './helpers';
-import tokenPriceToAssetTokens from '../../utils/misc/tokenPriceToAssetTokens';
-import { getPoolFromObject } from '../cetus/helpers';
-import { getTokenAmountsFromLiquidity } from '../../utils/clmm/tokenAmountFromLiquidity';
+import { AlphaPoolInfo, Receipt } from './types';
+import tokenPriceToAssetToken from '../../utils/misc/tokenPriceToAssetToken';
+
+const balanceFactor = 10 ** 9;
 
 const executor: FetcherExecutor = async (owner: string, cache: Cache) => {
   const client = getClientSui();
 
-  const receipts = await getOwnedObjects<Receipt>(client, owner, {
-    filter: {
-      MatchAny: distinctReceiptTypes.map((t) => ({
-        StructType: t,
-      })),
-    },
+  const [receipts, alphaPoolsInfo] = await Promise.all([
+    getOwnedObjects<Receipt>(client, owner, {
+      filter: {
+        MatchAny: receiptTypes.map((t) => ({
+          StructType: t,
+        })),
+      },
+    }),
+    cache.getItem<AlphaPoolInfo[]>(alphaPoolsInfoKey, {
+      prefix: platformId,
+      networkId: NetworkId.sui,
+    }),
+  ]);
+
+  if (receipts.length === 0 || !alphaPoolsInfo) return [];
+
+  const alphaPoolInfoById: Map<string, AlphaPoolInfo> = new Map();
+  alphaPoolsInfo.forEach((pool) =>
+    alphaPoolInfoById.set(pool.alphaPoolId, pool)
+  );
+
+  const mints: Set<string> = new Set();
+  alphaPoolsInfo.forEach((aI) => {
+    mints.add(aI.cointTypeA);
+    if (aI.cointTypeB) mints.add(aI.cointTypeB);
   });
 
-  if (receipts.length === 0) return [];
-
-  const pools = await multiGetObjects<Pool>(
-    client,
-    receipts
-      .map((r) => r.data?.content?.fields.pool_id)
-      .filter((p) => p !== null) as string[]
-  ).then((arrPools) => {
-    const poolsByKey = new Map<string, ObjectResponse<Pool>>();
-    arrPools.forEach((item) => {
-      if (!item) return;
-      poolsByKey.set(item.data?.content?.fields.id.id as string, item);
-    });
-    return poolsByKey;
-  });
+  const tokenPriceById = await cache.getTokenPricesAsMap(
+    [...Array.from(mints), formatMoveTokenAddress(alphaToken)],
+    NetworkId.sui
+  );
 
   const elements: PortfolioElement[] = [];
-
-  const positions: { name: string; balances: Balance[] }[] = [];
+  const cetusLiquidities: PortfolioLiquidity[] = [];
+  const alphaAssets: PortfolioAsset[] = [];
+  const naviAssets: PortfolioAsset[] = [];
 
   receipts.forEach((receipt) => {
+    if (!receipt.data?.content?.fields.pool_id) return;
+
+    const receiptInfo = receipt.data.content.fields;
+    const alphaPoolInfo = alphaPoolInfoById.get(receiptInfo.pool_id);
+    if (!alphaPoolInfo) return;
+
+    // Single Vaults
     if (
-      !receipt.data?.content?.fields.pool_id ||
-      !receipt.data?.content?.fields.xTokenBalance
-    )
-      return;
-    const pool = pools.get(receipt.data?.content?.fields.pool_id);
-    if (!pool || !pool.data?.type || !pool.data?.content?.fields) return;
-
-    const depositedCoinTypes = extractCointypesFromPool(pool.data?.type);
-    const exchangeRate = getExchangeRate(pool.data?.content?.fields);
-
-    const balances: Balance[] = [];
-
-    let totalLockedXTokens = new BigNumber(0);
-    let totalUnlockedXTokens = new BigNumber(0);
-
-    if (receipt.data?.type === alphaPoolReceiptType) {
-      // alpha vault
-
-      const xTokens = new BigNumber(receipt.data.content.fields.xTokenBalance);
-      const bal = new BigNumber(
-        Number(receipt.data.content.fields.unlocked_xtokens) *
-          exchangeRate.toNumber()
+      !alphaPoolInfo.cointTypeB ||
+      !alphaPoolInfo.tokenAmountB ||
+      !alphaPoolInfo.underlyingPoolId
+    ) {
+      const lockedShares = new BigNumber(receiptInfo.xTokenBalance).dividedBy(
+        alphaPoolInfo.xTokenSupply
       );
-      totalLockedXTokens = totalLockedXTokens.plus(xTokens.minus(bal));
+      const unlockedShares = new BigNumber(
+        receiptInfo.unlocked_xtokens
+      ).dividedBy(alphaPoolInfo.xTokenSupply);
 
-      const lockedTokens = totalLockedXTokens
-        .div(1e9)
-        .multipliedBy(exchangeRate);
-
-      balances.push({
-        coinType: alphaToken,
-        balance: lockedTokens,
-        locked: true,
-      });
-
-      const bal2 = BigNumber(
-        receipt.data.content.fields.unlocked_xtokens
-      ).multipliedBy(exchangeRate);
-      totalUnlockedXTokens = totalUnlockedXTokens.plus(bal2);
-
-      const unlockedTokens = totalUnlockedXTokens
-        .div(1e9)
-        .multipliedBy(exchangeRate);
-
-      balances.push({
-        coinType: alphaToken,
-        balance: unlockedTokens,
-        locked: false,
-      });
-    } else {
-      totalUnlockedXTokens = totalUnlockedXTokens.plus(
-        receipt.data.content.fields.xTokenBalance
+      const lockedAmount = lockedShares.multipliedBy(
+        alphaPoolInfo.tokensInvested
       );
-      const unlockedTokens = totalUnlockedXTokens
-        .div(1e9)
-        .multipliedBy(exchangeRate);
 
-      let coinType;
-      if (receipt.data?.type.includes('cetus')) {
-        coinType = cetusPoolMap[pool.data.objectId];
-      } else if (receipt.data?.type.includes('navi')) {
-        [coinType] = depositedCoinTypes;
+      const unlockedAmount = unlockedShares.multipliedBy(
+        alphaPoolInfo.tokensInvested
+      );
+
+      const tokenPrice = tokenPriceById.get(
+        formatMoveTokenAddress(alphaPoolInfo.cointTypeA)
+      );
+      if (!tokenPrice) return;
+
+      // Alpha Vault
+      if (alphaPoolInfo.unlockAt) {
+        if (!lockedAmount.isZero())
+          alphaAssets.push(
+            tokenPriceToAssetToken(
+              alphaPoolInfo.cointTypeA,
+              lockedAmount.dividedBy(balanceFactor).toNumber(),
+              NetworkId.sui,
+              tokenPrice,
+              undefined,
+              { lockedUntil: alphaPoolInfo.unlockAt }
+            )
+          );
+        if (!unlockedAmount.isZero())
+          alphaAssets.push(
+            tokenPriceToAssetToken(
+              alphaPoolInfo.cointTypeA,
+              unlockedAmount.dividedBy(balanceFactor).toNumber(),
+              NetworkId.sui,
+              tokenPrice,
+              undefined,
+              { isClaimable: true }
+            )
+          );
+        // Navi Strategies
+      } else if (!lockedAmount.isZero()) {
+        naviAssets.push(
+          tokenPriceToAssetToken(
+            alphaPoolInfo.cointTypeA,
+            lockedAmount.dividedBy(10 ** 9).toNumber(),
+            NetworkId.sui,
+            tokenPrice,
+            undefined
+          )
+        );
       }
+      // Cetus Strategies (2 tokens)
+    } else {
+      const [tokenPriceA, tokenPriceB] = [
+        tokenPriceById.get(alphaPoolInfo.cointTypeA),
+        tokenPriceById.get(alphaPoolInfo.cointTypeB),
+      ];
 
-      if (coinType)
-        balances.push({
-          coinType,
-          balance: unlockedTokens,
-        });
-    }
+      if (!tokenPriceA || !tokenPriceB) return;
 
-    positions.push({
-      name: receipt.data?.content.fields.name,
-      balances,
-    });
-  });
+      const assets: PortfolioAsset[] = [];
 
-  const poolsIds = positions
-    .map((p) => p.balances)
-    .flat()
-    .map((b) => b.coinType);
-  const poolsById: Map<string, cetusPool> = new Map();
-  const mints: string[] = [];
-  const poolsObjects = await multiGetObjects(client, poolsIds);
-  poolsObjects.forEach((poolObj) => {
-    if (poolObj.data?.content?.fields) {
-      const pool = getPoolFromObject(poolObj);
-      poolsById.set(poolObj.data.objectId, pool);
-      mints.push(pool.coinTypeA, pool.coinTypeB);
-    }
-  });
-
-  positions.forEach((position) => {
-    const assets: PortfolioAsset[] = [];
-
-    position.balances.forEach((balance) => {
-      if (balance.balance.isZero()) return;
-
-      const pool = poolsById.get(balance.coinType);
-      if (!pool) return;
-
-      // const { tokenAmountA, tokenAmountB } = getTokenAmountsFromLiquidity(
-      //   new BigNumber(clmmPosition.liquidity),
-      //   pool.current_tick_index,
-      //   clmmPosition.tick_lower_index,
-      //   clmmPosition.tick_upper_index,
-      //   false
-      // );
-
-      const attributes: PortfolioAssetAttributes = {};
-
-      if (balance.locked !== undefined)
-        attributes['isClaimable'] = !balance.locked;
+      const xTokenShares = new BigNumber(receiptInfo.xTokenBalance).dividedBy(
+        alphaPoolInfo.xTokenSupply
+      );
 
       assets.push(
-        ...tokenPriceToAssetTokens(
-          balance.coinType,
-          balance.balance.toNumber(),
+        tokenPriceToAssetToken(
+          alphaPoolInfo.cointTypeA,
+          xTokenShares
+            .times(alphaPoolInfo.tokenAmountA)
+            .dividedBy(10 ** tokenPriceA.decimals)
+            .toNumber(),
           NetworkId.sui,
-          undefined,
-          undefined,
-          attributes
+          tokenPriceA
         )
       );
+      assets.push(
+        tokenPriceToAssetToken(
+          alphaPoolInfo.cointTypeB,
+          xTokenShares
+            .times(alphaPoolInfo.tokenAmountB)
+            .dividedBy(10 ** tokenPriceB.decimals)
+            .toNumber(),
+          NetworkId.sui,
+          tokenPriceB
+        )
+      );
+      cetusLiquidities.push({
+        assets,
+        assetsValue: getUsdValueSum(assets.map((a) => a.value)),
+        rewardAssets: [],
+        rewardAssetsValue: 0,
+        value: getUsdValueSum(assets.map((a) => a.value)),
+        yields: [],
+      });
+    }
+  });
+
+  if (cetusLiquidities.length !== 0)
+    elements.push({
+      type: 'liquidity',
+      networkId: NetworkId.sui,
+      platformId,
+      name: 'LP Strategies',
+      value: getUsdValueSum(cetusLiquidities.map((l) => l.value)),
+      label: 'Deposit',
+      data: { liquidities: cetusLiquidities },
     });
 
-    if (assets.length === 0) return;
-
-    const element: PortfolioElementMultiple = {
+  if (alphaAssets.length !== 0)
+    elements.push({
       type: 'multiple',
       networkId: NetworkId.sui,
       platformId,
-      value: getUsdValueSum(assets.map((a) => a.value)),
+      name: 'Alpha Vault',
+      value: getUsdValueSum(alphaAssets.map((a) => a.value)),
       label: 'Deposit',
-      name: position.name,
-      data: { assets },
-    };
+      data: { assets: alphaAssets },
+    });
 
-    elements.push(element);
-  });
+  if (naviAssets.length !== 0)
+    elements.push({
+      type: 'multiple',
+      networkId: NetworkId.sui,
+      platformId,
+      name: 'Single Token Strategies',
+      value: getUsdValueSum(naviAssets.map((a) => a.value)),
+      label: 'Deposit',
+      data: { assets: naviAssets },
+    });
 
   return elements;
 };
