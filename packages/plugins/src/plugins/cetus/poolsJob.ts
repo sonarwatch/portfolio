d858af27fc8aa8791bc9d9ed9df324320f24f719
Courmont Guillaume <courmontguillaume@gmail.com>
2024-08-20 17:04:15 +0200

    better cetus pool fetch (still need more history from RPC)

--- packages/plugins/src/plugins/cetus/poolsJob.ts
+++ packages/plugins/src/plugins/cetus/poolsJob.ts
@@ -3,60 +3,74 @@ import BigNumber from 'bignumber.js';
 import { Cache } from '../../Cache';
 import { Job, JobExecutor } from '../../Job';
 import { getClientSui } from '../../utils/clients';
-import { clmmPoolsPrefix, createPoolEvent, platformId } from './constants';
+import {
+  clmmPoolsPrefix,
+  firstPool,
+  platformId,
+  poolParentId,
+} from './constants';
 import { getPoolFromObject } from './helpers';
 import storeTokenPricesFromSqrt from '../../utils/clmm/tokenPricesFromSqrt';
-import { ParsedJsonEvent } from './types';
+import { TablePoolInfo } from './types';
 import { multiGetObjects } from '../../utils/sui/multiGetObjects';
-import { queryEvents } from '../../utils/sui/queryEvents';
+import { getDynamicFieldObject } from '../../utils/sui/getDynamicFieldObject';
 
 const executor: JobExecutor = async (cache: Cache) => {
   const client = getClientSui();
   const promises = [];
   const cacheItems = [];
 
-  const eventsData = await queryEvents<ParsedJsonEvent>(client, {
-    MoveEventType: createPoolEvent,
-  });
-  let poolsAddresses;
-  let poolsObjects;
-  let sliceEvents;
-  for (let t = 0; t < eventsData.length; t += 50) {
-    sliceEvents = eventsData.slice(t, t + 50);
-    poolsAddresses = sliceEvents
-      .map((eventData) =>
-        eventData.parsedJson ? eventData.parsedJson.pool_id : []
-      )
-      .flat();
-
-    poolsObjects = await multiGetObjects(client, poolsAddresses);
-
-    let poolId;
-    let pool;
-    for (let i = 0; i < poolsObjects.length; i++) {
-      poolId = poolsObjects[i].data?.objectId;
-      if (!poolId) continue;
-
-      pool = getPoolFromObject(poolsObjects[i]);
-      if (!pool) continue;
-
-      promises.push(
-        storeTokenPricesFromSqrt(
-          cache,
-          NetworkId.sui,
-          pool.poolAddress,
-          new BigNumber(pool.coinAmountA),
-          new BigNumber(pool.coinAmountB),
-          new BigNumber(pool.current_sqrt_price),
-          pool.coinTypeA,
-          pool.coinTypeB
-        )
-      );
-      cacheItems.push({
-        key: poolId,
-        value: pool,
-      });
+  const poolsAddresses: string[] = [];
+  let poolInfo;
+  let value;
+  do {
+    if (!poolInfo) {
+      value = firstPool;
+    } else {
+      value = poolInfo.data?.content?.fields.value.fields.next;
     }
+
+    poolInfo = await getDynamicFieldObject<TablePoolInfo>(client, {
+      parentId: poolParentId,
+      name: {
+        type: '0x2::object::ID',
+        value,
+      },
+    });
+
+    if (poolInfo.data?.content)
+      poolsAddresses.push(
+        poolInfo.data.content.fields.value.fields.value.fields.pool_id
+      );
+  } while (poolInfo && poolInfo.data?.content?.fields.value.fields.next);
+
+  const poolsObjects = await multiGetObjects(client, poolsAddresses);
+
+  let poolId;
+  let pool;
+  for (let i = 0; i < poolsObjects.length; i++) {
+    poolId = poolsObjects[i].data?.objectId;
+    if (!poolId) continue;
+
+    pool = getPoolFromObject(poolsObjects[i]);
+    if (!pool) continue;
+
+    promises.push(
+      storeTokenPricesFromSqrt(
+        cache,
+        NetworkId.sui,
+        pool.poolAddress,
+        new BigNumber(pool.coinAmountA),
+        new BigNumber(pool.coinAmountB),
+        new BigNumber(pool.current_sqrt_price),
+        pool.coinTypeA,
+        pool.coinTypeB
+      )
+    );
+    cacheItems.push({
+      key: poolId,
+      value: pool,
+    });
   }
 
   await cache.setItems(cacheItems, {
