2c8596f070096ab924c34e51583cbb344ac32eeb
opptylabs <oppty@proton.me>
2024-08-27 14:04:13 +0200

    save.fi wrapped token2022

--- packages/plugins/src/plugins/save/constants.ts
+++ packages/plugins/src/plugins/save/constants.ts
@@ -19,8 +19,8 @@ export const pid = new PublicKey('So1endDq2YkqhipRh3WViPa8hdiSpxWy6z3Z6tMCpAo');
 export const merkleProgramId = new PublicKey(
   'mrksLcZ6rMs9xkmJgw6oKiR3GECw44Gb5NeDqu64kiw'
 );
-export const marketsEndpoint = 'https://api.solend.fi/v1/markets/configs';
-export const reserveEndpoint = 'https://api.solend.fi/v1/reserves/?ids=';
+export const marketsEndpoint = 'https://api.save.finance/v1/markets/configs';
+export const reserveEndpoint = 'https://api.save.finance/v1/reserves/?ids=';
 export const wadsDecimal = 18;
 export const mainMarket = '4UpD2fh7xH3VP9QQaXtsS1YY3bxzWhtfpks7FatyKvdY';
 export const rewardProofsEndpoint =
--- packages/plugins/src/plugins/save/index.ts
+++ packages/plugins/src/plugins/save/index.ts
@@ -7,7 +7,13 @@ import obligationFetcher from './obligationsFetcher';
 import { platform } from './constants';
 import rewardsFetcher from './rewardsFetcher';
 import rewardStatsJob from './rewardStatsJob';
+import token2022wrapperJob from './token2022wrapperJob';
 
 export const platforms: Platform[] = [platform];
-export const jobs: Job[] = [marketsJob, reservesJob, rewardStatsJob];
+export const jobs: Job[] = [
+  marketsJob,
+  reservesJob,
+  rewardStatsJob,
+  token2022wrapperJob,
+];
 export const fetchers: Fetcher[] = [obligationFetcher, rewardsFetcher];
