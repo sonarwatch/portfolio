111367079dfc82e412891e377a1e7d68ab3d80b8
opptylabs <oppty@proton.me>
2024-09-26 23:25:05 +0200

    deepbook deposits

--- packages/plugins/src/plugins/deepbook/constants.ts
+++ packages/plugins/src/plugins/deepbook/constants.ts
@@ -22,3 +22,9 @@ export const airdropStatics: AirdropStatics = {
 export const deepMint =
   '0xdeeb7a4662eec9f2f3def03fb937a663dddaa2e215b8078a284d026b7946c270::deep::DEEP';
 export const deepDecimals = 6;
+
+export const PACKAGE_ID = '0xdee9';
+
+export const MODULE_CLOB = 'clob_v2';
+
+export const poolsCacheKey = 'pools';
