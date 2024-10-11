4aadc442a4db5bcd8ce63b31f764f4c89440a95a
Courmont Guillaume <courmontguillaume@gmail.com>
2024-10-11 11:14:01 +0200

    disable failing jobs evm + fix

--- packages/plugins/src/plugins/sushiswap/index.ts
+++ packages/plugins/src/plugins/sushiswap/index.ts
@@ -2,23 +2,23 @@ import { NetworkId, Platform } from '@sonarwatch/portfolio-core';
 import { Fetcher } from '../../Fetcher';
 import { Job } from '../../Job';
 import {
-  avalancheTheGraphV2,
-  bnbTheGraphV2,
-  ethereumTheGraphV2,
+  // avalancheTheGraphV2,
+  // bnbTheGraphV2,
+  // ethereumTheGraphV2,
   platform,
-  polygonTheGraphV2,
+  // polygonTheGraphV2,
 } from './constants';
-import getPoolsJob from '../uniswap-v2/getPoolsJob';
+// import getPoolsJob from '../uniswap-v2/getPoolsJob';
 import getPositionsV2Fetcher from '../uniswap-v2/getPositionsV2Fetcher';
 import aptosPoolsJob from './aptos/poolsJob';
 
 export const platforms: Platform[] = [platform];
 export const jobs: Job[] = [
   aptosPoolsJob,
-  getPoolsJob(NetworkId.ethereum, platform.id, 'v2', ethereumTheGraphV2),
-  getPoolsJob(NetworkId.polygon, platform.id, 'v2', polygonTheGraphV2),
-  getPoolsJob(NetworkId.avalanche, platform.id, 'v2', avalancheTheGraphV2),
-  getPoolsJob(NetworkId.bnb, platform.id, 'v2', bnbTheGraphV2),
+  // getPoolsJob(NetworkId.ethereum, platform.id, 'v2', ethereumTheGraphV2),
+  // getPoolsJob(NetworkId.polygon, platform.id, 'v2', polygonTheGraphV2),
+  // getPoolsJob(NetworkId.avalanche, platform.id, 'v2', avalancheTheGraphV2),
+  // getPoolsJob(NetworkId.bnb, platform.id, 'v2', bnbTheGraphV2),
 ];
 export const fetchers: Fetcher[] = [
   getPositionsV2Fetcher(NetworkId.ethereum, platform.id, 'v2'),
