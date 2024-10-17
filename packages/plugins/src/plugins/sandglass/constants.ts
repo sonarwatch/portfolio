dbbf80be09476ae48f155a2aec6b4c6d0a908869
Courmont Guillaume <courmontguillaume@gmail.com>
2024-10-17 18:41:49 +0200

    improve sandglass display

--- packages/plugins/src/plugins/sandglass/constants.ts
+++ packages/plugins/src/plugins/sandglass/constants.ts
@@ -15,3 +15,11 @@ export const programId = new PublicKey(
   'SANDsy8SBzwUE8Zio2mrYZYqL52Phr2WQb9DDKuXMVK'
 );
 export const marketsInfoKey = 'markets';
+
+export const marketNameByAddress = new Map([
+  ['6SbYW288Kje2WD6TRRcAmikhA76cijBi36y1wYt4RsdN', 'JLP'],
+  ['4K9VeqpZNCVHtZN9mKJpTihp4N8a9LeS35qBnqqM83Et', 'bSOL'],
+  ['Amu99crLdqbfpzrBbfrXHu1myWoCNXrSu7RRkHZJ1Ymv', 'mSOL'],
+  ['5ofpU1rU4ajg3LuHKSMBBWHDubTW1XF7x6tCAXsk5Gju', 'JitoSOL'],
+  ['B8V6WVjPxW1UGwVDfxH2d2r8SyT4cqn7dQRK6XneVa7D', 'cUSDC'],
+]);
--- packages/plugins/src/plugins/sandglass/depositFetcher.ts
+++ packages/plugins/src/plugins/sandglass/depositFetcher.ts
@@ -1,17 +1,18 @@
-import {
-  NetworkId,
-  PortfolioAsset,
-  getUsdValueSum,
-} from '@sonarwatch/portfolio-core';
+import { NetworkId } from '@sonarwatch/portfolio-core';
 import { Cache } from '../../Cache';
 import { Fetcher, FetcherExecutor } from '../../Fetcher';
-import { marketsInfoKey, platformId, programId } from './constants';
+import {
+  marketNameByAddress,
+  marketsInfoKey,
+  platformId,
+  programId,
+} from './constants';
 import { getClientSolana } from '../../utils/clients';
 import { getParsedProgramAccounts } from '../../utils/solana';
 import { sandglassAccountStruct } from './structs';
 import { userAccountFilters } from './filters';
 import { MarketInfo } from './types';
-import tokenPriceToAssetToken from '../../utils/misc/tokenPriceToAssetToken';
+import { ElementRegistry } from '../../utils/elementbuilder/ElementRegistry';
 
 const executor: FetcherExecutor = async (owner: string, cache: Cache) => {
   const client = getClientSolana();
@@ -35,65 +36,24 @@ const executor: FetcherExecutor = async (owner: string, cache: Cache) => {
     marketInfoById.set(marketInfo.pubkey.toString(), marketInfo)
   );
 
-  const mints = marketsInfos
-    .map((marketInfo) => [
-      marketInfo.ptMint,
-      marketInfo.ytMint,
-      marketInfo.lpMint,
-      // marketInfo.syMint,
-    ])
-    .flat();
-  const tokenPriceById = await cache.getTokenPricesAsMap(
-    mints,
-    NetworkId.solana
-  );
+  const registry = new ElementRegistry(NetworkId.solana, platformId);
 
-  const assets: PortfolioAsset[] = [];
   for (const account of accounts) {
     const market = marketInfoById.get(account.marketAccount.toString());
     if (!market) continue;
 
+    const marketName = marketNameByAddress.get(market.pubkey);
+    const element = registry.addElementMultiple({
+      label: 'Staked',
+      name: marketName,
+    });
     const { stakePtAmount, stakeYtAmount, stakeLpAmount } = account.stakeInfo;
-    const ptPrice = tokenPriceById.get(market.ptMint);
-    const ytPrice = tokenPriceById.get(market.ytMint);
-    const lpPrice = tokenPriceById.get(market.lpMint);
-
-    if (ptPrice && !stakePtAmount.isZero()) {
-      const amount = stakePtAmount.dividedBy(10 ** ptPrice.decimals).toNumber();
-      assets.push(
-        tokenPriceToAssetToken(market.ptMint, amount, NetworkId.solana, ptPrice)
-      );
-    }
-
-    if (ytPrice && !stakeYtAmount.isZero()) {
-      const amount = stakeYtAmount.dividedBy(10 ** ytPrice.decimals).toNumber();
-      assets.push(
-        tokenPriceToAssetToken(market.ytMint, amount, NetworkId.solana, ytPrice)
-      );
-    }
-
-    if (lpPrice && !stakeLpAmount.isZero()) {
-      const amount = stakeLpAmount.dividedBy(10 ** lpPrice.decimals).toNumber();
-      assets.push(
-        tokenPriceToAssetToken(market.lpMint, amount, NetworkId.solana, lpPrice)
-      );
-    }
+    element.addAsset({ address: market.ptMint, amount: stakePtAmount });
+    element.addAsset({ address: market.ytMint, amount: stakeYtAmount });
+    element.addAsset({ address: market.lpMint, amount: stakeLpAmount });
   }
 
-  if (assets.length === 0) return [];
-
-  return [
-    {
-      label: 'Staked',
-      networkId: NetworkId.solana,
-      platformId,
-      type: 'multiple',
-      value: getUsdValueSum(assets.map((asset) => asset.value)),
-      data: {
-        assets,
-      },
-    },
-  ];
+  return registry.getElements(cache);
 };
 
 const fetcher: Fetcher = {
