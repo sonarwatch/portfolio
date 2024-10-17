b9114a3d5da72cbb201cb4b40ccc73a79cceff1f
Courmont Guillaume <courmontguillaume@gmail.com>
2024-10-17 17:17:31 +0200

    add job

--- packages/plugins/src/plugins/raydium/index.ts
+++ packages/plugins/src/plugins/raydium/index.ts
@@ -2,6 +2,7 @@ import { Platform } from '@sonarwatch/portfolio-core';
 import { Fetcher } from '../../Fetcher';
 import { Job } from '../../Job';
 // import lpTokensJob from './lpTokensJob';
+import lpTokensApiJob from './ammV2ApiJob';
 import clmmJob from './clmmJob';
 import cpmmJob from './cpmmJob';
 import farmsJob from './farmsJob';
@@ -9,10 +10,5 @@ import { platform } from './constants';
 import farmsFetcher from './farmsFetcher';
 
 export const platforms: Platform[] = [platform];
-export const jobs: Job[] = [
-  // lpTokensJob,
-  farmsJob,
-  clmmJob,
-  cpmmJob,
-];
+export const jobs: Job[] = [lpTokensApiJob, farmsJob, clmmJob, cpmmJob];
 export const fetchers: Fetcher[] = [farmsFetcher];
