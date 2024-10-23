21358452eeec5e20a4ac75f473f01f6fce67b108
Courmont Guillaume <courmontguillaume@gmail.com>
2024-10-23 09:54:10 +0200

    add claimed DBR

--- packages/plugins/src/plugins/debridge/airdropFetcher.ts
+++ packages/plugins/src/plugins/debridge/airdropFetcher.ts
@@ -1,6 +1,7 @@
 import { NetworkId } from '@sonarwatch/portfolio-core';
 import BigNumber from 'bignumber.js';
 import axios, { AxiosResponse } from 'axios';
+import { AccountInfo, PublicKey } from '@solana/web3.js';
 import {
   AirdropFetcher,
   AirdropFetcherExecutor,
@@ -14,18 +15,27 @@ import {
   dbrMint,
   platform as dbrPlatform,
   platform,
+  distributorPid,
+  receiptBuffer,
 } from './constants';
 import { ApiResponse } from './types';
+import { getClientSolana } from '../../utils/clients';
 
 const dbrFactor = new BigNumber(10 ** dbrDecimals);
 const realDistributions = [
-  'Claim 50% vested with bonus in 6 months after TGE',
   'First distribution',
+  'Claim 50% vested with bonus in 6 months after TGE',
 ];
 const executorSolana: AirdropFetcherExecutor = async (owner: string) => {
-  const apiRes: AxiosResponse<ApiResponse> = await axios.get(apiUrl + owner, {
-    timeout: 3000,
-  });
+  const [apiRes, receipt]: [
+    AxiosResponse<ApiResponse>,
+    AccountInfo<Buffer> | null
+  ] = await Promise.all([
+    axios.get(apiUrl + owner, {
+      timeout: 3000,
+    }),
+    getClientSolana().getAccountInfo(getSolPda(owner, 1)),
+  ]);
 
   if (!apiRes.data.distributions || apiRes.data.distributions.length === 0) {
     return getAirdropRaw({
@@ -46,12 +56,13 @@ const executorSolana: AirdropFetcherExecutor = async (owner: string) => {
   for (const distribution of apiRes.data.distributions) {
     if (!realDistributions.includes(distribution.title)) continue;
 
+    const isFirstDis = distribution.title === realDistributions[0];
     const tokens = new BigNumber(distribution.tokens).div(dbrFactor);
 
     items.push({
       amount: tokens.toNumber(),
-      isClaimed: false,
-      label: 'DBR',
+      isClaimed: isFirstDis ? !!receipt : false,
+      label: isFirstDis ? 'DBR' : 'DBR Vested(50%)',
       address: dbrMint,
       imageUri: dbrMint ? undefined : dbrPlatform.image,
     });
@@ -64,9 +75,15 @@ const executorSolana: AirdropFetcherExecutor = async (owner: string) => {
 };
 
 const executorEvm: AirdropFetcherExecutor = async (owner: string) => {
-  const apiRes: AxiosResponse<ApiResponse> = await axios.get(apiUrl + owner, {
-    timeout: 3000,
-  });
+  const [apiRes, receipt]: [
+    AxiosResponse<ApiResponse>,
+    AccountInfo<Buffer> | null
+  ] = await Promise.all([
+    axios.get(apiUrl + owner, {
+      timeout: 3000,
+    }),
+    getClientSolana().getAccountInfo(getEvmPda(owner, 1)),
+  ]);
 
   if (!apiRes.data.distributions || apiRes.data.distributions.length === 0) {
     return getAirdropRaw({
@@ -87,12 +104,13 @@ const executorEvm: AirdropFetcherExecutor = async (owner: string) => {
   for (const distribution of apiRes.data.distributions) {
     if (!realDistributions.includes(distribution.title)) continue;
 
+    const isFirstDis = distribution.title === realDistributions[0];
     const tokens = new BigNumber(distribution.tokens).div(dbrFactor);
 
     items.push({
       amount: tokens.toNumber(),
-      isClaimed: false,
-      label: 'DBR',
+      isClaimed: isFirstDis ? !!receipt : false,
+      label: isFirstDis ? 'DBR' : 'DBR Vested(50%)',
       address: undefined,
       imageUri: dbrMint ? undefined : dbrPlatform.image,
     });
@@ -121,3 +139,21 @@ export const fetcher = airdropFetcherToFetcher(
   'debridge-airdrop',
   firstDistribStatics.claimEnd
 );
+
+function getEvmPda(owner: string, season: number): PublicKey {
+  return PublicKey.findProgramAddressSync(
+    [
+      receiptBuffer,
+      Uint8Array.from([season]),
+      Buffer.from(owner.slice(2), 'hex'),
+    ],
+    distributorPid
+  )[0];
+}
+
+function getSolPda(owner: string, season: number): PublicKey {
+  return PublicKey.findProgramAddressSync(
+    [receiptBuffer, Uint8Array.from([season]), new PublicKey(owner).toBytes()],
+    distributorPid
+  )[0];
+}
