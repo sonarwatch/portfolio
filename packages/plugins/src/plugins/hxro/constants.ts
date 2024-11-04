e126f3e9fac740124566eef264704a8bdf4c2f48
Courmont Guillaume <courmontguillaume@gmail.com>
2024-11-04 18:16:33 +0100

    fix info and job

--- packages/plugins/src/plugins/hxro/constants.ts
+++ packages/plugins/src/plugins/hxro/constants.ts
@@ -1,26 +1,14 @@
 import { Platform } from '@sonarwatch/portfolio-core';
 import { PublicKey } from '@solana/web3.js';
-import { AirdropStatics } from '../../AirdropFetcher';
 
 export const platformId = 'hxro';
 export const platform: Platform = {
   id: platformId,
-  name: 'hxro Finance',
+  name: 'Hxro',
   image: 'https://sonar.watch/img/platforms/hxro.webp',
-  website: 'https://hxro.com/',
-  twitter: 'https://x.com/hxro_finance',
-  defiLlamaId: 'hxro-finance', // from https://defillama.com/docs/api
-};
-export const marketsCacheKey = `markets`;
-
-export const airdropStatics: AirdropStatics = {
-  claimLink: 'https://foo.com/claim',
-  emitterLink: 'https://foo.com',
-  emitterName: 'Foo Protocol',
-  id: 'foo-s1',
-  image: 'https://sonar.watch/img/platforms/foo.webp',
-  claimEnd: undefined,
-  claimStart: 1722672000000,
+  website: 'https://app.hxro.finance/',
+  twitter: 'https://twitter.com/HxroNetwork',
+  defiLlamaId: 'hxro-network', // from https://defillama.com/docs/api
 };
 
 export const pid = new PublicKey(
