639b07c4efd8e93c219f345c47dc7f02cab3a1aa
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2024-11-11 14:28:24 +0800

    add UniTokenAddress type

--- packages/core/src/utils/uniformTokenAddress.ts
+++ packages/core/src/utils/uniformTokenAddress.ts
@@ -12,32 +12,37 @@ import {
   uniformSolanaAddress,
 } from './uniformAddress';
 
-export function uniformBitcoinTokenAddress(address: string) {
+export type UniTokenAddress = string;
+
+export function uniformBitcoinTokenAddress(address: string): UniTokenAddress {
   assertBitcoinTokenAddress(address);
   return address;
 }
 
-export function uniformMoveTokenAddress(address: string) {
+export function uniformMoveTokenAddress(address: string): UniTokenAddress {
   assertMoveTokenAddress(address);
   const splits = address.split('::');
   splits[0] = uniformMoveAddress(splits[0]);
   return splits.join('::');
 }
 
-export function uniformEvmTokenAddress(address: string) {
+export function uniformEvmTokenAddress(address: string): UniTokenAddress {
   return uniformEvmAddress(address);
 }
 
-export function uniformSolanaTokenAddress(address: string) {
+export function uniformSolanaTokenAddress(address: string): UniTokenAddress {
   return uniformSolanaAddress(address);
 }
 
-export function uniformSeiTokenAddress(address: string) {
+export function uniformSeiTokenAddress(address: string): UniTokenAddress {
   assertSeiTokenAddress(address);
   return address;
 }
 
-const uniformers: Record<AddressSystemType, (address: string) => string> = {
+const uniformers: Record<
+  AddressSystemType,
+  (address: string) => UniTokenAddress
+> = {
   [AddressSystem.solana]: uniformSolanaTokenAddress,
   [AddressSystem.bitcoin]: uniformBitcoinTokenAddress,
   [AddressSystem.evm]: uniformEvmTokenAddress,
@@ -45,7 +50,10 @@ const uniformers: Record<AddressSystemType, (address: string) => string> = {
   [AddressSystem.sei]: uniformSeiTokenAddress,
 };
 
-export function uniformTokenAddress(address: string, networkId: NetworkIdType) {
+export function uniformTokenAddress(
+  address: string,
+  networkId: NetworkIdType
+): UniTokenAddress {
   const network = networks[networkId];
   if (!network) throw new Error(`NetworkId not supported: ${networkId}`);
 
@@ -56,7 +64,7 @@ export function uniformTokenAddress(address: string, networkId: NetworkIdType) {
 export function uniformTokenAddressFromAddressSystem(
   address: string,
   addressSystem: AddressSystemType
-) {
+): UniTokenAddress {
   const uniformer = uniformers[addressSystem];
   return uniformer(address);
 }
