0a562d90f3ff9c71c5f9d43c975a991fe706ccbf
Courmont Guillaume <courmontguillaume@gmail.com>
2024-11-14 09:53:55 +0100

    fix

--- packages/plugins/src/plugins/metaplex/index.ts
+++ packages/plugins/src/plugins/metaplex/index.ts
@@ -2,8 +2,8 @@ import { Platform } from '@sonarwatch/portfolio-core';
 import { Fetcher } from '../../Fetcher';
 import { Job } from '../../Job';
 import { platform } from './constants';
-import positionsSolanaFetcher from './positionsSolanaFetcher';
+import resizeNftsFetcher from './resizableNftFetcher';
 
 export const platforms: Platform[] = [platform];
 export const jobs: Job[] = [];
-export const fetchers: Fetcher[] = [positionsSolanaFetcher];
+export const fetchers: Fetcher[] = [resizeNftsFetcher];
