9e4036c51f3860b14658049bd8378458a8bb60b0
opptylabs <oppty@proton.me>
2024-11-19 09:16:28 +0100

    ensofi sol idl

--- packages/plugins/src/plugins/ensofi/constants.ts
+++ packages/plugins/src/plugins/ensofi/constants.ts
@@ -18,3 +18,19 @@ export const LoanType =
   '0x92d5efc1eec7e45b9e806fbc88ba175555dc605e63fa2db9da2b90b8b68f2e5c::loan_registry::Loan';
 export const offersCacheKey = 'offers';
 export const loansCacheKey = 'loans';
+
+export const programId = 'KLend2g3cP87fffoy8q1mQqGKjrxjC8boSyAYavgmjD';
+
+/*
+[
+    {
+        "dataSize": 8624
+    },
+    {
+        "memcmp": {
+            "offset": 32,
+            "bytes": "7u3HeHxYDLhnCoErrtycNokbQYbWGzLs6JSDqGAv5PfF"
+        }
+    }
+]
+ */
