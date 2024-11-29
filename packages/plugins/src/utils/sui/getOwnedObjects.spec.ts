5c8c1cd1eea285376252abfca4527f0aae00b43a
opptylabs <oppty@proton.me>
2024-11-29 13:02:26 +0100

    all getOwnedObjects and then filter

--- packages/plugins/src/utils/sui/getOwnedObjects.spec.ts
+++ packages/plugins/src/utils/sui/getOwnedObjects.spec.ts
@@ -1,18 +1,111 @@
+import { SuiObjectDataFilter } from '@mysten/sui/src/client/types/generated';
 import { getClientSui } from '../clients';
 import { getOwnedObjects } from './getOwnedObjects';
 
 describe('getOwnedObjects', () => {
-  it('should getOwnedObjects', async () => {
-    const client = getClientSui();
-    const objects = await getOwnedObjects(
-      client,
-      '0x0123456789012345678901234567890123456789012345678901234567891234',
-      {
-        options: {
-          showType: true,
+  const client = getClientSui();
+
+  const owner =
+    '0xcfa90668e446e80d0172ec7e9f9f0e80d48045bbb1817616552060574e049866';
+  const structTypeExample1 =
+    '0xba0dd78bdd5d1b5f02a689444522ea9a79e1bc4cd4d8e6a57b59f3fbcae5e978::farm::StakeReceipt';
+  const moveModulePackage =
+    '0xefe8b36d5b2e43728cc323298626b83177803521d195cfb11e15b910e892fddf';
+  const MoveModuleModule = 'obligation';
+  const structTypeScallopObligation = `${moveModulePackage}::${MoveModuleModule}::ObligationKey`;
+
+  const filters: (SuiObjectDataFilter | null)[] = [
+    null,
+    {
+      MoveModule: {
+        package: moveModulePackage,
+        module: MoveModuleModule,
+      },
+    },
+    {
+      Package: moveModulePackage,
+    },
+    {
+      StructType: structTypeScallopObligation,
+    },
+    {
+      MatchAny: [
+        {
+          StructType: structTypeExample1,
         },
-      }
-    );
-    expect(objects.length).toEqual(0);
+        {
+          StructType: structTypeScallopObligation,
+        },
+      ],
+    },
+    {
+      MatchNone: [
+        {
+          StructType: structTypeExample1,
+        },
+        {
+          StructType: structTypeScallopObligation,
+        },
+      ],
+    },
+    {
+      MatchAll: [
+        {
+          MoveModule: {
+            package: moveModulePackage,
+            module: MoveModuleModule,
+          },
+        },
+        {
+          StructType: structTypeScallopObligation,
+        },
+      ],
+    },
+  ];
+
+  it('should getOwnedObjects with right filters', async () => {
+    for (let i = 0; i < filters.length; i++) {
+      const filter = filters[i];
+
+      const filterWithoutCoin = filter
+        ? {
+            MatchAll: [
+              {
+                MatchNone: [
+                  {
+                    StructType: '0x2::coin::Coin',
+                  },
+                ],
+              },
+              filter,
+            ],
+          }
+        : {
+            MatchNone: [
+              {
+                StructType: '0x2::coin::Coin',
+              },
+            ],
+          };
+
+      const objectsFromRpc = await getOwnedObjects(
+        client,
+        owner,
+        {
+          filter: filterWithoutCoin,
+        },
+        true
+      );
+      const objectsFromCache = await getOwnedObjects(
+        client,
+        owner,
+        {
+          filter,
+        },
+        false
+      );
+
+      expect(objectsFromRpc.length).toEqual(objectsFromCache.length);
+    }
   });
 });
--- packages/plugins/src/utils/sui/getOwnedObjects.ts
+++ packages/plugins/src/utils/sui/getOwnedObjects.ts
@@ -1,19 +1,24 @@
-import { SuiObjectResponseQuery } from '@mysten/sui/client';
+import { SuiObjectResponseQuery, SuiObjectResponse } from '@mysten/sui/client';
+import TTLCache from '@isaacs/ttlcache';
+import { SuiObjectDataFilter } from '@mysten/sui/src/client/types/generated';
 import { SuiClient } from '../clients/types';
 import { ObjectResponse } from './types';
 
 const maxPage = 25;
 
-export async function getOwnedObjects<K>(
+type OwnedObjectsPromise = Promise<SuiObjectResponse[]>;
+const ownedObjectsByOwnerCache = new TTLCache({ max: 1000, ttl: 3000 });
+
+async function fetchAllOwnedObjects(
   client: SuiClient,
   owner: string,
   query?: SuiObjectResponseQuery
-): Promise<ObjectResponse<K>[]> {
+): Promise<SuiObjectResponse[]> {
   let page = 0;
   let hasNextPage = true;
   let cursor: string | null | undefined;
 
-  const objects: ObjectResponse<K>[] = [];
+  const objects: SuiObjectResponse[] = [];
 
   do {
     const res = await client.getOwnedObjects({
@@ -26,10 +31,138 @@ export async function getOwnedObjects<K>(
       filter: query?.filter,
       cursor,
     });
-    objects.push(...(res.data as ObjectResponse<K>[]));
+    objects.push(...res.data);
     cursor = res.nextCursor;
     hasNextPage = res.hasNextPage;
     page += 1;
   } while (hasNextPage && page <= maxPage);
+
   return objects;
 }
+
+async function getAllOwnedObjects(
+  client: SuiClient,
+  owner: string
+): OwnedObjectsPromise {
+  let ownedObjectsPromise = ownedObjectsByOwnerCache.get(
+    owner
+  ) as OwnedObjectsPromise;
+
+  if (!ownedObjectsPromise) {
+    ownedObjectsPromise = fetchAllOwnedObjects(client, owner, {
+      filter: {
+        MatchNone: [
+          {
+            StructType: '0x2::coin::Coin',
+          },
+        ],
+      },
+    });
+    ownedObjectsByOwnerCache.set(owner, ownedObjectsPromise);
+  }
+
+  return ownedObjectsPromise;
+}
+
+export async function getOwnedObjects<K>(
+  client: SuiClient,
+  owner: string,
+  query?: SuiObjectResponseQuery,
+  forceRpcCall?: boolean
+): Promise<ObjectResponse<K>[]> {
+  if (forceRpcCall)
+    return fetchAllOwnedObjects(client, owner, query) as Promise<
+      ObjectResponse<K>[]
+    >;
+  const objects = await getAllOwnedObjects(client, owner);
+  return objects.filter(
+    getFilter(query?.filter as MySuiObjectDataFilter)
+  ) as ObjectResponse<K>[];
+}
+
+function getFilter(
+  filter?: MySuiObjectDataFilter | null
+): (object: SuiObjectResponse) => boolean | undefined {
+  if (filter) {
+    if (filter.Package) {
+      return (object: SuiObjectResponse) =>
+        object?.data?.type?.startsWith(`${filter.Package}::`);
+    }
+
+    if (filter.MoveModule) {
+      return (object: SuiObjectResponse) =>
+        filter.MoveModule &&
+        object?.data?.type?.startsWith(
+          `${filter.MoveModule.package}::${filter.MoveModule.module}::`
+        );
+    }
+
+    if (filter.StructType) {
+      return (object: SuiObjectResponse) =>
+        object?.data?.type === filter.StructType;
+    }
+
+    if (filter.ObjectId) {
+      return (object: SuiObjectResponse) =>
+        object?.data?.objectId === filter.ObjectId;
+    }
+
+    if (filter.ObjectIds) {
+      return (object: SuiObjectResponse) =>
+        !!object?.data?.objectId &&
+        filter.ObjectIds &&
+        filter.ObjectIds.includes(object.data.objectId);
+    }
+
+    if (filter.Version) {
+      return (object: SuiObjectResponse) =>
+        object?.data?.objectId === filter.ObjectId;
+    }
+
+    if (filter.MatchAll) {
+      return (object: SuiObjectResponse) => {
+        for (const f of filter.MatchAll || []) {
+          const fn = getFilter(f as MySuiObjectDataFilter);
+          if (!fn(object)) return false;
+        }
+        return true;
+      };
+    }
+
+    if (filter.MatchNone) {
+      return (object: SuiObjectResponse) => {
+        for (const f of filter.MatchNone || []) {
+          const fn = getFilter(f as MySuiObjectDataFilter);
+          if (fn(object)) return false;
+        }
+        return true;
+      };
+    }
+
+    if (filter.MatchAny) {
+      return (object: SuiObjectResponse) => {
+        for (const f of filter.MatchAny || []) {
+          const fn = getFilter(f as MySuiObjectDataFilter);
+          if (fn(object)) return true;
+        }
+        return false;
+      };
+    }
+  }
+  return () => true;
+}
+
+type MySuiObjectDataFilter = {
+  MatchAll?: SuiObjectDataFilter[];
+  MatchAny?: SuiObjectDataFilter[];
+  MatchNone?: SuiObjectDataFilter[];
+  Package?: string;
+  MoveModule?: {
+    module: string;
+    package: string;
+  };
+  StructType?: string;
+  ObjectId?: string;
+  ObjectIds?: string[];
+  Version?: string;
+};
