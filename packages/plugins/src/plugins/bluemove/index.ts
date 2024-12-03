13e44e6a5f3ed15c79e59102eebab13f6814f3aa
opptylabs <oppty@proton.me>
2024-12-03 15:51:42 +0100

    bluemove locked lp

--- packages/plugins/src/plugins/bluemove/index.ts
+++ packages/plugins/src/plugins/bluemove/index.ts
@@ -3,7 +3,9 @@ import { Fetcher } from '../../Fetcher';
 import { Job } from '../../Job';
 import { platform } from './constants';
 import poolsJob from './poolsJob';
+import lockLpJob from './lockLpJob';
+import lockLpFetcher from './lockLpFetcher';
 
 export const platforms: Platform[] = [platform];
-export const jobs: Job[] = [poolsJob];
-export const fetchers: Fetcher[] = [];
+export const jobs: Job[] = [poolsJob, lockLpJob];
+export const fetchers: Fetcher[] = [lockLpFetcher];
