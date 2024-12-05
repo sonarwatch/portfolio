76e888b163469481ac469a74bba942e45358fbca
opptylabs <oppty@proton.me>
2024-12-05 11:30:01 +0100

    wormhole staking

--- packages/plugins/src/plugins/wormhole/constants.ts
+++ packages/plugins/src/plugins/wormhole/constants.ts
@@ -1,14 +1,18 @@
 import { Platform } from '@sonarwatch/portfolio-core';
+import { PublicKey } from '@solana/web3.js';
 
 export const platformId = 'wormhole';
 export const platform: Platform = {
   id: platformId,
   name: 'Wormhole',
   image: 'https://sonar.watch/img/platforms/wormhole.webp',
-  website: 'https://airdrop.wormhole.com/',
+  website: 'https://w.wormhole.com/',
   twitter: 'https://twitter.com/wormhole',
 };
 
 export const apiUrl = 'https://prod-flat-files-min.wormhole.com/';
 export const wMint = '85VBFQZC9TZkfaptBWjvUw7YbZjy52A6mjtPGjstQAmQ';
-export const wDecimal = 6;
+
+export const stakingProgramId = new PublicKey(
+  'sspu65omPW2zJGWDxmx8btqxudHezoQHSGZmnW8jbVz'
+);
---
+++ packages/plugins/src/plugins/wormhole/stakingFetcher.ts
@@ -0,0 +1,49 @@
+import { NetworkId } from '@sonarwatch/portfolio-core';
+import { PublicKey } from '@solana/web3.js';
+import { Cache } from '../../Cache';
+import { Fetcher, FetcherExecutor } from '../../Fetcher';
+import { platformId, stakingProgramId, wMint } from './constants';
+import { getClientSolana } from '../../utils/clients';
+import { ElementRegistry } from '../../utils/elementbuilder/ElementRegistry';
+import { getParsedMultipleAccountsInfo } from '../../utils/solana';
+import { tokenBalanceStruct } from './structs';
+
+const executor: FetcherExecutor = async (owner: string, cache: Cache) => {
+  const client = getClientSolana();
+
+  const tokenBalances = await getParsedMultipleAccountsInfo(
+    client,
+    tokenBalanceStruct,
+    [
+      PublicKey.findProgramAddressSync(
+        [Buffer.from('token_balance'), new PublicKey(owner).toBytes()],
+        stakingProgramId
+      )[0],
+    ]
+  );
+
+  if (!tokenBalances.length) return [];
+
+  const elementRegistry = new ElementRegistry(NetworkId.solana, platformId);
+  const element = elementRegistry.addElementMultiple({
+    label: 'Staked',
+  });
+
+  tokenBalances.forEach((tokenBalance) => {
+    if (!tokenBalance) return;
+    element.addAsset({
+      address: wMint,
+      amount: tokenBalance?.balance,
+    });
+  });
+
+  return elementRegistry.getElements(cache);
+};
+
+const fetcher: Fetcher = {
+  id: `${platformId}-staking`,
+  networkId: NetworkId.solana,
+  executor,
+};
+
+export default fetcher;
