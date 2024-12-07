7d8a4444731da5d1ef7a6411a6af803ba2017cac
Guillaume <courmontguillaume@gmail.com>
2024-12-07 11:37:49 +0100

    add photo finish

---
+++ packages/plugins/src/plugins/photofinish/constants.ts
@@ -0,0 +1,14 @@
+import { Platform } from '@sonarwatch/portfolio-core';
+
+export const platformId = 'photofinish';
+export const platform: Platform = {
+  id: platformId,
+  name: 'Photo Finish',
+  image: 'https://sonar.watch/img/platforms/photofinish.webp',
+  website: 'https://photofinish.live/',
+  twitter: 'https://x.com/photofinishgame',
+};
+
+export const apiUrl =
+  'https://api.photofinish.live/users/external/staked-crown/';
+export const crownMint = 'GDfnEsia2WLAW5t8yx2X5j2mkfA74i5kwGdDuZHt7XmG';
