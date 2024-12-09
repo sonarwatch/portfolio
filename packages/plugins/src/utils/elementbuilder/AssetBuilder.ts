d39886849fc941ff2c9b12977287e2640f1415f6
opptylabs <oppty@proton.me>
2024-12-09 16:03:57 +0100

    elementRegistry with generic assets

--- packages/plugins/src/utils/elementbuilder/AssetBuilder.ts
+++ packages/plugins/src/utils/elementbuilder/AssetBuilder.ts
@@ -1,70 +1,10 @@
-import {
-  NetworkIdType,
-  PortfolioAsset,
-  PortfolioAssetAttributes,
-} from '@sonarwatch/portfolio-core';
-import BigNumber from 'bignumber.js';
-import { PortfolioAssetParams } from './PortfolioAssetParams';
-import tokenPriceToAssetToken from '../misc/tokenPriceToAssetToken';
+import { NetworkIdType, PortfolioAsset } from '@sonarwatch/portfolio-core';
 import { TokenPriceMap } from '../../TokenPriceMap';
-import tokenPriceToAssetTokens from '../misc/tokenPriceToAssetTokens';
 
-export class AssetBuilder {
-  address: string;
-  amount: number | BigNumber | string;
-  attributes: PortfolioAssetAttributes;
-  alreadyShifted: boolean;
-
-  constructor(params: PortfolioAssetParams) {
-    this.address = params.address.toString();
-    this.amount = params.amount;
-    this.attributes = params.attributes || {};
-    this.alreadyShifted = params.alreadyShifted || false;
-  }
-
-  getUnderlyings(
+export abstract class AssetBuilder {
+  abstract mints(): string[];
+  abstract get(
     networkId: NetworkIdType,
     tokenPrices: TokenPriceMap
-  ): PortfolioAsset[] {
-    const tokenPrice = tokenPrices.get(this.address);
-    if (!tokenPrice) return [];
-
-    let amount = new BigNumber(this.amount);
-
-    if (amount.isZero() && tokenPrice.underlyings) return [];
-
-    if (!this.alreadyShifted)
-      amount = amount.dividedBy(10 ** tokenPrice.decimals);
-
-    return tokenPriceToAssetTokens(
-      tokenPrice.address,
-      amount.toNumber(),
-      networkId,
-      tokenPrice,
-      undefined,
-      this.attributes
-    );
-  }
-
-  get(
-    networkId: NetworkIdType,
-    tokenPrices: TokenPriceMap
-  ): PortfolioAsset | null {
-    let amount = new BigNumber(this.amount);
-    if (amount.isZero()) return null;
-    const tokenPrice = tokenPrices.get(this.address);
-    if (!tokenPrice) return null;
-
-    if (!this.alreadyShifted)
-      amount = amount.dividedBy(10 ** tokenPrice.decimals);
-
-    return tokenPriceToAssetToken(
-      tokenPrice.address,
-      amount.toNumber(),
-      networkId,
-      tokenPrice,
-      undefined,
-      this.attributes
-    );
-  }
+  ): PortfolioAsset | null;
 }
