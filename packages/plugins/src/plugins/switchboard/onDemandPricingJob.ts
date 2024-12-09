0ccbecfcbbbd6f218b08ae54ff41e83cf103cc99
Courmont Guillaume <courmontguillaume@gmail.com>
2024-12-09 18:20:23 +0100

    add switchboard pricing (on demand)

---
+++ packages/plugins/src/plugins/switchboard/onDemandPricingJob.ts
@@ -0,0 +1,74 @@
+import {
+  NetworkId,
+  solanaNativeWrappedAddress,
+  TokenPriceSource,
+} from '@sonarwatch/portfolio-core';
+import { PublicKey } from '@solana/web3.js';
+import { Cache } from '../../Cache';
+import { Job, JobExecutor } from '../../Job';
+import { platformId } from './constants';
+import { getClientSolana } from '../../utils/clients';
+import { walletTokensPlatform } from '../tokens/constants';
+import { getSwitchboardOnDemandPrices } from './helpers/getSwitchboardOnDemandPrice';
+import { getDecimalsForToken } from '../../utils/misc/getCachedDecimalsForToken';
+import { SwitchboardTokenPricingInfo } from './types';
+import { usdcSolanaMint } from '../../utils/solana';
+
+const feedsToFetch: SwitchboardTokenPricingInfo[] = [
+  {
+    feedsAddresses: [
+      'Ceveqpim1FJZfx9DPeFDVDSz2HJavUqPPEJtZ2osNEmS',
+      'E8TLLh5jkYDvSXfAES7qe3s8Cfjj4hyvjksuvUHe8NEw',
+      'BSzfJs4d1tAkSDqkepnfzEVcx2WtDVnwwXa2giy9PLeP',
+    ],
+    networkId: NetworkId.solana,
+    mintAddress: solanaNativeWrappedAddress,
+  },
+  {
+    feedsAddresses: ['2Dv72h14k6ynrUvQ8mhu5ihwrk1Kte6Aj2tpKhXLsHrR'],
+    mintAddress: usdcSolanaMint,
+    networkId: NetworkId.solana,
+  },
+];
+
+const executor: JobExecutor = async (cache: Cache) => {
+  const connection = getClientSolana();
+  const sources: TokenPriceSource[] = [];
+
+  for (const tokenInfo of feedsToFetch) {
+    const values = await getSwitchboardOnDemandPrices(
+      connection,
+      tokenInfo.feedsAddresses.map((t) => new PublicKey(t))
+    );
+
+    const decimals = await getDecimalsForToken(
+      tokenInfo.mintAddress,
+      NetworkId.solana
+    );
+
+    tokenInfo.feedsAddresses.forEach((feedToFetch, i) => {
+      const price = values.at(i);
+      if (!price || !decimals) return;
+
+      const source: TokenPriceSource = {
+        address: tokenInfo.mintAddress,
+        decimals,
+        id: `${platformId}-feed-${feedToFetch.toString()}`,
+        networkId: tokenInfo.networkId,
+        platformId: walletTokensPlatform.id,
+        weight: 0.8,
+        timestamp: Date.now(),
+        price,
+      };
+      sources.push(source);
+    });
+  }
+
+  await cache.setTokenPriceSources(sources);
+};
+const job: Job = {
+  id: `${platformId}-ondemand-pricing`,
+  executor,
+  label: 'realtime',
+};
+export default job;
