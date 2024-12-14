c48f44c598a6ae5085f64f235de85c10cd418b6d
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2024-12-14 11:21:27 +0800

    wip

---
+++ packages/plugins/src/plugins/superposition/constants.ts
@@ -0,0 +1,18 @@
+import { Platform } from '@sonarwatch/portfolio-core';
+
+export const platformId = 'superposition';
+export const platform: Platform = {
+  id: platformId,
+  name: 'Superposition',
+  image: 'https://sonar.watch/img/platforms/superposition.webp',
+  website: 'https://www.superposition.finance/',
+  twitter: 'https://x.com/superpositionfi',
+  defiLlamaId: 'superposition', // from https://defillama.com/docs/api
+};
+export const marketsCacheKey = `markets`;
+
+export const packageId =
+  '0xccd1a84ccea93531d7f165b90134aa0415feb30e8757ab1632dac68c0055f5c2';
+export const portfolioType = `${packageId}::portfolio::Portfolio`;
+export const collateralPositionType = `${packageId}::portfolio::PositionView`;
+export const liabilityPositionType = `${packageId}::portfolio::SupplyPosition`;
---
+++ packages/plugins/src/plugins/superposition/index.ts
@@ -0,0 +1,10 @@
+import { Platform } from '@sonarwatch/portfolio-core';
+import { Fetcher } from '../../Fetcher';
+import { Job } from '../../Job';
+import { platform } from './constants';
+import marketsJob from './marketsJob';
+import lendingFetcher from './lendingFetcher';
+
+export const platforms: Platform[] = [platform];
+export const jobs: Job[] = [marketsJob];
+export const fetchers: Fetcher[] = [lendingFetcher];
