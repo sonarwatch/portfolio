bd1924db7f6e65811adc5834d4897a882592ac56
Courmont Guillaume <courmontguillaume@gmail.com>
2024-12-16 12:37:15 +0100

    add nclb allocation

--- packages/plugins/src/plugins/sanctum/index.ts
+++ packages/plugins/src/plugins/sanctum/index.ts
@@ -8,8 +8,15 @@ import {
   fetcher as s1Fetcher,
 } from './s1AirdropFetcher';
 import { AirdropFetcher } from '../../AirdropFetcher';
+import {
+  airdropFetcher as nclbAirdropFetcher,
+  fetcher as nclbFetcher,
+} from './nclbAirdropFetcher';
 
 export const platforms: Platform[] = [platform];
 export const jobs: Job[] = [lstsJob];
-export const fetchers: Fetcher[] = [s1Fetcher];
-export const airdropFetchers: AirdropFetcher[] = [s1AirdropFetcher];
+export const fetchers: Fetcher[] = [s1Fetcher, nclbFetcher];
+export const airdropFetchers: AirdropFetcher[] = [
+  s1AirdropFetcher,
+  nclbAirdropFetcher,
+];
