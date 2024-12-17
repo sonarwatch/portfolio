11077a4f625dcdf32b98bb32df437d051125bdd5
Courmont Guillaume <courmontguillaume@gmail.com>
2024-12-17 10:22:08 +0100

    add suins & deepbook normal fetcher

--- packages/plugins/src/plugins/deepbook/index.ts
+++ packages/plugins/src/plugins/deepbook/index.ts
@@ -2,7 +2,7 @@ import { Platform } from '@sonarwatch/portfolio-core';
 import { Fetcher } from '../../Fetcher';
 import { Job } from '../../Job';
 import { platform } from './constants';
-import { airdropFetcher } from './airdropFetcher';
+import { airdropFetcher, fetcher as s1Fetcher } from './airdropFetcher';
 import poolsV2Job from './v2/poolsJob';
 import depositsV2Fetcher from './v2/depositsFetcher';
 import poolsV3Job from './v3/poolsJob';
@@ -10,5 +10,9 @@ import depositsV3Fetcher from './v3/depositsFetcher';
 
 export const platforms: Platform[] = [platform];
 export const jobs: Job[] = [poolsV2Job, poolsV3Job];
-export const fetchers: Fetcher[] = [depositsV2Fetcher, depositsV3Fetcher];
+export const fetchers: Fetcher[] = [
+  depositsV2Fetcher,
+  depositsV3Fetcher,
+  s1Fetcher,
+];
 export { airdropFetcher };
