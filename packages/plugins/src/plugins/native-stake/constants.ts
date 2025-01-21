b44ead0663a9f64d7ea389680999cbb3a3694e0f
Guillaume <courmontguillaume@gmail.com>
2025-01-21 11:05:54 +0100

    change name

--- packages/plugins/src/plugins/native-stake/constants.ts
+++ packages/plugins/src/plugins/native-stake/constants.ts
@@ -3,7 +3,7 @@ import { Platform } from '@sonarwatch/portfolio-core';
 export const platformId = 'native-stake';
 export const nativeStakePlatform: Platform = {
   id: platformId,
-  name: 'Native Stake',
+  name: 'Validators',
   image: 'https://sonar.watch/img/platforms/native-stake.webp',
 };
 export const validatorsKey = 'activeValidators';
