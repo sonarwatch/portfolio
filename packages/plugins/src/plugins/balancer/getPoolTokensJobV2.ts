e54002e98672816fcb52aa280ec38b04ce4b13ff
jtardioli <joshua.tardioli@gmail.com>
2025-02-07 17:49:42 -0300

    save progress pre balancer tokens script

--- packages/plugins/src/plugins/balancer/getPoolTokensJobV2.ts
+++ packages/plugins/src/plugins/balancer/getPoolTokensJobV2.ts
@@ -1,7 +1,34 @@
 import { Job, JobExecutor } from '../../Job';
-import { platformId } from './constants';
+import { balancerApiNetworkByNetworkId, platformId } from './constants';
 
-const executor: JobExecutor = async () => {};
+const executor: JobExecutor = async () => {
+  for (const x of Object.entries(balancerApiNetworkByNetworkId)) {
+    console.log(x);
+  }
+  // const network = networks[networkId];
+  // const tokenList: AxiosResponse<UniTokenList> | null = await axios
+  //   .get(network.tokenListUrl)
+  //   .catch(() => null);
+  // if (!tokenList) return;
+
+  // for (let i = 0; i < tokenList.data.tokens.length; i++) {
+  //   const token = tokenList.data.tokens[i];
+  //   const address = formatTokenAddress(token.address, networkId);
+  //   const fToken = {
+  //     ...token,
+  //     address,
+  //   };
+  //   await cache.setItem(address, fToken, {
+  //     prefix: CACHE_KEYS.TOKEN_LIST_INFO_PREFIX,
+  //     networkId,
+  //     ttl,
+  //   });
+  // }
+  // await cache.setItem(networkId, tokenList.data, {
+  //   prefix: tokenListsPrefix,
+  //   ttl,
+  // });
+};
 const job: Job = {
   id: `${platformId}-pool-tokens-v2`,
   executor,
