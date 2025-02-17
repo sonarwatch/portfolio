9ddae1e4f62bbc1af98a202284b61f35d7b25fd8
jtardioli <joshua.tardioli@gmail.com>
2025-02-17 14:43:27 -0300

    add fetcher scafold

--- packages/plugins/src/plugins/convex/index.ts
+++ packages/plugins/src/plugins/convex/index.ts
@@ -1,8 +1,9 @@
-import { Platform } from '@sonarwatch/portfolio-core';
+import { NetworkId, Platform } from '@sonarwatch/portfolio-core';
 import { Fetcher } from '../../Fetcher';
 import { Job } from '../../Job';
 import { platform } from './constants';
+import fetcher from './fetcher';
 
 export const platforms: Platform[] = [platform];
 export const jobs: Job[] = [];
-export const fetchers: Fetcher[] = [];
+export const fetchers: Fetcher[] = [fetcher(NetworkId.fraxtal)];
