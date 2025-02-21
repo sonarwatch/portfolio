93550621ad015d545286647a88bdd9e70e3fe3ed
opptylabs <oppty@proton.me>
2025-02-21 14:16:26 +0100

    baskt deprecated

--- packages/plugins/src/plugins/baskt/index.ts
+++ packages/plugins/src/plugins/baskt/index.ts
@@ -2,8 +2,7 @@ import { Platform } from '@sonarwatch/portfolio-core';
 import { Fetcher } from '../../Fetcher';
 import { Job } from '../../Job';
 import { platform } from './constants';
-import marketsJob from './marketsJob';
 
 export const platforms: Platform[] = [platform];
-export const jobs: Job[] = [marketsJob];
+export const jobs: Job[] = [];
 export const fetchers: Fetcher[] = [];
