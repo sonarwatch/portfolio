84f0c45a8be4a3ec76e313d94875f3128b878607
opptylabs <oppty@proton.me>
2025-02-25 15:14:43 +0100

    ParsedGpaBuilder helper

---
+++ packages/plugins/src/utils/solana/beets/ParsedGpa.ts
@@ -0,0 +1,54 @@
+import { GpaBuilder } from '@metaplex-foundation/beet-solana';
+import { Connection, PublicKey } from '@solana/web3.js';
+import { BeetField } from '@metaplex-foundation/beet';
+import { GlobalBeetStruct, ParsedAccount } from '../types';
+
+export class ParsedGpa<T> {
+  protected gpaBuilder;
+
+  static build<T>(
+    connection: Connection,
+    beets: GlobalBeetStruct<T>,
+    programId: PublicKey
+  ) {
+    return new ParsedGpa<T>(connection, programId, beets);
+  }
+
+  private constructor(
+    protected readonly connection: Connection,
+    programId: PublicKey,
+    protected beets: GlobalBeetStruct<T>
+  ) {
+    this.gpaBuilder = GpaBuilder.fromStruct<T>(
+      programId,
+      beets as { fields: BeetField<T, T[keyof T]>[] }
+    );
+  }
+
+  addInnerFilter(keys: string, val: T[keyof T] & NonNullable<unknown>) {
+    this.gpaBuilder.addInnerFilter(keys, val);
+    return this;
+  }
+
+  addFilter(key: keyof T & string, val: T[keyof T]) {
+    this.gpaBuilder.addFilter(key, val);
+    return this;
+  }
+
+  dataSize(size: number) {
+    this.gpaBuilder.dataSize(size);
+    return this;
+  }
+
+  async run() {
+    const accounts = await this.gpaBuilder.run(this.connection);
+    return accounts.map(
+      (account) =>
+        ({
+          pubkey: account.pubkey,
+          lamports: account.account.lamports,
+          ...this.beets.deserialize(account.account.data)[0],
+        } as ParsedAccount<T>)
+    );
+  }
+}
