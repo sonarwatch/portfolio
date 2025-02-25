2aad0953a8ea225cf72356cc97ff8a5ab3bc1c15
Aleks CP <me@alekscp.com>
2025-02-25 17:00:37 +0100

    Adapt to post debug

--- tools/scripts/publish-rewrite-name.mjs
+++ tools/scripts/publish-rewrite-name.mjs
@@ -14,8 +14,7 @@ pkgJson.name = pkgJson.name.replace('@sonarwatch/', '@octavlabs/');
 
 writeFileSync(pkgPath, JSON.stringify(pkgJson, null, 2), 'utf8');
 
-// execSync('npm publish --verbose --access public --otp 123456', {
-execSync('npm publish --verbose --access restricted', {
+execSync('npm publish', {
   cwd: distDir,
   stdio: 'inherit',
 });
