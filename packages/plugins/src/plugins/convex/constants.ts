3e871fa27136eb5db53b0fdd6b481ab57608e107
BitWonka <79578887+BitWonka@users.noreply.github.com>
2025-03-29 06:24:30 +0200

    fix build

--- packages/plugins/src/plugins/convex/constants.ts
+++ packages/plugins/src/plugins/convex/constants.ts
@@ -1,13 +1,4 @@
-import { Platform } from '@sonarwatch/portfolio-core';
-
 export const platformId = 'convex';
-export const platform: Platform = {
-  id: platformId,
-  name: 'Convex',
-  image: 'https://icons.llama.fi/convex-finance.jpg',
-  defiLlamaId: 'convex-finance', // from https://defillama.com/docs/api
-  website: 'https://www.convexfinance.com/',
-};
 
 export const stakedCvxFXSAddress = '0x8c279F6Bfa31c47F29e5d05a68796f2A6c216892';
 export const wFXBAddress = '0x8301A2C86615Edf08d8980ECCca8287322423390';
