b2426527cced2265605bfa236f5f2e71c89e7411
jtardioli <joshua.tardioli@gmail.com>
2025-03-31 19:40:13 -0300

    save my place

--- packages/plugins/src/plugins/morpho/index.ts
+++ packages/plugins/src/plugins/morpho/index.ts
@@ -3,9 +3,11 @@ import { Job } from '../../Job';
 import aaveV3CollateralFetcher from './aaveV3CollateralFetcher';
 import aaveV3SupplyOnlyFetcher from './aaveV3SupplyOnlyFetcher';
 import aaveV3Job from './aaveV3Job';
+import { fetcher } from './morphoFetcher';
 
 export const jobs: Job[] = [aaveV3Job];
 export const fetchers: Fetcher[] = [
   aaveV3CollateralFetcher,
   aaveV3SupplyOnlyFetcher,
+  fetcher,
 ];
