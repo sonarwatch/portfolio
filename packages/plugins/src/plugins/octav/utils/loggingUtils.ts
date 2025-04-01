fd772ffa720d1deb9949419bdd4128d320ab7e0f
Charles Ouimet <charles.ouimet@gmail.com>
2025-04-01 13:50:06 -0400

    Refine comment

--- packages/plugins/src/plugins/octav/utils/loggingUtils.ts
+++ packages/plugins/src/plugins/octav/utils/loggingUtils.ts
@@ -9,8 +9,8 @@ export interface LoggingContext {
   
 /**
  * Logs a message with structured context information only when OCTAV_VERBOSE_LOGGING is enabled.
- * The below solution is not as fancy as supporting logger injection, but I really wanted to get logging working quickly
- * while I was working on protocol integrations.
+ * The below solution is not as fancy as supporting logger injection or eventing, but I really wanted
+ * to get logging working quickly while I was working on protocol integrations.
  *
  * Ref: https://octavfi.slack.com/archives/C08KX01HLMS/p1743515897984209?thread_ts=1743511676.283229&cid=C08KX01HLMS
  */
