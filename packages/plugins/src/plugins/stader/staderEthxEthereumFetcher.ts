58f453aed5f3760eeb399e51e5f9bec5b47237fb
Charles Ouimet <charles.ouimet@gmail.com>
2025-04-01 12:17:07 -0400

    Decreased potential copy-pasta issues by defining an array of targets to retrieve balances from.

--- packages/plugins/src/plugins/stader/staderEthxEthereumFetcher.ts
+++ packages/plugins/src/plugins/stader/staderEthxEthereumFetcher.ts
@@ -21,21 +21,23 @@ const DECIMALS_ON_CONTRACT = 18;
 const NETWORK_ID = NetworkId.ethereum;
 
 const executor: FetcherExecutor = async (owner: string, cache: Cache) => {
-  const logCtx = { fn: 'staderEthxEthereumFetcher::executor', owner, networkId: NETWORK_ID, contractForBalances: ETHX_CONTRACT_ADDRESS_ETHREUM_MAINNET };
+  const contractsToFetchBalanceFor = [ETHX_CONTRACT_ADDRESS_ETHREUM_MAINNET, STAKING_POOL_MANAGER_ADDRESS_ETHREUM_MAINNET];
+  const logCtx = { fn: 'staderEthxEthereumFetcher::executor', owner, networkId: NETWORK_ID };
 
   verboseLog({ ...logCtx, contractsToFetchBalanceFor }, 'Fetching stader ethx balances');
 
   const balances = await getBalances(
     owner,
-    [ETHX_CONTRACT_ADDRESS_ETHREUM_MAINNET, STAKING_POOL_MANAGER_ADDRESS_ETHREUM_MAINNET],
+    contractsToFetchBalanceFor,
     NETWORK_ID
   );
 
   verboseLog({ ...logCtx, balances }, 'Balances retrieved');
+
   const elements: PortfolioElement[] = [];
   const rawBalance0 = balances.at(0)?.toString();
   if (rawBalance0) {
-    const contractAddress = ETHX_CONTRACT_ADDRESS_ETHREUM_MAINNET;
+    const contractAddress = contractsToFetchBalanceFor[0];
     const contractDecimals = DECIMALS_ON_CONTRACT;
     const amount = new BigNumber(rawBalance0).div(10 ** contractDecimals).toNumber();
 
@@ -68,7 +70,7 @@ const executor: FetcherExecutor = async (owner: string, cache: Cache) => {
 
   const rawBalance1 = balances.at(1)?.toString();
   if (rawBalance1) {
-    const contractAddress = STAKING_POOL_MANAGER_ADDRESS_ETHREUM_MAINNET;
+    const contractAddress = contractsToFetchBalanceFor[1];
     const contractDecimals = DECIMALS_ON_CONTRACT;
 
     const amount = new BigNumber(rawBalance1).div(10 ** contractDecimals).toNumber();
