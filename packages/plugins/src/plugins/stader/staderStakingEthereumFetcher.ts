781eaeec06ecd2f4b5b9e4696eade1d777ac6322
Charles Ouimet <charles.ouimet@gmail.com>
2025-04-02 09:18:18 -0400

    Refactor code to simplify the execution

--- packages/plugins/src/plugins/stader/staderStakingEthereumFetcher.ts
+++ packages/plugins/src/plugins/stader/staderStakingEthereumFetcher.ts
@@ -13,20 +13,19 @@ import {
 import { Cache } from '../../Cache';
 import { getBalances } from '../../utils/evm/getBalances';
 import tokenPriceToAssetToken from '../../utils/misc/tokenPriceToAssetToken';
-import { verboseLog } from '../octav/utils/loggingUtils';
+import { LoggingContext, verboseLog } from '../octav/utils/loggingUtils';
 
 const DECIMALS_ON_CONTRACT = 18;
 const NETWORK_ID = NetworkId.ethereum;
 
-const executor: FetcherExecutor = async (owner: string, cache: Cache) => {
+const fetchStakedEthx = async (
+  owner: string,
+  cache: Cache,
+  logCtx: LoggingContext
+): Promise<PortfolioElement | undefined> => {
   const contractsToFetchBalanceFor = [
     CONTRACT_ADDRESS_ETHX_TOKEN_ETHEREUM_MAINNET,
   ];
-  const logCtx = {
-    fn: 'staderStakingEthereumFetcher::executor',
-    owner,
-    networkId: NETWORK_ID,
-  };
 
   verboseLog(
     { ...logCtx, contractsToFetchBalanceFor },
@@ -41,40 +40,55 @@ const executor: FetcherExecutor = async (owner: string, cache: Cache) => {
 
   verboseLog({ ...logCtx, balances }, 'Balances retrieved');
 
-  const elements: PortfolioElement[] = [];
   const rawBalance0 = balances.at(0)?.toString();
-  if (rawBalance0) {
-    const contractAddress = contractsToFetchBalanceFor[0];
-    const contractDecimals = DECIMALS_ON_CONTRACT;
-    const amount = new BigNumber(rawBalance0)
-      .div(10 ** contractDecimals)
-      .toNumber();
+  if (!rawBalance0) {
+    return undefined;
+  }
 
-    const tokenPrice = await cache.getTokenPrice(contractAddress, NETWORK_ID);
+  const contractAddress = contractsToFetchBalanceFor[0];
+  const contractDecimals = DECIMALS_ON_CONTRACT;
+  const amount = new BigNumber(rawBalance0)
+    .div(10 ** contractDecimals)
+    .toNumber();
 
-    verboseLog(
-      { ...logCtx, contractAddress, amount, tokenPrice },
-      'Token price retrieved from cache'
-    );
+  const tokenPrice = await cache.getTokenPrice(contractAddress, NETWORK_ID);
 
-    const stakedAsset = tokenPriceToAssetToken(
-      contractAddress,
-      amount,
-      NETWORK_ID,
-      tokenPrice
-    );
+  verboseLog(
+    { ...logCtx, contractAddress, amount, tokenPrice },
+    'Token price retrieved from cache'
+  );
 
-    const stakedElement: PortfolioElement = {
-      networkId: NETWORK_ID,
-      label: 'Staked',
-      platformId,
-      type: PortfolioElementType.multiple,
-      value: stakedAsset.value,
-      data: {
-        assets: [stakedAsset],
-      },
-    };
-    elements.push(stakedElement);
+  const stakedAsset = tokenPriceToAssetToken(
+    contractAddress,
+    amount,
+    NETWORK_ID,
+    tokenPrice
+  );
+
+  return {
+    networkId: NETWORK_ID,
+    label: 'Staked',
+    platformId,
+    type: PortfolioElementType.multiple,
+    value: stakedAsset.value,
+    data: {
+      assets: [stakedAsset],
+    },
+  };
+};
+
+const executor: FetcherExecutor = async (owner: string, cache: Cache) => {
+  const logCtx = {
+    fn: 'staderStakingEthereumFetcher::executor',
+    owner,
+    networkId: NETWORK_ID,
+  };
+
+  const elements: PortfolioElement[] = [];
+
+  const stakedEthx = await fetchStakedEthx(owner, cache, logCtx);
+  if (stakedEthx) {
+    elements.push(stakedEthx);
   }
 
   return elements;
