ca6b1b863fc224290fa075d914a0b9c2455306c8
Noemie Sarraillon <noemiesarraillon@gmail.com>
2025-04-02 10:58:10 -0400

    add helper functions

--- packages/plugins/src/plugins/lido/stakedFetcher.ts
+++ packages/plugins/src/plugins/lido/stakedFetcher.ts
@@ -2,7 +2,6 @@ import { ethereumNetwork, getUsdValueSum } from '@sonarwatch/portfolio-core';
 import BigNumber from 'bignumber.js';
 import { Cache } from '../../Cache';
 import {
-  maticTokenAddress,
   platformId,
   stakedAddresses,
   wstETHAddress,
@@ -13,64 +12,8 @@ import { Fetcher, FetcherExecutor } from '../../Fetcher';
 import { getEvmClient } from '../../utils/clients';
 import tokenPriceToAssetToken from '../../utils/misc/tokenPriceToAssetToken';
 import { ethFactor } from '../../utils/evm/constants';
-import { maticAbi, wstETHAbi } from './abis';
 import { getBalances } from '../../utils/evm/getBalances';
-import { EvmClient } from '../../utils/clients/types';
-
-const getWstETHAsset = async (
-  balance: bigint,
-  client: EvmClient,
-  cache: Cache
-) => {
-  const conversionResult = await client.readContract({
-    address: wstETHAddress,
-    abi: wstETHAbi,
-    functionName: 'getStETHByWstETH',
-    args: [balance],
-  });
-
-  const stETHAmount = new BigNumber((conversionResult as bigint).toString());
-  const ethTokenPrice = await cache.getTokenPrice(
-    ethereumNetwork.native.address,
-    networkId
-  );
-
-  if (!ethTokenPrice?.price) return null;
-
-  return tokenPriceToAssetToken(
-    ethereumNetwork.native.address,
-    stETHAmount.div(ethFactor).toNumber(),
-    networkId,
-    ethTokenPrice
-  );
-};
-
-const getStMATICAsset = async (
-  balance: bigint,
-  client: EvmClient,
-  cache: Cache
-) => {
-  const conversionResult = await client.readContract({
-    address: stMATICAddress,
-    abi: maticAbi,
-    functionName: 'convertStMaticToMatic',
-    args: [balance],
-  });
-
-  const maticAmount = new BigNumber(
-    (conversionResult as bigint[]).at(0)?.toString() || '0'
-  );
-  const maticPrice = await cache.getTokenPrice(maticTokenAddress, networkId);
-
-  if (!maticPrice?.price) return null;
-
-  return tokenPriceToAssetToken(
-    maticTokenAddress,
-    maticAmount.div(ethFactor).toNumber(),
-    networkId,
-    maticPrice
-  );
-};
+import { getWstETHAsset, getStMATICAsset } from './helper';
 
 const executor: FetcherExecutor = async (owner: string, cache: Cache) => {
   const client = getEvmClient(networkId);
