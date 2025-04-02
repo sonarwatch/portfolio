283e59b25157a7f4bb71ea726651e4af7de4b3a2
Charles Ouimet <charles.ouimet@gmail.com>
2025-04-02 16:32:43 -0400

    Got rid of extra spaces

--- packages/plugins/src/plugins/stader/constants.ts
+++ packages/plugins/src/plugins/stader/constants.ts
@@ -6,14 +6,14 @@ export const platformId = 'stader';
  * Matches https://stader.gitbook.io/stader/ethereum/smart-contracts#ethx-mainnet-smart-contracts and
  * https://github.com/llamafolio/llamafolio-api/blob/bf669cbaf424a7694954d151e39e143d28ff2e35/src/adapters/stader/ethereum/index.ts#L6-L11
  */
-export const CONTRACT_ADDRESS_ETHX_TOKEN_ETHEREUM_MAINNET: Address  = '0xA35b1B31Ce002FBF2058D22F30f95D405200A15b';
+export const CONTRACT_ADDRESS_ETHX_TOKEN_ETHEREUM_MAINNET: Address = '0xA35b1B31Ce002FBF2058D22F30f95D405200A15b';
 
 /**
  * Matches https://stader.gitbook.io/stader/ethereum/smart-contracts#ethx-mainnet-smart-contracts
  */
-export const CONTRACT_ADDRESS_PERMISSIONLESS_NODE_REGISTRY_ETHEREUM_MAINNET: Address  = '0x4f4Bfa0861F62309934a5551E0B2541Ee82fdcF1';
+export const CONTRACT_ADDRESS_PERMISSIONLESS_NODE_REGISTRY_ETHEREUM_MAINNET: Address = '0x4f4Bfa0861F62309934a5551E0B2541Ee82fdcF1';
 
-export const CONTRACT_ADDRESS_STADER_UTILITY_POOL_ETHEREUM_MAINNET: Address  = '0xED6EE5049f643289ad52411E9aDeC698D04a9602';
+export const CONTRACT_ADDRESS_STADER_UTILITY_POOL_ETHEREUM_MAINNET: Address = '0xED6EE5049f643289ad52411E9aDeC698D04a9602';
 
 export const CONTRACT_ADDRESS_STADER_COLLATERAL_POOL_ETHEREUM_MAINNET: Address = '0x7Af4730cc8EbAd1a050dcad5c03c33D2793EE91f';
 
