fa68d413a6a9b3a6c0fe087573fff10156c6ea2b
Charles Ouimet <charles.ouimet@gmail.com>
2025-04-04 14:40:26 -0400

    [feature/OF-5597] Moved away from manually crafting `PortfolioElement`s to leveraging `ElementRegistry`
    Slack discussion that triggered this conversion: https://octavfi.slack.com/archives/C08KX01HLMS/p1743778862813079
    
    Thanks to @BitWonka for pointing it out !

---
+++ packages/plugins/src/utils/octav/addStakedToRegistry.ts
@@ -0,0 +1,34 @@
+import { Address } from 'viem';
+import { ElementRegistry } from '../elementbuilder/ElementRegistry';
+import { LoggingContext, verboseLog } from './loggingUtils';
+
+export const addStakedToRegistry = (
+  registry: ElementRegistry,
+  assetName: string,
+  assetContractAddress: Address,
+  amount: number,
+  logCtx: LoggingContext
+): ElementRegistry => {
+  const element = registry.addElementMultiple({
+    label: 'Staked',
+    name: assetName,
+    tags: ['Staked'],
+  });
+
+  element.addAsset({
+    address: assetContractAddress,
+    amount,
+  });
+
+  verboseLog(
+    {
+      ...logCtx,
+      element,
+      assetContractAddress,
+      amount,
+    },
+    'Added element to registry'
+  );
+
+  return registry;
+};
