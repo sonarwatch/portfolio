7ce9386596bd44e2e3ec71e1cc2a0607b0c2050b
Davina Tshipata Mbiya <nmbiya1@gmail.com>
2025-04-07 09:42:43 -0400

    use checksum addresses

--- packages/plugins/src/plugins/renzo/constants.ts
+++ packages/plugins/src/plugins/renzo/constants.ts
@@ -12,24 +12,24 @@ export const renzoNetworkConfigs: RenzoNetworkConfig[] = [
     networkId: NetworkId.ethereum,
     stakedContracts: [
       {
-        address: '0xbf5495efe5db9ce00f80364c8b423567e58d2110',
-        token: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+        address: '0xbF5495eFe5Db9Ce00f80364C8B423567e58D2110',
+        token: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
       },
       {
-        address: '0xd4fcde9bb1d746dd7e5463b01dd819ee06af25db',
-        token: '0xec53bf9167f50cdeb3ae105f56099aaab9061f83',
+        address: '0xD4fCDe9Bb1D746dD7E5463b01Dd819EE06Af25DB',
+        token: '0xec53BF9167f50cdEB3AE105F56099aAab9061F83',
       },
       {
-        address: '0x77b1183e730275f6a8024ce53d54bcc12b368f60',
-        token: '0x3b50805453023a91a8bf641e279401a0b23fa6f9',
+        address: '0x77B1183E730275f6A8024CE53d54BCC12B368f60',
+        token: '0x3B50805453023a91a8Bf641e279401a0b23fA6f9',
       },
     ],
     activeStakeContract: {
-      address: '0x1736011d3e075351b319dbc1da28dac68ea830a6',
-      token: '0x3b50805453023a91a8bf641e279401a0b23fa6f9',
+      address: '0x1736011D3E075351b319DbC1dA28Dac68ea830A6',
+      token: '0x3B50805453023a91a8Bf641e279401a0b23fA6f9',
     },
     depositContract: {
-      address: '0x5efc9d10e42fb517456f4ac41eb5e2ebe42c8918',
+      address: '0x5efc9D10e42Fb517456f4ac41eB5E2eBe42C8918',
     },
   },
 ];
