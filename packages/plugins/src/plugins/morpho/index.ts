fe91feffc274b6708dea5c68f457f3bffcbc100e
josht <85530348+jtardioli@users.noreply.github.com>
2025-04-08 10:11:23 -0400

    Feature/of 5642/integrate morpho (#50)
    * save my place
    
    * pre messing up the prices
    
    * Finisg deposit and lending positions
    
    * update imports
    
    * yield fetcher set up
    
    * cover yield positions
    
    * vaulst job
    
    * finish yield positions
    
    * update imports
    
    * fix imports
    
    * fix imports
    
    * save
    
    * update env file
    
    * clean up new abis
    
    * add config for other chains
    
    * add support for other hcains
    
    * remove other chains since they are not really supported
    
    * charles requested changes
    
    * update tokenprice
    
    * switch over to yield positions
    
    * save refactors
    
    * save changes
    
    * update to use pagination
    
    * suggestions
    
    * update price function
    
    * next round of fixing
    
    * update to new fancy way
    
    * dont need to filter out 0
    
    * timestamp outside of loop
    
    * move to big int zero constant
    
    * update rewards fetcher
    
    * strictly type the function
    
    * add stronger typing
    
    * add stronger types
    
    * fix calculations
    
    * save
    
    * comment out morpho aave integration
    
    * bump version numbers

--- packages/plugins/src/plugins/morpho/index.ts
+++ packages/plugins/src/plugins/morpho/index.ts
@@ -1,9 +1,9 @@
 import { NetworkId } from '@sonarwatch/portfolio-core';
 import { Fetcher } from '../../Fetcher';
 import { Job } from '../../Job';
-import aaveV3CollateralFetcher from './aaveV3CollateralFetcher';
-import aaveV3SupplyOnlyFetcher from './aaveV3SupplyOnlyFetcher';
-import aaveV3Job from './aaveV3Job';
+// import aaveV3CollateralFetcher from './aaveV3CollateralFetcher';
+// import aaveV3SupplyOnlyFetcher from './aaveV3SupplyOnlyFetcher';
+// import aaveV3Job from './aaveV3Job';
 
 import { getLendingFetcher } from './lendingFetcher';
 import { getYieldFetcher } from './yieldFetcher';
@@ -13,13 +13,13 @@ import { marketsJob } from './marketsJob';
 import { vaultsJob } from './vaultsJob';
 
 export const jobs: Job[] = [
-  aaveV3Job,
+  // aaveV3Job,
   marketsJob(NetworkId.ethereum),
   vaultsJob(NetworkId.ethereum),
 ];
 export const fetchers: Fetcher[] = [
-  aaveV3CollateralFetcher,
-  aaveV3SupplyOnlyFetcher,
+  // aaveV3CollateralFetcher,
+  // aaveV3SupplyOnlyFetcher,
   getLendingFetcher(NetworkId.ethereum),
   getYieldFetcher(NetworkId.ethereum),
   getRewardsFetcher(NetworkId.ethereum),
