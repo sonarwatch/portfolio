b725e8c73c1ba7783b42de88b6b3430d44e38123
Guillaume <courmontguillaume@gmail.com>
2025-04-11 16:51:11 +0200

    bette fix

--- packages/plugins/src/plugins/nirvana/depositFetcher.ts
+++ packages/plugins/src/plugins/nirvana/depositFetcher.ts
@@ -26,6 +26,11 @@ const executor: FetcherExecutor = async (owner: string, cache: Cache) => {
     pid
   )
     .addFilter('owner', new PublicKey(owner))
+    // the only real USDC accounts
+    .addFilter(
+      'tenant',
+      new PublicKey('BcAoCEdkzV2J21gAjCCEokBw5iMnAe96SbYo9F6QmKWV')
+    )
     .addDataSizeFilter(272)
     .run();
   if (personalAccounts.length === 0) return [];
@@ -33,11 +38,6 @@ const executor: FetcherExecutor = async (owner: string, cache: Cache) => {
   const elementRegistry = new ElementRegistry(NetworkId.solana, platformId);
 
   personalAccounts.forEach((acc) => {
-    // Skip the demo accounts from temple.nirvana.finance
-    if (
-      acc.tenant.toString() === '8PWyxJYYpoJgrgeGRUEqqFRun4z9zzZFy5cCZvXSXWe5'
-    )
-      return;
     const lendingElement = elementRegistry.addElementBorrowlend({
       label: 'Lending',
       ref: acc.pubkey,
