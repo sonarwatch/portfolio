e983cf2847f1e8fa3cfa7833aba8795b96d4fa69
Courmont Guillaume <courmontguillaume@gmail.com>
2025-04-17 21:56:56 +0200

    add mPST pricing

---
+++ packages/plugins/src/plugins/huma/mpstPricingJob.ts
@@ -0,0 +1,27 @@
+import { NetworkId, walletTokensPlatformId } from '@sonarwatch/portfolio-core';
+import { Cache } from '../../Cache';
+import { Job, JobExecutor } from '../../Job';
+import { platformId } from './constants';
+import { usdcSolanaMint } from '../../utils/solana';
+
+const executor: JobExecutor = async (cache: Cache) => {
+  const usdcPrice = await cache.getTokenPrice(usdcSolanaMint, NetworkId.solana);
+  if (!usdcPrice) return;
+  await cache.setTokenPriceSource({
+    address: 'HUPfpnsaJtJGpJxAPNX1vXah7BgYiQYt1c2JMgMumvPs',
+    networkId: NetworkId.solana,
+    decimals: 6,
+    id: 'Huma',
+    platformId: walletTokensPlatformId,
+    price: usdcPrice.price,
+    timestamp: Date.now(),
+    weight: 1,
+  });
+};
+
+const job: Job = {
+  id: `${platformId}-token-stats`,
+  executor,
+  labels: ['normal'],
+};
+export default job;
