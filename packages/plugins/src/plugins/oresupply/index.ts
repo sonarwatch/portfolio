991ae4e8cc6601cfac36808e0e32c9528eb94089
Courmont Guillaume <courmontguillaume@gmail.com>
2025-04-17 19:42:13 +0200

    fix

--- packages/plugins/src/plugins/oresupply/index.ts
+++ packages/plugins/src/plugins/oresupply/index.ts
@@ -1,7 +1,7 @@
 import { Fetcher } from '../../Fetcher';
 import { Job } from '../../Job';
-import positionsSolanaFetcher from './stakeFetcher';
+import stakeFetcher from './stakeFetcher';
 import boostJob from './boostJob';
 
 export const jobs: Job[] = [boostJob];
-export const fetchers: Fetcher[] = [positionsSolanaFetcher];
+export const fetchers: Fetcher[] = [stakeFetcher];
