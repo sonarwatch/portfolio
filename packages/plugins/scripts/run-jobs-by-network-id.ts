d9adb604045ce0d7c423be81b17b79f1b7f9e76c
andrei_kozel <andrei.kozel@p2p.org>
2025-04-21 14:07:09 +0200

    Added jobs running with timeouts

--- packages/plugins/scripts/run-jobs-by-network-id.ts
+++ packages/plugins/scripts/run-jobs-by-network-id.ts
@@ -1,7 +1,8 @@
 import {
-  assertNetworkId, Network,
+  assertNetworkId,
+  Network,
   NetworkIdType,
-  networks
+  networks,
 } from '@sonarwatch/portfolio-core';
 import { getCache, getLlamaProtocolsJob, jobs } from '../src';
 
@@ -11,14 +12,23 @@ if (!networkId || networkId === '') {
   process.exit(1);
 }
 const fNetworkId: NetworkIdType = assertNetworkId(networkId);
-const networkData: Network = networks[fNetworkId]
+const networkData: Network = networks[fNetworkId];
 
-const allJobs = [...jobs, getLlamaProtocolsJob([{id: networkData.id, defiLlamaId: networkData.llamaId}])];
+const allJobs = [
+  ...jobs,
+  getLlamaProtocolsJob([
+    {
+      id: networkData.id,
+      defiLlamaId: networkData.llamaId,
+    },
+  ]),
+];
 
 async function runJobs(network: NetworkIdType) {
-  const networkJobs = allJobs.filter(
-    (f) => f.networkIds.includes(network) || f.networkIds.includes('ALL')
-  );
+  const networkJobs = allJobs
+    .filter(
+      (f) => f.networkIds.includes(network) || f.networkIds.includes('ALL')
+    );
 
   if (!jobs.length) {
     console.error(`Job cannot be found for network: ${network}`);
@@ -29,17 +39,40 @@ async function runJobs(network: NetworkIdType) {
 
   const startDate = Date.now();
   console.log(`Running jobs: ${jobs.length}`);
-  const promises = networkJobs.map(async (job) => {
-    console.log(`${job.id}. Running.`);
-    const startJob = Date.now();
-    await job.executor(cache);
-    const durationJob = ((Date.now() - startJob) / 1000).toFixed(2);
-    console.log(`${job.id}. Finished (${durationJob}s)`);
-  });
-  await Promise.all(promises);
+  const chunkSize = 30;
+
+  const chunks = Array.from(
+    { length: Math.ceil(networkJobs.length / chunkSize) },
+    (_, i) => networkJobs.slice(i * chunkSize, i * chunkSize + chunkSize)
+  );
+  for (const chunk of chunks) {
+    await Promise.all(
+      chunk.map(async (job) => {
+        console.log(`${job.id}. Running.`);
+        const startJob = Date.now();
+        try {
+          await awaitWithTimeout(job.executor(cache), 60000);
+          const durationJob = ((Date.now() - startJob) / 1000).toFixed(2);
+          console.log(`${job.id}. Finished (${durationJob}s)`);
+        } catch (err) {
+          const durationJob = ((Date.now() - startJob) / 1000).toFixed(2);
+          console.error(`${job.id}. Failed (${durationJob}s)`);
+        }
+      })
+    );
+  }
   const duration = ((Date.now() - startDate) / 1000).toFixed(2);
   console.log(`Finished (${duration}s)`);
   process.exit(0);
+}
+
+async function awaitWithTimeout<T>(promise: Promise<T>, timeoutMs: number): Promise<T> {
+  return Promise.race([
+    promise,
+    new Promise<never>((_, reject) => {
+      setTimeout(() => reject(new Error(`Timeout after ${timeoutMs}ms`)), timeoutMs)
+    })
+  ]);
 }
 
 runJobs(fNetworkId);
