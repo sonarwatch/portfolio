9495b20639af8381b292542c726bc9accf7b11dd
opptylabs <oppty@proton.me>
2025-04-25 16:51:11 +0200

    tx-parser cnft as spam

--- packages/tx-parser/src/utils/transactionIsCnftMint.ts
+++ packages/tx-parser/src/utils/transactionIsCnftMint.ts
@@ -8,15 +8,24 @@ const mintToCollectionV1InstructionDiscriminator = [
 ];
 const mintV1InstructionDiscriminator = [145, 98, 192, 118, 184, 147, 118, 104];
 
+function arraysEqual(a: number[], b: number[]): boolean {
+  if (a.length !== b.length) return false;
+  return a.every((value, index) => value === b[index]);
+}
+
 export const transactionIsCnftMint = (
   txn: ParsedTransactionWithMeta
 ): boolean =>
-  txn.transaction.message.instructions.some(
-    (i) =>
-      i.programId.toString() === bubblegumContract.address &&
-      isPartiallyDecodedInstruction(i) &&
-      [
-        mintToCollectionV1InstructionDiscriminator,
-        mintV1InstructionDiscriminator,
-      ].includes(getDiscriminator(i.data))
-  );
+  txn.transaction.message.instructions.some((i) => {
+    if (
+      i.programId.toString() !== bubblegumContract.address ||
+      !isPartiallyDecodedInstruction(i)
+    ) {
+      return false;
+    }
+    const discriminator = getDiscriminator(i.data);
+    return [
+      mintToCollectionV1InstructionDiscriminator,
+      mintV1InstructionDiscriminator,
+    ].some((d) => arraysEqual(d, discriminator));
+  });
