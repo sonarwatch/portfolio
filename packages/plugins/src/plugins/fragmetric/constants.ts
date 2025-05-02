dbf918a0a2d81fba54ded4ce5ea47963beb79e4c
Courmont Guillaume <courmontguillaume@gmail.com>
2025-05-02 10:29:35 +0200

    add fragBTC pricing

--- packages/plugins/src/plugins/fragmetric/constants.ts
+++ packages/plugins/src/plugins/fragmetric/constants.ts
@@ -19,4 +19,10 @@ export const vaults = [
     tokenMint: 'jtojtomepa8beP8AuQc6eXt5FriJwfFMwQx2v2f9mCL',
     reserveAccount: '3Fz1WVV7N3h5VScoveHyote6Y129F3Gmc3PcfnBuSbV3',
   },
+  {
+    vaultProgramAddress: 'BmJvUzoiiNBRx3v2Gqsix9WvVtw8FaztrfBHQyqpMbTd',
+    receiptToken: 'FRAGB4KZGLMy3wH1nBajP3Q17MHnecEvTPT6wb4pX5MB',
+    tokenMint: 'zBTCug3er3tLyffELcvDNrKkCymbPWysGcWihESYfLg',
+    reserveAccount: 'B5eYu4dKBS8bTvLdeZungomcaQx52rDprdKEcyHzgbBD',
+  },
 ];
