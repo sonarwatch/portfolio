e8fd9160e5c74d8fac0381a200ca8a1b1e546d3c
Courmont Guillaume <courmontguillaume@gmail.com>
2025-05-15 10:53:05 +0200

    fix

--- packages/plugins/src/plugins/helium/depositFetcher.ts
+++ packages/plugins/src/plugins/helium/depositFetcher.ts
@@ -1,12 +1,7 @@
 import { NetworkId } from '@sonarwatch/portfolio-core';
 import { Cache } from '../../Cache';
-import { platformId, stakeRegistryId } from './constants';
+import { platformId } from './constants';
 import { Fetcher, FetcherExecutor } from '../../Fetcher';
-import { getPositionAccounts } from './utils';
-import { ElementRegistry } from '../../utils/elementbuilder/ElementRegistry';
-import { getClientSolana } from '../../utils/clients';
-import { ParsedGpa } from '../../utils/solana/beets/ParsedGpa';
-import { positionDataStruct } from './structs';
 
 const executor: FetcherExecutor = async (owner: string, cache: Cache) => [];
 
