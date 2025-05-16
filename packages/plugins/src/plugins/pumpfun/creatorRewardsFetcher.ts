0e71819981d91a3f559cebd24dc2db79b6bcb72f
opptylabs <oppty@proton.me>
2025-05-16 08:39:43 +0200

    pumpfun rewards fix

--- packages/plugins/src/plugins/pumpfun/creatorRewardsFetcher.ts
+++ packages/plugins/src/plugins/pumpfun/creatorRewardsFetcher.ts
@@ -7,59 +7,35 @@ import BigNumber from 'bignumber.js';
 import { Cache } from '../../Cache';
 import { Fetcher, FetcherExecutor } from '../../Fetcher';
 import { platformId } from './constants';
-import { coinCreatorVaultAta, creatorVaultPda } from './helpers';
+import { creatorVaultPda } from './helpers';
 import { ElementRegistry } from '../../utils/elementbuilder/ElementRegistry';
 import { getClientSolana } from '../../utils/clients';
-import { getParsedAccountInfo } from '../../utils/solana/getParsedAccountInfo';
-import { tokenAccountStruct } from '../../utils/solana';
 
 const executor: FetcherExecutor = async (owner: string, cache: Cache) => {
   const client = getClientSolana();
   const ownerPk = new PublicKey(owner);
   const creatorVault = creatorVaultPda(ownerPk);
-  const coinCreatorVault = coinCreatorVaultAta(
-    ownerPk,
-    solanaNativeWrappedAddress
+
+  const creatorVaultAccount = await client.getAccountInfo(creatorVault);
+
+  if (!creatorVaultAccount) return [];
+
+  const rentExemptionLamports = await client.getMinimumBalanceForRentExemption(
+    creatorVaultAccount.data.length
   );
-  const [creatorVaultAccount, coinCreatorVaultAccount] = await Promise.all([
-    client.getAccountInfo(creatorVault),
-    coinCreatorVault
-      ? getParsedAccountInfo(client, tokenAccountStruct, coinCreatorVault)
-      : undefined,
-  ]);
-
-  let creatorVaultAmount = new BigNumber(0);
-  let coinVaultAmount = new BigNumber(0);
-  if (creatorVaultAccount !== null) {
-    const rentExemptionLamports =
-      await client.getMinimumBalanceForRentExemption(
-        creatorVaultAccount.data.length
-      );
-    creatorVaultAmount = new BigNumber(creatorVaultAccount.lamports).minus(
-      rentExemptionLamports
-    );
-  }
-
-  if (coinCreatorVaultAccount) {
-    coinVaultAmount = new BigNumber(coinCreatorVaultAccount.amount);
-  }
 
   const registry = new ElementRegistry(NetworkId.solana, platformId);
+
   const element = registry.addElementMultiple({
     label: 'Rewards',
     link: `https://pump.fun/profile/${owner}?&tab=coins`,
   });
+
   element.addAsset({
     address: solanaNativeWrappedAddress,
-    amount: coinVaultAmount,
-    ref: coinCreatorVault,
-    attributes: {
-      isClaimable: true,
-    },
-  });
-  element.addAsset({
-    address: solanaNativeWrappedAddress,
-    amount: creatorVaultAmount,
+    amount: new BigNumber(creatorVaultAccount.lamports).minus(
+      rentExemptionLamports
+    ),
     ref: creatorVault,
     attributes: {
       isClaimable: true,
