2a7c34ab71a23783a54603a5e9d6d33c471cc0b4
Courmont Guillaume <courmontguillaume@gmail.com>
2025-05-16 13:33:57 +0200

    feat(tx-parser): add Creator Rewards service to drip platform

--- packages/tx-parser/src/services/drip/index.ts
+++ packages/tx-parser/src/services/drip/index.ts
@@ -3,7 +3,8 @@ import { ServiceDefinition } from '../../ServiceDefinition';
 
 const platformId = 'drip';
 const dripcNFTSenderAddress = 'DRiPPP2LytGjNZ5fVpdZS7Xi1oANSY3Df1gSxvUKpzny';
-
+const creatorRewardsSenderAddress =
+  'DRiPChZsgQMpQMkEd3odwE6fnAfcXjGgtfrqymBQTuTS';
 export const services: ServiceDefinition[] = [
   {
     id: `${platformId}-collecting`,
@@ -15,6 +16,19 @@ export const services: ServiceDefinition[] = [
         (accountKey) =>
           accountKey.signer &&
           accountKey.pubkey.toString() === dripcNFTSenderAddress
+      );
+    },
+  },
+  {
+    id: `${platformId}-creator-rewards`,
+    name: 'Creator Rewards',
+    platformId,
+    networkId: NetworkId.solana,
+    matchTransaction(txn) {
+      return txn.transaction.message.accountKeys.some(
+        (accountKey) =>
+          accountKey.signer &&
+          accountKey.pubkey.toString() === creatorRewardsSenderAddress
       );
     },
   },
