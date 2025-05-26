8f5036bfe1dbb913bb3d024072171bddc43cef79
opptylabs <oppty@proton.me>
2025-05-26 14:33:57 +0200

    saveSOL price

--- packages/plugins/src/plugins/save/index.ts
+++ packages/plugins/src/plugins/save/index.ts
@@ -4,6 +4,12 @@ import reservesJob from './reservesJob';
 import marketsJob from './marketsJob';
 import obligationFetcher from './obligationsFetcher';
 import token2022wrapperJob from './token2022wrapperJob';
+import saveSolPriceJob from './saveSolPriceJob';
 
-export const jobs: Job[] = [marketsJob, reservesJob, token2022wrapperJob];
+export const jobs: Job[] = [
+  marketsJob,
+  reservesJob,
+  token2022wrapperJob,
+  saveSolPriceJob,
+];
 export const fetchers: Fetcher[] = [obligationFetcher];
