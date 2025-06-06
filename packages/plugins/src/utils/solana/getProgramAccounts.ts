354b90de5d1ca11de4dd4cb265a381ff65ef429b
Andrew Kozel <47276289+andrey-kozel@users.noreply.github.com>
2025-06-06 17:50:49 +0300

    LAM-1523 Added safe cache checking (#32)

--- packages/plugins/src/utils/solana/getProgramAccounts.ts
+++ packages/plugins/src/utils/solana/getProgramAccounts.ts
@@ -20,70 +20,83 @@ export async function getProgramAccounts(
   filters?: GetProgramAccountsFilter[],
   maxAccounts = 0
 ) {
-  return withTiming(`getProgramAccounts ${programId.toString()}`, async () => {
-    const compressedAccounts: string | undefined = await withTiming(`RedisLoading. ${programId.toString()}`, async () => getCache().getItem(
-      `${programId.toString()}`,
-      {
-        networkId: NetworkId.solana,
-        prefix: programCachePrefix,
+  return withTiming(
+    `getProgramAccounts ${programId.toString()}`,
+    async () => {
+      try {
+        const compressedAccounts: string | undefined = await withTiming(
+          `RedisLoading. ${programId.toString()}`,
+          async () =>
+            getCache().getItem(`${programId.toString()}`, {
+              networkId: NetworkId.solana,
+              prefix: programCachePrefix,
+            }),
+          MAX_TIME_FILTERING_TO_LOG
+        );
+
+        if (compressedAccounts) {
+          const cachedAccounts: any[] = await decompress(compressedAccounts);
+          console.log(
+            `Program Accounts are loaded from cache for program ${programId}`
+          );
+
+          return cachedAccounts
+            .filter(({ account }) => {
+              const data = Buffer.from(account.data.data);
+              return filters?.every((filter) => {
+                if ('dataSize' in filter) {
+                  return data.length === filter['dataSize'];
+                }
+                if ('memcmp' in filter) {
+                  const bytes = bs58.decode(filter['memcmp'].bytes);
+                  const segment = data.subarray(
+                    filter['memcmp'].offset,
+                    filter['memcmp'].offset + bytes.length
+                  );
+
+                  return segment.equals(bytes);
+                }
+                return true;
+              });
+            })
+            .map((account) => ({
+              pubkey: new PublicKey(account.pubkey),
+              account: {
+                data: Buffer.from(account.account.data.data),
+                executable: account.account.executable,
+                lamports: account.account.lamports,
+                owner: new PublicKey(account.account.owner),
+                rentEpoch: account.account.rentEpoch,
+                space: account.account.space,
+              },
+            }));
+        }
+      } catch (err) {
+        console.error(
+          `Failed to extract cached data for ${programId.toString()}`,
+          err
+        );
       }
-    ), MAX_TIME_FILTERING_TO_LOG);
 
-    if (compressedAccounts) {
-      const cachedAccounts: any[] = await decompress(compressedAccounts)
-      console.log(
-        `Program Accounts are loaded from cache for program ${programId}`
-      );
+      const config: GetProgramAccountsConfig = {
+        encoding: 'base64',
+        filters,
+      };
 
-      return cachedAccounts
-        .filter(({ account }) => {
-          const data = Buffer.from(account.data.data);
-          return filters?.every((filter) => {
-            if ('dataSize' in filter) {
-              return data.length === filter['dataSize'];
-            }
-            if ('memcmp' in filter) {
-              const bytes = bs58.decode(filter['memcmp'].bytes);
-              const segment = data.subarray(
-                filter['memcmp'].offset,
-                filter['memcmp'].offset + bytes.length
-              );
+      if (maxAccounts <= 0) {
+        return connection.getProgramAccounts(programId, config);
+      }
 
-              return segment.equals(bytes);
-            }
-            return true;
-          });
-        })
-        .map((account) => ({
-          pubkey: new PublicKey(account.pubkey),
-          account: {
-            data: Buffer.from(account.account.data.data),
-            executable: account.account.executable,
-            lamports: account.account.lamports,
-            owner: new PublicKey(account.account.owner),
-            rentEpoch: account.account.rentEpoch,
-            space: account.account.space,
-          },
-        }));
-    }
+      const accountsRes = await connection.getProgramAccounts(programId, {
+        ...config,
+        dataSlice: { offset: 0, length: 0 },
+      });
 
-    const config: GetProgramAccountsConfig = {
-      encoding: 'base64',
-      filters,
-    };
+      if (accountsRes.length > maxAccounts)
+        throw new Error(`Too much accounts to get (${accountsRes.length})`);
 
-    if (maxAccounts <= 0) {
       return connection.getProgramAccounts(programId, config);
-    }
-
-    const accountsRes = await connection.getProgramAccounts(programId, {
-      ...config,
-      dataSlice: { offset: 0, length: 0 },
-    });
-
-    if (accountsRes.length > maxAccounts)
-      throw new Error(`Too much accounts to get (${accountsRes.length})`);
-
-    return connection.getProgramAccounts(programId, config);
-  }, MAX_TIME_MS_EXECUTION_TO_LOG);
+    },
+    MAX_TIME_MS_EXECUTION_TO_LOG
+  );
 }
