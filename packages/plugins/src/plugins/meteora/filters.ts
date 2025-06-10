08007cace73ab8dcc1fd09ae3492cf2faa070822
opptylabs <oppty@proton.me>
2025-06-10 14:07:03 +0200

    meteora damm v1 pools gpa filter

--- packages/plugins/src/plugins/meteora/filters.ts
+++ packages/plugins/src/plugins/meteora/filters.ts
@@ -2,16 +2,6 @@ import { GetProgramAccountsFilter } from '@solana/web3.js';
 
 export const vaultsFilters: GetProgramAccountsFilter[] = [{ dataSize: 10240 }];
 
-export const constantPoolsFilters: GetProgramAccountsFilter[] = [
-  { dataSize: 944 },
-];
-
-export const stablePoolsFilters: GetProgramAccountsFilter[] = [
-  { dataSize: 1387 },
-];
-
-export const memePoolsFilters: GetProgramAccountsFilter[] = [{ dataSize: 952 }];
-
 export const farmAccountFilter = (
   owner: string
 ): GetProgramAccountsFilter[] => [
