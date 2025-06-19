49ed4479b4c8939df0d7c31975cd735a8763fb59
opptylabs <oppty@proton.me>
2025-06-19 17:00:40 +0200

    refactor(utils): remove `formatUsdValue` helper and update `getUsdValueSum` and `getUsdValueSumStrict` to handle formatting inline

--- packages/core/src/utils/getUsdValueSum.ts
+++ packages/core/src/utils/getUsdValueSum.ts
@@ -1,16 +1,13 @@
 import { UsdValue } from '../UsdValue';
 
-const formatUsdValue = (value: UsdValue): number => {
-  if (value === null || value === 0) return 0;
-  return Number(value.toFixed(9));
-};
-
 export function getUsdValueSum(usdValues: UsdValue[]): number {
-  return formatUsdValue(
-    usdValues.reduce(
-      (sum: number, currUsdValue) =>
-        currUsdValue !== null ? sum + currUsdValue : sum,
-      0
-    )
+  return Number(
+    usdValues
+      .reduce(
+        (sum: number, currUsdValue) =>
+          currUsdValue !== null ? sum + currUsdValue : sum,
+        0
+      )
+      .toFixed(9)
   );
 }
--- packages/core/src/utils/getUsdValueSumStrict.ts
+++ packages/core/src/utils/getUsdValueSumStrict.ts
@@ -1,9 +1,11 @@
 import { UsdValue } from '../UsdValue';
 
 export function getUsdValueSumStrict(usdValues: UsdValue[]): UsdValue {
-  return usdValues.reduce(
+  const value = usdValues.reduce(
     (sum, currUsdValue) =>
       sum !== null && currUsdValue !== null ? sum + currUsdValue : null,
     0
   );
+  if (value === null || value === 0) return value;
+  return Number(value.toFixed(9));
 }
