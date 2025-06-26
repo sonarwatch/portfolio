c5613e6187470cf7d6d3e0ba981dd520d2568d16
opptylabs <oppty@proton.me>
2025-06-26 15:40:24 +0200

    huma yield

--- packages/plugins/src/plugins/huma/index.ts
+++ packages/plugins/src/plugins/huma/index.ts
@@ -4,7 +4,8 @@ import mpstPricingJob from './mpstPricingJob';
 import { airdropFetcher, fetcher } from './airdropFetcher';
 import { AirdropFetcher } from '../../AirdropFetcher';
 import stakingFetcher from './stakingFetcher';
+import pstYieldJob from './pstYieldJob';
 
-export const jobs: Job[] = [mpstPricingJob];
+export const jobs: Job[] = [mpstPricingJob, pstYieldJob];
 export const fetchers: Fetcher[] = [fetcher, stakingFetcher];
 export const airdropFetchers: AirdropFetcher[] = [airdropFetcher];
