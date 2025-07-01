984a24b98f0fb07aeb06e71166d6a0f532199763
Guillaume <courmontguillaume@gmail.com>
2025-07-01 16:01:30 +0200

    feat(plugins) : W.I.P voltr

---
+++ packages/plugins/src/plugins/voltr/constants.ts
@@ -0,0 +1,10 @@
+import { PublicKey } from '@solana/web3.js';
+
+export const platformId = 'voltr';
+
+export const vaultPid = new PublicKey(
+  'EwAei87GBsgeLueC7mShT2TNbH3BYumP4RskusxUFBn6'
+);
+export const adaptorPid = new PublicKey(
+  '3BufioDyECNwuFJLRGCXNbZFrsnJnCsALMfDKjQEnk8x'
+);
